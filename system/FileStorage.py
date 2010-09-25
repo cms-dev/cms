6819da070a911aa6b58e48232707d1ef2dc9d576
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2010-09-25 17:39:51 +0200

    First FileStorage sketch

---
+++ system/FileStorage.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+import socket
+import sys
+import os
+import tempfile
+import shutil
+import hashlib
+
+# FIXME - Bad hack
+def maybe_mkdir(d):
+	try:
+		os.mkdir(d)
+	except:
+		pass
+
+class FileStorage:
+	def __init__(self):
+		# Create server
+		server = SimpleXMLRPCServer(("localhost", 8000))
+		server.register_introspection_functions()
+
+		# Create server directories
+		self.basedir = "fs"
+		self.tmpdir = os.path.join(self.basedir, "tmp")
+		self.objdir = os.path.join(self.basedir, "objects")
+		maybe_mkdir(self.basedir)
+		maybe_mkdir(self.tmpdir)
+		maybe_mkdir(self.objdir)
+
+		# Run the server's main loop
+		server.serve_forever()
+
+	def put(self, address, port, description = ""):
+		fileSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		fileSocket.connect((address, port))
+		# FIXME - Error management
+		(tempFile, tempFilename) = tempfile.mkstemp(dir = self.tmpdir)
+		hasher = hashlib.sha1()
+		while True:
+			data = fileSocket.recv(8192)
+			if not data:
+				break
+			tempFile.write(data)
+			hasher.update(data)
+		tempFile.close()
+		fileSocket.close()
+		digest = hasher.hexdigest()
+		shutil.move(tempFilename, os.path.join(self.objdir, digest))
+		return digest
+
+	def get(self, address, port, digest):
+		fileSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		fileSocket.connect((address, port))
+		# FIXME - Error management
+		try:
+			with open(os.path.join(self.objdir, digest)) as inputFile:
+				while True:
+					data = inputFile.read(8192)
+					if not data:
+						break
+					remaining = len(data)
+					while remaining > 0:
+						sent = fileSocket.send(data[-remaining:])
+						remaining -= sent
+		except IOError:
+			fileSocket.close()
+			return False
+		fileSocket.close()
+		return True
+
