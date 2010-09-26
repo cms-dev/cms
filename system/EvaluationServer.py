41377122b4726aceffb4937e617c724d29d564f8
Stefano Maggiolo <s.maggiolo@gmail.com>
2010-09-26 21:26:57 +0200

    Method add_job to ask for a compilation on EvaluationServer. Sketch for a test of evaluation.

--- system/EvaluationServer.py
+++ system/EvaluationServer.py
@@ -107,18 +107,23 @@ class JobDispatcher(threading.Thread):
                     except LookupError:
                         time.sleep(2)
 
-                log("Asking worker %d (%s:%d) to %s submission %s" % (worker,
-                                                                      Configuration.workers[worker][0],
-                                                                      Configuration.workers[worker][1],
-                                                                      action,
-                                                                      submission.couch_id))
-                p = xmlrpclib.ServerProxy("http://%s:%d" % Configuration.workers[worker])
+                log("Asking worker %d (%s:%d) to %s submission %s" %
+                    (worker,
+                     Configuration.workers[worker][0],
+                     Configuration.workers[worker][1],
+                     action,
+                     submission.couch_id))
+                p = xmlrpclib.ServerProxy("http://%s:%d" %
+                                          Configuration.workers[worker])
                 if action == "compile":
                     p.compile(submission.couch_id)
                 elif action == "evaluate":
                     p.evaluate(submission.couch_id)
 
 class EvaluationServer:
+    JOB_PRIORITY_HIGH, JOB_PRIORITY_MEDIUM, JOB_PRIORITY_LOW = xrange(3)
+    JOB_TYPE_COMPILATION, JOB_TYPE_EVALUATION = xrange(2)
+
     def __init__(self, contest, listen_address = None, listen_port = None):
         self.contest = contest
         if listen_address == None:
@@ -135,27 +140,78 @@ class EvaluationServer:
         self.jd = JobDispatcher(queue, workers)
         jd.start()
 
+        server.register_function(self.add_job)
         server.register_function(self.compilation_finished)
         server.register_function(self.evaluation_finished)
 
         # Run forever the server's main loop
         server.serve_forever()
 
+    def add_job(self, submission_id):
+        log("Queueing compilation for submission %s" % (submission_id))
+        submission = CouchObject.from_couch(submission_id)
+        self.queue.lock()
+        self.queue.push((EvaluationServer.JOB_TYPE_COMPILATION,
+                         submission_id),
+                        EvaluationServer.JOB_PRIORITY_HIGH,
+                        submission.timestamp)
+        self.queue.unlock()
+
     def action_finished(self, job):
         worker = self.workers.find_worker(job)
         self.workers.release_worker(worker)
-        log("Worker %d (%s:%d) finished to %s submission %s" % (worker,
-                                                                Configuration.workers[worker][0],
-                                                                Configuration.workers[worker][1],
-                                                                job[0],
-                                                                job[1].couch_id))
+        log("Worker %d (%s:%d) finished to %s submission %s" %
+            (worker,
+             Configuration.workers[worker][0],
+             Configuration.workers[worker][1],
+             job[0],
+             job[1].couch_id))
 
     def compilation_finished(self, success, submission_id):
         self.action_finished(("compile", submission_id))
-        if not success:
+        if success:
+            log("Compilation succeeded for submission %s" % (submission_id))
+            submission = CouchObject.from_couch(submission_id)
+            log("Queueing evaluation for submission %s" % (submission_id))
+            self.queue.lock()
+            self.queue.push((EvaluationServer.JOB_TYPE_EVALUATION,
+                             submission_id),
+                            EvaluationServer.JOB_PRIORITY_LOW,
+                            submission.timestamp)
+            self.queue.unlock()
+        else:
+            self.add_job(submission_id)
             log("Compilation failed for submission %s" % (submission_id))
 
     def evaluation_finished(self, success, submission_id):
         self.action_finished(("evaluate", submission_id))
-        if not success:
+        if success:
+            log("Evaluation succeeded for submission %s" % (submission_id))
+        else:
+            submission = CouchObject.from_couch(submission_id)
+            self.queue.lock()
+            self.queue.push((EvaluationServer.JOB_TYPE_EVALUATION,
+                             submission_id),
+                            EvaluationServer.JOB_PRIORITY_LOW,
+                            submission.timestamp)
+            self.queue.unlock()
             log("Evaluation failed for submission %s" % (submission_id))
+
+
+if __name__ == "__main__":
+    import Worker
+    import Contest
+    import Submission
+
+    c = Contest.sample_contest()
+    s = Submission.sample_submission()
+    c.submissions.append(s)
+
+    es_address, es_port = Configuration.evaluation_server
+    e = EvaluationServer(c, es_address, es_port)
+    for worker in Configuration.workers:
+        address, port = worker
+        w = Worker(c, address, port)
+
+    es = xmlrpclib.ServerProxy("http://localhost:%d" % es_port)
+    es.add_job(s.couch_id)
