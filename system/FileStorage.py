69965af9d7138b8f4509a360c42303746a843164
Stefano Maggiolo <s.maggiolo@gmail.com>
2010-09-26 10:52:35 +0200

    Changed tabs to spaces

--- system/FileStorage.py
+++ system/FileStorage.py
@@ -12,70 +12,70 @@ import hashlib
 
 # FIXME - Bad hack
 def maybe_mkdir(d):
-	try:
-		os.mkdir(d)
-	except:
-		pass
+    try:
+        os.mkdir(d)
+    except:
+        pass
 
 class FileStorage:
-	def __init__(self, basedir, listen_address = "", listen_port = 8000):
-		# Create server
-		server = SimpleXMLRPCServer((listen_address, listen_port))
-		server.register_introspection_functions()
+    def __init__(self, basedir, listen_address = "", listen_port = 8000):
+        # Create server
+        server = SimpleXMLRPCServer((listen_address, listen_port))
+        server.register_introspection_functions()
 
-		# Create server directories
-		self.basedir = basedir
-		self.tmpdir = os.path.join(self.basedir, "tmp")
-		self.objdir = os.path.join(self.basedir, "objects")
-		maybe_mkdir(self.basedir)
-		maybe_mkdir(self.tmpdir)
-		maybe_mkdir(self.objdir)
+        # Create server directories
+        self.basedir = basedir
+        self.tmpdir = os.path.join(self.basedir, "tmp")
+        self.objdir = os.path.join(self.basedir, "objects")
+        maybe_mkdir(self.basedir)
+        maybe_mkdir(self.tmpdir)
+        maybe_mkdir(self.objdir)
 
-		server.register_function(self.get)
-		server.register_function(self.put)
+        server.register_function(self.get)
+        server.register_function(self.put)
 
-		# Run the server's main loop
-		server.serve_forever()
+        # Run the server's main loop
+        server.serve_forever()
 
-	def put(self, address, port, description = ""):
-		fileSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		fileSocket.connect((address, port))
-		# FIXME - Error management
-		tempFile, tempFilename = tempfile.mkstemp(dir = self.tmpdir)
-		tempFile = os.fdopen(tempFile, "w")
-		hasher = hashlib.sha1()
-		while True:
-			data = fileSocket.recv(8192)
-			if not data:
-				break
-			tempFile.write(data)
-			hasher.update(data)
-		tempFile.close()
-		fileSocket.close()
-		digest = hasher.hexdigest()
-		shutil.move(tempFilename, os.path.join(self.objdir, digest))
-		return digest
+    def put(self, address, port, description = ""):
+        fileSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        fileSocket.connect((address, port))
+        # FIXME - Error management
+        tempFile, tempFilename = tempfile.mkstemp(dir = self.tmpdir)
+        tempFile = os.fdopen(tempFile, "w")
+        hasher = hashlib.sha1()
+        while True:
+            data = fileSocket.recv(8192)
+            if not data:
+                break
+            tempFile.write(data)
+            hasher.update(data)
+        tempFile.close()
+        fileSocket.close()
+        digest = hasher.hexdigest()
+        shutil.move(tempFilename, os.path.join(self.objdir, digest))
+        return digest
 
-	def get(self, address, port, digest):
-		fileSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		fileSocket.connect((address, port))
-		# FIXME - Error management
-		try:
-			with open(os.path.join(self.objdir, digest)) as inputFile:
-				while True:
-					data = inputFile.read(8192)
-					if not data:
-						break
-					remaining = len(data)
-					while remaining > 0:
-						sent = fileSocket.send(data[-remaining:])
-						remaining -= sent
-		except IOError:
-			fileSocket.close()
-			return False
-		fileSocket.close()
-		return True
+    def get(self, address, port, digest):
+        fileSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        fileSocket.connect((address, port))
+        # FIXME - Error management
+        try:
+            with open(os.path.join(self.objdir, digest)) as inputFile:
+                while True:
+                    data = inputFile.read(8192)
+                    if not data:
+                        break
+                    remaining = len(data)
+                    while remaining > 0:
+                        sent = fileSocket.send(data[-remaining:])
+                        remaining -= sent
+        except IOError:
+            fileSocket.close()
+            return False
+        fileSocket.close()
+        return True
 
 if __name__ == "__main__":
-	fs = FileStorage("fs")
+    fs = FileStorage("fs")
 
--- system/FileStorageLib.py
+++ system/FileStorageLib.py
@@ -10,85 +10,85 @@ import shutil
 import random
 
 class FilePutThread(threading.Thread):
-	def __init__(self, putSocket, putFile):
-		threading.Thread.__init__(self)
-		self.putSocket = putSocket
-		self.putFile = putFile
+    def __init__(self, putSocket, putFile):
+        threading.Thread.__init__(self)
+        self.putSocket = putSocket
+        self.putFile = putFile
 
-	def run(self):
-		(conn, addr) = self.putSocket.accept()
-		while True:
-			data = self.putFile.read(8192)
-			if not data:
-				break
-			remaining = len(data)
-			while remaining > 0:
-				sent = conn.send(data[-remaining:])
-				remaining -= sent
-		conn.close()
+    def run(self):
+        (conn, addr) = self.putSocket.accept()
+        while True:
+            data = self.putFile.read(8192)
+            if not data:
+                break
+            remaining = len(data)
+            while remaining > 0:
+                sent = conn.send(data[-remaining:])
+                remaining -= sent
+        conn.close()
 
 class FileGetThread(threading.Thread):
-	def __init__(self, getSocket, getFile):
-		threading.Thread.__init__(self)
-		self.getSocket = getSocket
-		self.getFile = getFile
+    def __init__(self, getSocket, getFile):
+        threading.Thread.__init__(self)
+        self.getSocket = getSocket
+        self.getFile = getFile
 
-	def run(self):
-		(conn, addr) = self.getSocket.accept()
-		while True:
-			data = conn.recv(8192)
-			if not data:
-				break
-			self.getFile.write(data)
-		conn.close()
+    def run(self):
+        (conn, addr) = self.getSocket.accept()
+        while True:
+            data = conn.recv(8192)
+            if not data:
+                break
+            self.getFile.write(data)
+        conn.close()
 
 class FileStorageLib:
-	def __init__(self):
-		self.bind_address = ''
-		local_addresses = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] + ['localhost']
-		self.local_address = local_addresses[0]
-		self.remote_address = 'localhost' # FIXME: get FileStorage ip from configuration
-		self.fs = xmlrpclib.ServerProxy('http://localhost:8000')
+    def __init__(self):
+        self.bind_address = ''
+        local_addresses = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] + ['localhost']
+        self.local_address = local_addresses[0]
+        self.remote_address = 'localhost' # FIXME: get FileStorage ip from configuration
+        self.fs = xmlrpclib.ServerProxy('http://localhost:8000')
 
-	def put(self, path):
-		putSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		port = self.random_bind(putSocket, self.bind_address)
-		putSocket.listen(1)
-		with open(path) as putFile:
-			ft = FilePutThread(putSocket, putFile)
-			ft.start()
-			res = self.fs.put(self.local_address, port)
-		putSocket.close()
-		return res
+    def put(self, path):
+        putSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        port = self.random_bind(putSocket, self.bind_address)
+        putSocket.listen(1)
+        with open(path) as putFile:
+            ft = FilePutThread(putSocket, putFile)
+            ft.start()
+            res = self.fs.put(self.local_address, port)
+        putSocket.close()
+        return res
 
-	def get(self, dig, path):
-		getSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		port = self.random_bind(getSocket, self.bind_address)
-		getSocket.listen(1)
-		with open(path, "w") as getFile:
-			ft = FileGetThread(getSocket, getFile)
-			ft.start()
-			res = self.fs.get(self.local_address, port, dig)
-			ft.join()
-		getSocket.close()
-		return res
+    def get(self, dig, path):
+        getSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        port = self.random_bind(getSocket, self.bind_address)
+        getSocket.listen(1)
+        with open(path, "w") as getFile:
+            ft = FileGetThread(getSocket, getFile)
+            ft.start()
+            res = self.fs.get(self.local_address, port, dig)
+            ft.join()
+        getSocket.close()
+        return res
 
-	def random_bind(self, bindSocket, address):
-		ok = False
-		while not ok:
-			try:
-				port = random.randint(10000, 60000)
-				bindSocket.bind((address, port))
-				ok = True
-			except socket.error as (errno, strerror):
-				if errno != os.errno.EADDRINUSE:
-					raise
+    def random_bind(self, bindSocket, address):
+        ok = False
+        while not ok:
+            try:
+                port = random.randint(10000, 60000)
+                bindSocket.bind((address, port))
+                ok = True
+            except socket.error as (errno, strerror):
+                if errno != os.errno.EADDRINUSE:
+                    raise
                 return port
 
 if __name__ == "__main__":
-	FSL = FileStorageLib()
-	dig = FSL.put("ciao.txt")
-	print "Put file ciao.txt, digest = %s" % (dig)
-	res = FSL.get(dig, "ciao2.txt")
-	print "Got file ciao2.txt, value =", res
+    FSL = FileStorageLib()
+    dig = FSL.put("ciao.txt")
+    print "Put file ciao.txt, digest = %s" % (dig)
+    res = FSL.get(dig, "ciao2.txt")
+    print "Got file ciao2.txt, value =", res
 
