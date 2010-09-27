5c266d43b117ce8cbf41a0a81cfbdcf46455dbb4
Matteo Boscariol <boscarim@hotmail.com>
2010-09-27 12:15:08 +0200

    Typo

--- system/ContestWebServer.py
+++ system/ContestWebServer.py
@@ -106,7 +106,7 @@ class UseTokenHandler(BaseHandler):
                     # salvataggio in couchdb
                     s.to_couch()
                     # avvisare Eval Server
-                    try
+                    try:
                         es = xmlrpclib.ServerProxy("http://%s:%d"%Configuration.evaluation_server)
                         es.use_token(s.couch_id)
                     except:
