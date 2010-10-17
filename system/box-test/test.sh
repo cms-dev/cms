e5988c7f9280d253886c98c82ede92e59097e10d
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2010-10-17 10:26:34 +0200

    Differentiate from timeout and wall-clock timeout in sample programs for mo-box.

--- system/box-test/test.sh
+++ system/box-test/test.sh
@@ -12,7 +12,7 @@ for i in $PROGLIST ; do
 	redecho "==> $i <=="
 	rm $i
 	make $i
-	../mo-box -t 0.1 -M $i.log -m 10000 -v -v -v -v -v -a 0 -- ./$i
+	../mo-box -t 1 -x 1 -w 3 -M $i.log -m 10000 -v -v -v -v -v -a 0 -- ./$i
 	echo Exited with code $?
 done
 
