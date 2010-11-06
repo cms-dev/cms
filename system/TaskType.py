23906803f98892afb707e499f8581309ca3d6fbf
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2010-11-07 00:20:15 +0100

    Rationality and error checking while compiling significantly increased.

--- system/TaskType.py
+++ system/TaskType.py
@@ -37,6 +37,8 @@ class BatchTaskType:
         self.submission = submission
 
     def finish_compilation(self, success, compilation_success = False, text = ""):
+        if "sandbox" in self.__dict__:
+            self.sandbox.delete()
         if not success:
             return False
         if compilation_success:
@@ -63,26 +65,25 @@ class BatchTaskType:
         valid, language = self.submission.verify_source()
         if not valid or language == None:
             Utils.log("Invalid submission or couldn't detect language")
-            return self.terminate_compilation("Invalid files in submission")
+            return self.finish_compilation(True, False, "Invalid files in submission")
         if len(self.submission.files) != 1:
             Utils.log("Submission cointains %d files, expecting 1" % (len(self.submission.files)))
-            return self.terminate_compilation("Invalid files in submission")
+            return self.finish_compilation(True, False, "Invalid files in submission")
 
         source_filename = self.submission.files.keys()[0]
         executable_filename = source_filename.replace(".%s" % (language), "")
 
         try:
-            sandbox = Sandbox()
+            self.sandbox = Sandbox()
         except:
             Utils.log("Couldn't create sandbox", Utils.Logger.SEVERITY_IMPORTANT)
-            return False
-        Utils.log("Created sandbox in %s" % (sandbox.path, self.submission.couch_id))
+            return self.finish_compilation(False)
 
         try:
-            sandbox.create_file_from_storage(source_filename, self.submission.files[source_filename])
+            self.sandbox.create_file_from_storage(source_filename, self.submission.files[source_filename])
         except:
             Utils.log("Couldn't copy file %s in sandbox" % (source_filename))
-            return False
+            return self.finish_compilation(False)
 
         if language == "c":
             command = ["/usr/bin/gcc", "-DEVAL", "-static", "-O2", "-lm", "-o", executable_filename, source_filename]
@@ -90,101 +91,73 @@ class BatchTaskType:
             command = ["/usr/bin/g++", "-DEVAL", "-static", "-O2", "-o", executable_filename, source_filename]
         elif language == "pas":
             command = ["/usr/bin/gpc", "-dEVAL", "-XS", "-O2", "-o%s" % (executable_filename), source_filename]
-        sandbox.chdir = sandbox.path
-        sandbox.preserve_env = True
-        sandbox.filter_syscalls = 1
-        sandbox.allow_fork = True
-        sandbox.file_check = 1
+
+        self.sandbox.chdir = self.sandbox.path
+        self.sandbox.preserve_env = True
+        self.sandbox.filter_syscalls = 1
+        self.sandbox.allow_fork = True
+        self.sandbox.file_check = 1
         # FIXME - This allows the compiler to read files in other temporary directories,
         # so they should be cleaned beforehand; moreover, file access limits are not
         # enforced on children processes (like ld); and these paths are tested only with g++
-        sandbox.allow_path = ['/etc/', '/lib/', '/usr/', '/tmp/', source_filename, executable_filename]
-        sandbox.timeout = 10
-        sandbox.address_space = 256 * 1024
-        sandbox.stdout_file = sandbox.relative_path("compiler_stdout.txt")
-        sandbox.stderr_file = sandbox.relative_path("compiler_stderr.txt")
+        self.sandbox.allow_path = ['/etc/', '/lib/', '/usr/', '/tmp/', source_filename, executable_filename]
+        self.sandbox.timeout = 10
+        self.sandbox.address_space = 256 * 1024
+        self.sandbox.stdout_file = self.sandbox.relative_path("compiler_stdout.txt")
+        self.sandbox.stderr_file = self.sandbox.relative_path("compiler_stderr.txt")
         Utils.log("Starting compilation")
         try:
-            sandbox.execute(command)
+            self.sandbox.execute(command)
         except Exception as e:
-            Utils.log("Couldn't spawn sandbox, raised exception %s" % (repr(e)))
-            return False
+            Utils.log("Couldn't spawn sandbox (exception %s)" % (repr(e)))
+            return self.finish_compilation(False)
 
-        executable_present = True
+        executable_present = self.sandbox.file_exists(executable_filename)
+        exit_status = self.sandbox.get_exit_status()
+        exit_code = self.sandbox.get_exit_code()
         try:
-            sandbox.stat_file(executable_filename)
-        except OSError:
-            executable_present = False
-
-        exit_status = sandbox.get_exit_status()
-        exit_code = sandbox.get_exit_code()
-        try:
-            stderr = sandbox.get_file_to_string("compiler_stderr.txt")
+            stderr = self.sandbox.get_file_to_string("compiler_stderr.txt")
         except IOError as e:
             Utils.log("Couldn't retrieve compiler stderr (exception: %s)" % (repr(e)), Utils.Logger.SEVERITY_IMPORTANT)
-            return False
+            return self.finish_compilation(False)
+
         if exit_status == Sandbox.EXIT_OK and exit_code == 0:
             try:
-                self.submission.executables = {}
-                self.submission.executables[executable_filename] = sandbox.get_file_to_storage(executable_filename, "Executable %s for submission %s" % (executable_filename, self.submission.couch_id))
-                self.submission.compilation_text = "OK %s\nCompiler output:\n%s" % (sandbox.get_stats(), stderr)
-                self.submission.compilation_outcome = "ok"
-                self.submission.to_couch()
-                Utils.log("Compilation for submission %s successfully finished" % (self.submission.couch_id))
-                return True
+                self.submission.executables = {executable_filename: self.sandbox.get_file_to_storage(executable_filename, "Executable %s for submission %s" % (executable_filename, self.submission.couch_id))}
             except (IOError, OSError) as e:
-                Utils.log("Compilation for submission %s successfully finished, but coudln't update the database (exception: %s)" % (self.submission.couch_id, repr(e)), Utils.Logger.SEVERITY_IMPORTANT)
-                return False
+                Utils.log("Compilation apparently successfully finished, but coudln't retrieve executable file (exception: %s)" % (repr(e)), Utils.Logger.SEVERITY_IMPORTANT)
+                return self.finish_compilation(False)
+            else:
+                Utils.log("Compilation successfully finished")
+                return self.finish_compilation(True, True, "OK %s\nCompiler output:\n%s" % (self.sandbox.get_stats(), stderr))                
 
         if exit_status == Sandbox.EXIT_OK and exit_code != 0:
-            try:
-                self.submission.compilation_text = "Failed %s\nCompiler output:\n%s" % (sandbox.get_stats(), stderr)
-                self.submission.compilation_outcome = "fail"
-                self.submission.to_couch()
-                Utils.log("Compilation failed")
-                return True
-            except (IOError, OSError) as e:
-                Utils.log("Compilation failed, but couldn't update the database (exception: %s)" % (repr(e)), Utils.Logger.SEVERITY_IMPORTANT)
-                return False
+            Utils.log("Compilation failed")
+            return self.finish_compilation(True, False, "Failed %s\nCompiler output:\n%s" % (self.sandbox.get_stats(), stderr))
+
+        if exit_status == Sandbox.EXIT_TIMEOUT:
+            Utils.log("Compilation timed out")
+            return self.finish_compilation(True, False, "Time out %s\nCompiler output:\n%s" % (self.sandbox.get_stats(), stderr))
+
+        if exit_status == Sandbox.EXIT_SIGNAL:
+            signal = self.sandbox.get_killing_signal()
+            Utils.log("Compilation killed with signal %d" % (signal))
+            return self.finish_compilation(True, False, "Killed with signal %d %s\nThis could be triggered by violating memory limits\nCompiler output:\n%s" % (signal, self.sandbox.get_stats(), stderr))
 
         if exit_status == Sandbox.EXIT_SANDBOX_ERROR:
             Utils.log("Compilation aborted because of sandbox error", Utils.Logger.SEVERITY_IMPORTANT)
-            return False
+            return self.finish_compilation(False)
 
         if exit_status == Sandbox.EXIT_SYSCALL:
-            Utils.log("Compilation aborted becasue of forbidden syscall", Utils.Logger.SEVERITY_IMPORTANT)
-            return False
+            Utils.log("Compilation aborted because of forbidden syscall", Utils.Logger.SEVERITY_IMPORTANT)
+            return self.finish_compilation(False)
 
         if exit_status == Sandbox.EXIT_FILE_ACCESS:
             Utils.log("Compilaton aborted because of forbidden file access", Utils.Logger.SEVERITY_IMPORTANT)
-            return False
+            return self.finish_compilation(False)
 
-        if exit_status == Sandbox.EXIT_TIMEOUT:
-            try:
-                self.submission.compilation_test = "Time out %s\nCompiler output:\n%s" % (sandbox.get_stats(), stderr)
-                self.submission.compilation_outcome = "fail"
-                self.submission.to_couch()
-                Utils.log("Compilation timed out")
-                return True
-            except (IOError, OSError) as e:
-                Utils.log("Compilation timed out, but couldn't update the database (exception: %s)" % (repr(e)), Utils.Logger.SEVERITY_IMPORTANT)
-                return False
-
-        if exit_status == Sandbox.EXIT_SIGNAL:
-            try:
-                signal = sandbox.get_killing_signal()
-                self.submission.compilation_test = "Killed with signal %d %s\nThis could be triggered by violating memory limits\nCompiler output:\n%s" % (signal, sandbox.get_stats(), stderr)
-                self.submission.compilation_outcome = "fail"
-                self.submission.to_couch()
-                Utils.log("Compilation killed with signal %d" % (signal))
-                return True
-            except (IOError, OSError) as e:
-                Utils.log("Compilation killed with signal %d, but couldn't update the database (exception: %s)" % (signal, repr(e)), Utils.Logger.SEVERITY_IMPORTANT)
-                return False
-
-        #sandbox.delete()
-        #Utils.log("Sandbox for compiling submission %s deleted" % (self.submission.couch_id))
-        return False
+        Utils.log("Shouldn't arrive here, failing", Utils.Logger.SEVERITY_IMPORTANT)
+        return self.finish_compilation(False)
 
     def execute_single(self, test_number):
         executable_filename = self.submission.executables.keys()[0]
