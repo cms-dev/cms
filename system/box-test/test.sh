7c6f096478478f04c3595a7746b103956be9c755
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2010-11-27 16:43:33 +0100

    Enable output.txt file for bigwrite test.

--- system/box-test/test.sh
+++ system/box-test/test.sh
@@ -12,7 +12,7 @@ for i in $PROGLIST ; do
 	redecho "==> $i <=="
 	rm $i
 	make $i
-	../mo-box -f -t 1 -x 1 -w 3 -M $i.log -m 10000 -v -v -v -v -v -a 0 -- ./$i
+	../mo-box -f -t 1 -x 1 -w 3 -M $i.log -m 10000 -v -v -v -v -v -a 1 -p output.txt -- ./$i
 	echo Exited with code $?
 done
 
