889824e374d4972903695f5261f33dafafaee7e5
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2011-01-15 14:29:48 +0100

    Better cache consistency check.

--- system/check_cache.sh
+++ system/check_cache.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-# Simple and hacky  one-liner to check if files present in both fs/objects and
-# fs-cache/objects actually have the same content.
 
+# Check whether files present in both fs/objects and fs-cache/objects actually
+# have the same content.
 for file in `cat <(ls fs/objects/) <(ls fs-cache/objects/) | sort | uniq -d` ; do
 	diff fs/objects/$file fs-cache/objects/$file
+done
+
+# Check whether files present if fs/objects and fs-cache/objects have their
+# filename matching their SHA1 sum
+for file in fs/objects/* fs-cache/objects/* ; do
+	diff <(sha1sum $file | cut -d' ' -f1) <(echo `basename $file`)
 done
 
