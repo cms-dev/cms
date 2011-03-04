fbf5cc56a81d96513bef318da772dab0144b9918
Stefano Maggiolo <s.maggiolo@gmail.com>
2011-03-04 19:12:58 +0100

    Stylistic editing to CWS.

--- system/ContestWebServer.py
+++ system/ContestWebServer.py
@@ -42,16 +42,16 @@ import Utils
 from Submission import Submission
 from FileStorageLib import FileStorageLib
 
+
 class BaseHandler(tornado.web.RequestHandler):
-    """
-    Base RequestHandler for this application.
+    """Base RequestHandler for this application.
 
     All the RequestHandler classes in this application should be a
     child of this class.
     """
+
     def prepare(self):
-        """
-        This method is executed at the beginning of each request.
+        """This method is executed at the beginning of each request.
         """
         # Attempt to update the contest and all its references
         # If this fails, the request terminates.
@@ -59,13 +59,13 @@ class BaseHandler(tornado.web.RequestHandler):
         try:
             c.refresh()
         except Exception as e:
-            Utils.log("CouchDB exception:"+repr(e),Utils.Logger.SEVERITY_CRITICAL)
+            Utils.log("CouchDB exception:" + repr(e),
+                      Utils.Logger.SEVERITY_CRITICAL)
             self.write("Can't connect to CouchDB Server")
             self.finish()
-        
+
     def get_current_user(self):
-        """
-        Gets the current user logged in from the cookies
+        """Gets the current user logged in from the cookies
 
         If a valid cookie is retrieved, returns a User object with the
         username specified in the cookie. Otherwise, returns None.
@@ -73,7 +73,8 @@ class BaseHandler(tornado.web.RequestHandler):
         if self.get_secure_cookie("login") == None:
             return None
         try:
-            username, cookie_time = pickle.loads(self.get_secure_cookie("login"))
+            username, cookie_time = \
+                pickle.loads(self.get_secure_cookie("login"))
         except:
             return None
         #if cookie_time == None or cookie_time < upsince:
@@ -83,14 +84,16 @@ class BaseHandler(tornado.web.RequestHandler):
         return current_user
 
     def render_params(self):
+        """Default parameters to give to all handlers
+        """
         r = {}
-        r["timestamp"] = time.time();
-        r["contest"] = c;
+        r["timestamp"] = time.time()
+        r["contest"] = c
         r["phase"] = BusinessLayer.contest_phase(**r)
         r["cookie"] = str(self.cookies)
         return r
 
-    def valid_phase(self,r_param):
+    def valid_phase(self, r_param):
         if r_param["phase"] != 0:
             self.redirect("/")
             return False
@@ -98,8 +101,7 @@ class BaseHandler(tornado.web.RequestHandler):
 
 #class RefreshingHandler(BaseHandler):
 #    def prepare(self):
-#        """
-#        This method is executed at the beginning of each request.
+#        """This method is executed at the beginning of each request.
 #        """
 #        # Attempt to update the contest and all its references
 #        # If this fails, the request terminates.
@@ -109,26 +111,29 @@ class BaseHandler(tornado.web.RequestHandler):
 #            BusinessLayer.update_submissions(c)
 #            BusinessLayer.update_users(c)
 #        except Exception as e:
-#            Utils.log("CouchDB exception:"+repr(e),Utils.Logger.SEVERITY_CRITICAL)
+#            Utils.log("CouchDB exception:" + repr(e),
+#                      Utils.Logger.SEVERITY_CRITICAL)
 #            self.write("Can't connect to CouchDB Server")
 #            self.finish()
 
+
 class MainHandler(BaseHandler):
+    """Home page handler.
     """
-    Home page handler.
-    """
+
     def get(self):
         r_params = self.render_params()
         self.render("welcome.html", **r_params)
 
+
 class LoginHandler(BaseHandler):
+    """Login handler.
     """
-    Login handler.
-    """
+
     def post(self):
         username = self.get_argument("username", "")
         password = self.get_argument("password", "")
-        next = self.get_argument("next","/")
+        next = self.get_argument("next", "/")
         user = BusinessLayer.get_user_by_username(c, username)
         user.refresh()
         if user == None or user.password != password:
@@ -136,33 +141,38 @@ class LoginHandler(BaseHandler):
                       (username, password, self.request.remote_ip))
             self.redirect("/?login_error=true")
             return
-        if WebConfig.ip_lock and user.ip != "0.0.0.0" and user.ip != self.request.remote_ip:
+        if WebConfig.ip_lock and user.ip != "0.0.0.0" \
+                and user.ip != self.request.remote_ip:
             Utils.log("Unexpected IP: user=%s pass=%s remote_ip=%s." %
                       (username, password, self.request.remote_ip))
             self.redirect("/?login_error=true")
             return
         if user.hidden and WebConfig.block_hidden_users:
-            Utils.log("Hidden user login attempt: user=%s pass=%s remote_ip=%s." %
+            Utils.log("Hidden user login attempt: " +
+                      "user=%s pass=%s remote_ip=%s." %
                       (username, password, self.request.remote_ip))
             self.redirect("/?login_error=true")
             return
 
-        self.set_secure_cookie("login", \
-                  pickle.dumps( (self.get_argument("username"), time.time()) ) )
+        self.set_secure_cookie("login",
+                               pickle.dumps((self.get_argument("username"),
+                                             time.time())))
         self.redirect(next)
 
+
 class LogoutHandler(BaseHandler):
+    """Logout handler.
     """
-    Logout handler.
-    """
+
     def get(self):
         self.clear_cookie("login")
         self.redirect("/")
 
+
 class SubmissionViewHandler(BaseHandler):
+    """Shows the submissions stored in the contest.
     """
-    Shows the submissions stored in the contest.
-    """
+
     @tornado.web.authenticated
     def get(self, task_name):
 
@@ -179,15 +189,17 @@ class SubmissionViewHandler(BaseHandler):
         r_params["task"].refresh()
 
         # get the list of the submissions
-        r_params["submissions"] = BusinessLayer.get_submissions_by_username(c, self.current_user.username, task_name)
+        r_params["submissions"] = BusinessLayer.get_submissions_by_username(\
+                c, self.current_user.username, task_name)
         BusinessLayer.refresh_array(r_params["submissions"])
 
         self.render("submission.html", **r_params)
 
+
 class SubmissionDetailHandler(BaseHandler):
+    """Shows additional details for the specified submission.
     """
-    Shows additional details for the specified submission.
-    """
+
     @tornado.web.authenticated
     def get(self, submission_id):
 
@@ -195,42 +207,49 @@ class SubmissionDetailHandler(BaseHandler):
         if not self.valid_phase(r_params):
             return
         # search the submission in the contest
-        submission = BusinessLayer.get_submission(c, submission_id, self.current_user.username)
+        submission = BusinessLayer.get_submission(c,
+                                                  submission_id,
+                                                  self.current_user.username)
         if submission == None:
             raise tornado.web.HTTPError(404)
         submission.refresh()
-        r_params["submission"] = submission;
-        r_params["task"] = submission.task;
+        r_params["submission"] = submission
+        r_params["task"] = submission.task
         r_params["task"].refresh()
         self.render("submission_detail.html", **r_params)
 
+
 class SubmissionFileHandler(BaseHandler):
+    """Shows a submission file.
     """
-    Shows a submission file.
-    """
+
     @tornado.web.authenticated
     def get(self, submission_id, filename):
 
         r_params = self.render_params()
         if not self.valid_phase(r_params):
             return
-        submission = BusinessLayer.get_submission(c, submission_id, self.current_user.username)
+        submission = BusinessLayer.get_submission(c,
+                                                  submission_id,
+                                                  self.current_user.username)
         submission.refresh()
         # search the submission in the contest
-        file_content = BusinessLayer.get_file_from_submission(submission, filename )
+        file_content = BusinessLayer.get_file_from_submission(submission,
+                                                              filename)
         if file_content == None:
             raise tornado.web.HTTPError(404)
 
         # FIXME - Set the right headers
-        self.set_header("Content-Type","text/plain")
+        self.set_header("Content-Type", "text/plain")
         self.set_header("Content-Disposition",
                         "attachment; filename=\"%s\"" % (filename))
         self.write(file_content)
 
+
 class TaskViewHandler(BaseHandler):
+    """Shows the data of a task in the contest.
     """
-    Shows the data of a task in the contest.
-    """
+
     @tornado.web.authenticated
     def get(self, task_name):
 
@@ -242,15 +261,17 @@ class TaskViewHandler(BaseHandler):
         except:
             raise tornado.web.HTTPError(404)
         r_params["task"].refresh()
-        r_params["submissions"] = BusinessLayer.get_submissions_by_username(c, self.current_user.username, task_name)
+        r_params["submissions"] = BusinessLayer.get_submissions_by_username(\
+            c, self.current_user.username, task_name)
         BusinessLayer.refresh_array(r_params["submissions"])
 
-        self.render("task.html", **r_params);
+        self.render("task.html", **r_params)
+
 
 class TaskStatementViewHandler(BaseHandler):
+    """Shows the statement file of a task in the contest.
     """
-    Shows the statement file of a task in the contest.
-    """
+
     @tornado.web.authenticated
     def get(self, task_name):
 
@@ -275,9 +296,9 @@ class TaskStatementViewHandler(BaseHandler):
 
 
 class UseTokenHandler(BaseHandler):
+    """Handles the detailed feedbaack requests.
     """
-    Handles the detailed feedbaack requests.
-    """
+
     @tornado.web.authenticated
     def post(self):
         r_params = self.render_params()
@@ -285,7 +306,7 @@ class UseTokenHandler(BaseHandler):
             return
         timestamp = r_params["timestamp"]
 
-        sub_id = self.get_argument("id","")
+        sub_id = self.get_argument("id", "")
         if sub_id == "":
             raise tornado.web.HTTPError(404)
 
@@ -295,18 +316,19 @@ class UseTokenHandler(BaseHandler):
         s.refresh()
 
         try:
-            warned = BusinessLayer.enable_detailed_feedback(c, s, timestamp, self.current_user)
+            warned = BusinessLayer.enable_detailed_feedback(c, s, timestamp,
+                                                            self.current_user)
             r_params["submission"] = s
             self.render("successfulToken.html", **r_params)
         except BusinessLayer.FeedbackAlreadyRequested:
             # Either warn the user about the issue or simply
             # redirect him to the detail page.
-            self.redirect("/submissions/details/"+sub_id)
+            self.redirect("/submissions/details/" + sub_id)
             return
         except BusinessLayer.TokenUnavailableException:
             # Redirect the user to the detail page
             # warning him about the unavailable tokens.
-            self.redirect("/submissions/details/"+sub_id+"?notokens=true")
+            self.redirect("/submissions/details/" + sub_id + "?notokens=true")
             return
         except BusinessLayer.ConnectionFailure:
             self.render("errors/connectionFailure.html")
@@ -316,9 +338,9 @@ class UseTokenHandler(BaseHandler):
 
 
 class SubmitHandler(BaseHandler):
+    """Handles the received submissions.
     """
-    Handles the received submissions.
-    """
+
     @tornado.web.authenticated
     def post(self, task_name):
 
@@ -331,10 +353,10 @@ class SubmitHandler(BaseHandler):
         task.refresh()
 
         try:
-          uploaded = self.request.files[task_name][0]
+            uploaded = self.request.files[task_name][0]
         except KeyError:
-          self.write("No file chosen.")
-          return
+            self.write("No file chosen.")
+            return
         files = {}
 
         if uploaded["content_type"] == "application/zip":
@@ -350,7 +372,8 @@ class SubmitHandler(BaseHandler):
             files[uploaded["filename"]] = uploaded["body"]
 
         try:
-            s, warned = BusinessLayer.submit( c, task, self.current_user, files, timestamp)
+            s, warned = BusinessLayer.submit(c, task, self.current_user,
+                                             files, timestamp)
             r_params["submission"] = s
             r_params["warned"] = warned
             self.render("successfulSub.html", **r_params)
@@ -363,75 +386,104 @@ class SubmitHandler(BaseHandler):
         except BusinessLayer.InvalidSubmission:
             self.render("errors/invalidSubmission.html", **r_params)
 
+
 class UserHandler(BaseHandler):
-    
+
     @tornado.web.authenticated
     def get(self):
         r_params = self.render_params()
-        self.render("user.html",**r_params)
+        self.render("user.html", **r_params)
+
 
 class InstructionHandler(BaseHandler):
+
     def get(self):
         r_params = self.render_params()
-        self.render("instructions.html",**r_params)
+        self.render("instructions.html", **r_params)
+
 
 class NotificationsHandler(BaseHandler):
+
     def post(self):
-        timestamp = time.time();
-        last_request = self.get_argument("lastrequest",timestamp)
+        timestamp = time.time()
+        last_request = self.get_argument("lastrequest", timestamp)
         messages = []
         announcements = []
         if last_request != "":
-            announcements = [x for x in c.announcements if x["date"] > float(last_request) and x["date"] < timestamp]
+            announcements = [x for x in c.announcements
+                             if x["date"] > float(last_request) \
+                                 and x["date"] < timestamp]
             if self.current_user != None:
-                messages = [x for x in self.current_user.messages if x["date"] > float(last_request) and x["date"] < timestamp]
+                messages = [x for x in self.current_user.messages
+                            if x["date"] > float(last_request) \
+                                and x["date"] < timestamp]
         self.set_header("Content-Type", "text/xml")
         self.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
         self.write("<root>")
-        self.write("<announcements>"+str(len(announcements))+"</announcements>")
-        self.write("<messages>"+str(len(messages))+"</messages>")
-        self.write("<requestdate>"+str(timestamp)+"</requestdate>");
+        self.write("<announcements>%d</announcements>" % len(announcements))
+        self.write("<messages>%d</messages>" % len(messages))
+        self.write("<requestdate>%s</requestdate>" % str(timestamp))
         self.write("</root>")
 
+
 class QuestionHandler(BaseHandler):
+
     @tornado.web.authenticated
     def post(self):
         pass
 
 handlers = [
-            (r"/", MainHandler),
-            (r"/login", LoginHandler),
-            (r"/logout", LogoutHandler),
-            (r"/submissions/([a-zA-Z0-9_-]+)", SubmissionViewHandler),
-            (r"/submissions/details/([a-zA-Z0-9_-]+)", SubmissionDetailHandler),
-            (r"/submission_file/([a-zA-Z0-9_.-]+)/([a-zA-Z0-9_.-]+)", SubmissionFileHandler),
-            (r"/tasks/([a-zA-Z0-9_-]+)", TaskViewHandler),
-            (r"/tasks/([a-zA-Z0-9_-]+)/statement", TaskStatementViewHandler),
-            (r"/usetoken/", UseTokenHandler),
-            (r"/submit/([a-zA-Z0-9_.-]+)", SubmitHandler),
-            (r"/user", UserHandler),
-            (r"/instructions",InstructionHandler),
-            (r"/notifications",NotificationsHandler),
-            (r"/questions",QuestionHandler),
-            (r"/stl/(.*)", tornado.web.StaticFileHandler, {"path": WebConfig.stl_path} ),
-
+            (r"/", \
+                 MainHandler),
+            (r"/login", \
+                 LoginHandler),
+            (r"/logout", \
+                 LogoutHandler),
+            (r"/submissions/([a-zA-Z0-9_-]+)", \
+                 SubmissionViewHandler),
+            (r"/submissions/details/([a-zA-Z0-9_-]+)", \
+                 SubmissionDetailHandler),
+            (r"/submission_file/([a-zA-Z0-9_.-]+)/([a-zA-Z0-9_.-]+)", \
+                 SubmissionFileHandler),
+            (r"/tasks/([a-zA-Z0-9_-]+)", \
+                 TaskViewHandler),
+            (r"/tasks/([a-zA-Z0-9_-]+)/statement", \
+                 TaskStatementViewHandler),
+            (r"/usetoken/", \
+                 UseTokenHandler),
+            (r"/submit/([a-zA-Z0-9_.-]+)", \
+                 SubmitHandler),
+            (r"/user", \
+                 UserHandler),
+            (r"/instructions", \
+                 InstructionHandler),
+            (r"/notifications", \
+                 NotificationsHandler),
+            (r"/questions", \
+                 QuestionHandler),
+            (r"/stl/(.*)", \
+                 tornado.web.StaticFileHandler, {"path": WebConfig.stl_path}),
            ]
 
 application = tornado.web.Application(handlers, **WebConfig.parameters)
 
 if __name__ == "__main__":
     Utils.set_service("contest web server")
-    http_server = tornado.httpserver.HTTPServer(application, ssl_options = WebConfig.ssl_options)
-    http_server.listen(WebConfig.listen_port);
+    http_server = tornado.httpserver.HTTPServer(\
+        application, ssl_options=WebConfig.ssl_options)
+    http_server.listen(WebConfig.listen_port)
     try:
         c = Utils.ask_for_contest()
     except AttributeError as e:
-        Utils.log("CouchDB server unavailable: "+repr(e), Utils.Logger.SEVERITY_CRITICAL)
+        Utils.log("CouchDB server unavailable: " + repr(e),
+                  Utils.Logger.SEVERITY_CRITICAL)
         exit(1)
     Utils.log("Contest Web Server for contest %s started..." % (c.couch_id))
     upsince = time.time()
     try:
         tornado.ioloop.IOLoop.instance().start()
     except KeyboardInterrupt:
-        Utils.log("Contest Web Server for contest %s stopped. %d threads alive" % (c.couch_id, threading.activeCount()))
+        Utils.log("Contest Web Server for contest " +
+                  "%s stopped. %d threads alive" \
+                      % (c.couch_id, threading.activeCount()))
         exit(0)
