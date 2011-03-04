64c6cce331c511ffe9534c9b279471a70ce1a1f5
Matteo Boscariol <boscarim@hotmail.com>
2011-03-04 11:37:46 +0100

    Reimporter script to refresh a contest with new information.

---
+++ system/YamlReimporter.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Programming contest management system
+# Copyright © 2010-2011 Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
+# Copyright © 2010-2011 Stefano Maggiolo <s.maggiolo@gmail.com>
+# Copyright © 2010-2011 Matteo Boscariol <boscarim@hotmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import yaml
+import os
+import sys
+
+from Task import Task
+from User import User
+from Contest import Contest
+from ScoreType import ScoreTypes
+from FileStorageLib import FileStorageLib
+import Utils
+
+def reimport_contest(path, old_contest):
+    path = os.path.realpath(path)
+    super_path, name = os.path.split(path)
+    conf = yaml.load(open(os.path.join(path,"contest.yaml")))
+
+    params = {"name": name, "couch_id": old_contest.couch_id, "couch_rev": old_contest.couch_rev}
+    assert name == conf["nome_breve"]
+    params["description"] = conf["nome"]
+    params["tasks"] = []
+    for task in conf["problemi"]:
+        matching_tasks = [ x for x in old_contest.tasks if x.name == task ]
+        if matching_tasks != []:
+          params["tasks"].append(reimport_task(matching_tasks[0], os.path.join(path, task)))
+        else:
+          params["tasks"].append(reimport_task(None, os.path.join(path, task)))
+        
+    params["token_initial"] = conf.get("token_initial", 0)
+    params["token_max"] = conf.get("token_max", 0)
+    params["token_total"] = conf.get("token_total", 0)
+    params["token_min_interval"] = conf.get("token_min_interval", 0)
+    params["token_gen_time"] = conf.get("token_gen_time", 1)
+    params["users"] = []
+    for user in conf["utenti"]:
+        matching_users = [ x for x in old_contest.users if x.username == user ]
+        if matching_users != []:
+            params["users"].append(reimport_user(matching_users[0],user))
+        else:
+            params["users"].append(reimport_user(None,user))
+
+    params["start"] = conf["inizio"]
+    params["stop"] = conf["fine"]
+
+    renewed_contest = Contest(**params)
+    renewed_contest.to_couch()
+    return renewed_contest
+
+def reimport_user(old_user, user_dict):
+    
+    params = {}
+    if old_user != None:
+        params["couch_id"] = old_user.couch_id
+        params["couch_rev"] = old_user.couch_rev
+
+    params["username"] = user_dict["username"]
+    params["password"] = user_dict["password"]
+    name = user_dict.get("nome", "")
+    surname = user_dict.get("cognome", user_dict["username"])
+    params["real_name"] = " ".join([name, surname])
+    params["ip"] = user_dict.get("ip", "0.0.0.0")
+    params["hidden"] = user_dict.get("fake", False)
+    params["tokens"] = []
+
+    renewed_user = User(**params)
+    renewed_user.to_couch()
+    return renewed_user
+
+def reimport_task(old_task, path):
+    path = os.path.realpath(path)
+    super_path, name = os.path.split(path)
+    conf = yaml.load(open(os.path.join(super_path, name + ".yaml")))
+    FSL = FileStorageLib()
+
+    params = {"name": name}
+    if old_task != None:
+        params["couch_id"] = old_task.couch_id
+        params["couch_rev"] = old_task.couch_rev
+    assert name == conf["nome_breve"]
+    params["title"] = conf["nome"]
+    params["time_limit"] = conf["timeout"]
+    params["memory_limit"] = conf["memlimit"]
+    params["attachments"] = [] # FIXME - Use auxiliary
+    params["statement"] = FSL.put(os.path.join(path, "testo", "testo.pdf"), "PDF statement for task %s" % (name))
+    params["task_type"] = Task.TASK_TYPE_BATCH
+    params["submission_format"] = ["%s.%%l" % (name)]
+    try:
+        fd = open(os.path.join(path, "cor", "correttore"))
+    except IOError:
+        fd = None
+    if fd != None:
+        params["managers"] = { "checker": FSL.put_file(fd) }
+    else:
+        params["managers"] = {}
+    params["score_type"] = ScoreTypes.SCORE_TYPE_SUM
+    params["score_parameters"] = [],
+    params["testcases"] = [ (FSL.put(os.path.join(path, "input", "input%d.txt" % (i)), "Input %d for task %s" % (i, name)),
+                             FSL.put(os.path.join(path, "output", "output%d.txt" % (i)), "Output %d for task %s" % (i, name)))
+                            for i in range(int(conf["n_input"]))]
+    params["public_testcases"] = conf.get("risultati", "").split(",")
+    if params["public_testcases"] == [""]:
+        params["public_testcases"] = []
+    params["token_initial"] = conf.get("token_initial", 0)
+    params["token_max"] = conf.get("token_max", 0)
+    params["token_total"] = conf.get("token_total", 0)
+    params["token_min_interval"] = conf.get("token_min_interval", 0)
+    params["token_gen_time"] = conf.get("token_gen_time", 60)
+
+    renewed_task = Task(**params)
+    renewed_task.to_couch()
+    return renewed_task
+
+if __name__ == "__main__":
+    import sys
+    c = Utils.ask_for_contest(1)
+    c = reimport_contest(sys.argv[1], c)
+    print "Couch ID: %s" % (c.couch_id)
