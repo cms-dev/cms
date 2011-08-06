98407935595dae855d352de8d8e610a550ebc511
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2011-08-06 17:09:03 +0200

    check_cache.sh fixed.

--- cms/contrib/check_cache.sh
+++ cms/contrib/check_cache.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# Check whether files present in both fs/objects and fs-cache/objects actually
-# have the same content.
-for file in `cat <(ls fs/objects/) <(ls fs-cache/objects/) | sort | uniq -d` ; do
-	diff fs/objects/$file fs-cache/objects/$file
-done
-
 # Check whether files present if fs/objects and fs-cache/objects have their
 # filename matching their SHA1 sum
 for file in fs/objects/* fs-cache/objects/* ; do
-	diff <(sha1sum $file | cut -d' ' -f1) <(echo `basename $file`)
+	REAL_SUM=`sha1sum $file | cut -d' ' -f1`
+	PRESUMED_SUM=`basename $file`
+	if diff <(echo $REAL_SUM) <(echo $PRESUMED_SUM) >/dev/null ; then
+		true
+	else
+		echo "File $file has wrong checksum $REAL_SUM"
+
+		# If requested, delete wrong files
+		if [ "z$1" == "zdelete" ] ; then
+			echo "Deleting file $file"
+			rm -f $file
+		fi
+	fi
 done
 
