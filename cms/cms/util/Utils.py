f453d81740a6f1351b9b46899806a84c61b15314
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2011-08-14 16:01:55 +0200

    Comments and bugfixes in Utils.

--- cms/cms/util/Utils.py
+++ cms/cms/util/Utils.py
@@ -83,15 +83,18 @@ def get_contest_list(session=None):
 def ask_for_contest(skip=0):
     if isinstance(skip, int) and len(sys.argv) > skip + 1:
         contest_id = sys.argv[skip + 1]
+
     elif isinstance(skip, str):
         contest_id = skip
+
     else:
+
         with SessionGen() as session:
             contests = get_contest_list(session)
-            print "Contests available:"
             # The ids of the contests are cached, so the session can
             # be closed as soon as possible
             matches = {}
+            print "Contests available:"
             for i, row in enumerate(contests):
                 print "%3d  -  ID: %s  -  Name: %s  -  Description: %s" % (i + 1, row.id, row.name, row.description),
                 matches[i+1] = row.id
@@ -99,17 +102,22 @@ def ask_for_contest(skip=0):
                     print " (default)"
                 else:
                     print
+
         contest_number = raw_input("Insert the number next to the contest you want to load: ")
         if contest_number == "":
             contest_number = 1
         try:
             contest_id = matches[int(contest_number)]
-        except ValueError:
+        except (ValueError, KeyError):
             print "Insert a correct number."
             sys.exit(1)
+
     return contest_id
 
 def filter_ansi_escape(s):
+    """Filter out ANSI commands from the given string.
+
+    """
     ansi_mode = False
     res = ''
     for c in s:
@@ -122,12 +130,25 @@ def filter_ansi_escape(s):
     return res
 
 def maybe_mkdir(d):
+    """Make a directory without throwing an exception if it already
+    exists. Warning: this method fails silently also if the directory
+    could not be created because there is a non-directory file with
+    the same name. In oter words, there is no guarantee that after the
+    execution, the file d exists and is a directory.
+
+    """
     try:
         os.mkdir(d)
-    except:
+    except OSError:
         pass
 
 def get_compilation_command(language, source_filename, executable_filename):
+    """Returns the compilation command for the specified language,
+    source filename and executable filename. The command is a list of
+    strings, suitable to be passed to the methods in subprocess
+    package.
+
+    """
     # For compiling in 32-bit mode under 64-bit OS: add "-march=i686",
     # "-m32" for gcc/g++. Don't know about Pascal. Anyway, this will
     # require some better support from the evaluation environment
