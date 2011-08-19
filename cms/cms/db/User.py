b6911bfc265eb331de6cda3e5380874dc48ea6f4
Stefano Maggiolo <s.maggiolo@gmail.com>
2011-08-20 01:24:43 +0200

    Documenting and cosmetic to User.py. Also, timezone is a single word.

--- cms/cms/db/User.py
+++ cms/cms/db/User.py
@@ -19,59 +19,112 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from sqlalchemy import Column, Integer, String, Boolean, Unicode, Float, ForeignKey, DateTime
+"""User-related database interface for SQLAlchemy. Not to be used
+directly (import it from SQLAlchemyAll).
+
+"""
+
+from sqlalchemy import Column, ForeignKey, Boolean, Integer, Float, String
 from sqlalchemy.orm import relationship, backref
 
 from cms.db.SQLAlchemyUtils import Base
 from cms.db.Contest import Contest
 
+
 class User(Base):
+    """Class to store a 'user participating in a contest'. Not to be
+    used directly (import it from SQLAlchemyAll).
+
+    """
+    # TODO: we really need to split this as a user (as in: not paired
+    # with a contest) and a participation.
     __tablename__ = 'users'
 
+    # Auto increment primary key.
     id = Column(Integer, primary_key=True)
+
+    # Real name (human readable) of the user.
+    real_name = Column(String, nullable=False)
+
+    # Username and password to log in the CWS.
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
-    real_name = Column(String, nullable=False)
+
+    # User can log in CWS only from this ip.
     ip = Column(String, nullable=True)
-    time_zone = Column(Float, nullable=False)
+
+    # For future use (read: APIO contest times depend on the timezone
+    # of the user).
+    timezone = Column(Float, nullable=False)
+
+    # A hidden user is used only for debugging purpose.
     hidden = Column(Boolean, nullable=False)
-    contest_id = Column(Integer, ForeignKey(Contest.id, onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
 
-    #messages (backref)
-    #questions (backref)
-    #tokens (backref)
-    contest = relationship(Contest,
-                           backref=backref("users",
-                                           single_parent=True,
-                                           cascade="all, delete, delete-orphan"))
+    # Contest (id and object) to which the user is participating.
+    contest_id = Column(Integer,
+                        ForeignKey(Contest.id,
+                                   onupdate="CASCADE", ondelete="CASCADE"),
+                        nullable=False)
+    contest = relationship(
+        Contest,
+        backref=backref("users",
+                        single_parent=True,
+                        cascade="all, delete, delete-orphan"))
 
-    def __init__(self, username, password,
-                 real_name, ip, time_zone = 0.0, contest=None, tokens = [], 
-                 hidden = False, messages = [], questions = []):
+    # Follows the description of the fields automatically added by
+    # SQLAlchemy.
+    # messages (list of Message objects)
+    # questions (list of Question objects)
+    # tokens (list of Token objects)
+
+    def __init__(self, real_name, username, password, ip,
+                 timezone=0.0, contest=None, tokens=None,
+                 hidden=False, messages=None, questions=None):
+        self.real_name = real_name
         self.username = username
         self.password = password
-        self.real_name = real_name
-        self.time_zone = time_zone
+        self.timezone = timezone
         self.ip = ip
+        if tokens == None:
+            tokens = []
         self.tokens = tokens
         self.hidden = hidden
+        if messages == None:
+            messages = []
         self.messages = messages
+        if questions == None:
+            questions = []
         self.questions = questions
         self.contest = contest
 
+
 class Message(Base):
+    """Class to store a private message from the managers to the
+    user. Not to be used directly (import it from SQLAlchemyAll).
+
+    """
     __tablename__ = 'messages'
 
+    # Auto increment primary key.
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey(User.id, onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
+
+    # Time the message was sent.
     timestamp = Column(Integer, nullable=False)
+
+    # Subject and body of the message.
     subject = Column(String, nullable=False)
     text = Column(String, nullable=False)
 
-    user = relationship(User,
-                        backref=backref('messages',
-                                        single_parent=True,
-                                        cascade="all, delete, delete-orphan"))
+    # User (id and object) owning the message.
+    user_id = Column(Integer,
+                     ForeignKey(User.id,
+                                onupdate="CASCADE", ondelete="CASCADE"),
+                     nullable=False)
+    user = relationship(
+        User,
+        backref=backref('messages',
+                        single_parent=True,
+                        cascade="all, delete, delete-orphan"))
 
     def __init__(self, timestamp, subject, text, user=None):
         self.timestamp = timestamp
@@ -79,22 +132,43 @@ class Message(Base):
         self.text = text
         self.user = user
 
+
 class Question(Base):
+    """Class to store a private question from the user to the
+    managers, and its answer. Not to be used directly (import it from
+    SQLAlchemyAll).
+
+    """
     __tablename__ = 'questions'
 
+    # Auto increment primary key.
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey(User.id, onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
+
+    # Time the question was made.
     question_timestamp = Column(Integer, nullable=False)
+
+    # Subject and body of the question.
     subject = Column(String, nullable=False)
     text = Column(String, nullable=False)
+
+    # Time the reply was sent.
     reply_timestamp = Column(Integer, nullable=True)
+
+    # Short (as in 'chosen amongst some predetermined choices') and
+    # long answer.
     short_reply = Column(String, nullable=True)
     long_reply = Column(String, nullable=True)
 
-    user = relationship(User,
-                        backref=backref('questions',
-                                        single_parent=True,
-                                        cascade="all, delete, delete-orphan"))
+    # User (id and object) owning the question.
+    user_id = Column(Integer,
+                     ForeignKey(User.id,
+                                onupdate="CASCADE", ondelete="CASCADE"),
+                     nullable=False)
+    user = relationship(
+        User,
+        backref=backref('questions',
+                        single_parent=True,
+                        cascade="all, delete, delete-orphan"))
 
     def __init__(self, question_timestamp, subject, text,
                  reply_timestamp=None, short_reply=None, long_reply=None,
@@ -106,8 +180,3 @@ class Question(Base):
         self.short_reply = short_reply
         self.long_reply = long_reply
         self.user = user
-
-def sample_user(contest):
-    import random
-    return User("username-%d" % (random.randint(1, 1000)), "password",
-                "Mister Real Name", "10.0.0.101", 0.0, contest=contest)
