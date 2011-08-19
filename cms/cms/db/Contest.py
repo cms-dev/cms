f1859ae4981819e8bec15d58645fa71826fe83fd
Stefano Maggiolo <s.maggiolo@gmail.com>
2011-08-19 16:56:10 +0200

    Comments and cosmetic changes in Contest. Plus, changed slightly the semantic of token_gen_* to agree with IOI rules.

--- cms/cms/db/Contest.py
+++ cms/cms/db/Contest.py
@@ -19,37 +19,68 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from sqlalchemy import Column, Integer, String, Boolean, Unicode, Float, ForeignKey, DateTime
+"""Contest-related database interface for SQLAlchemy. Not to be used
+directly (import it from SQLAlchemyAll).
+
+"""
+
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, backref
 
 from cms.db.SQLAlchemyUtils import Base
 
+
 class Contest(Base):
+    """Class to store a contest (which is a single day of a
+    programming competition). Not to be used directly (import it from
+    SQLAlchemyAll).
+
+    """
     __tablename__ = 'contests'
 
+    # Auto increment primary key.
     id = Column(Integer, primary_key=True)
+
+    # Short name of the contest, and longer description. Both human
+    # readable.
     name = Column(String, nullable=False)
     description = Column(String, nullable=True)
+
+    # Follows the enforcement of token for any person, for all the
+    # task. This enforcements add up to the ones defined task-wise.
+    # T_initial is the initial number, T_max is the maximum number in
+    # any given time (or -1 to ignore), T_total is the maximum number
+    # that can be used in the whole contest (or -1), T_min_interval
+    # the minimum interval in seconds between to uses of a token,
+    # Every T_gen_time from the beginning of the contest we generate
+    # T_gen_number tokens.
     token_initial = Column(Integer, nullable=False)
     token_max = Column(Integer, nullable=False)
     token_total = Column(Integer, nullable=False)
-    token_min_interval = Column(Float, nullable=False)
-    token_gen_time = Column(Float, nullable=False)
+    token_min_interval = Column(Integer, nullable=False)
+    token_gen_time = Column(Integer, nullable=False)
+    token_gen_number = Column(Integer, nullable=False)
+
+    # Beginning and ending of the contest, unix times.
     start = Column(Integer, nullable=True)
     stop = Column(Integer, nullable=True)
 
-    #tasks (backref)
-    #announcements (skipped for now)
-    #ranking_view (backref)
-    #tasks (backref)
-    #users (backref)
-    #get_submissions (in SQLAlchemyAll)
+    # Follows the description of the fields automatically added by
+    # SQLAlchemy.
+    # tasks (list of Task objects)
+    # announcements (list of Announcement objects)
+    # ranking_view (RankingView object)
+    # users (list of User objects)
+
+    # Moreover, we have the following method.
+    # get_submissions (defined in SQLAlchemyAll)
 
     def __init__(self, name, description, tasks, users,
                  token_initial, token_max, token_total,
-                 token_min_interval, token_gen_time,
-                 start = None, stop = None, announcements = [],
-                 ranking_view = None):
+                 token_min_interval,
+                 token_gen_time, token_gen_number,
+                 start=None, stop=None, announcements=None,
+                 ranking_view=None):
         self.name = name
         self.description = description
         self.tasks = tasks
@@ -59,9 +90,12 @@ class Contest(Base):
         self.token_total = token_total
         self.token_min_interval = token_min_interval
         self.token_gen_time = token_gen_time
+        self.token_gen_number = token_gen_number
         self.start = start
         self.stop = stop
         self.announcements = announcements
+        if self.announcements == None:
+            self.announcement = []
         self.ranking_view = ranking_view
 
     #def update_ranking_view(self):
@@ -94,38 +128,32 @@ class Contest(Base):
     #            return i
     #    raise KeyError("Task not found")
 
+
 class Announcement(Base):
+    """Class to store a messages sent by the contest managers to all
+    the users. Not to be used directly (import it from SQLAlchemyAll).
+
+    """
     __tablename__ = 'announcements'
 
+    # Auto increment primary key.
     id = Column(Integer, primary_key=True)
-    contest_id = Column(Integer, ForeignKey(Contest.id, onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
+
+    # Contest for which the announcements are.
+    contest_id = Column(Integer,
+                        ForeignKey(Contest.id,
+                                   onupdate="CASCADE",
+                                   ondelete="CASCADE"),
+                        nullable=False)
+    contest = relationship(Contest, backref=backref('announcements'))
+
+    # Time, subject and text of the announcements.
     timestamp = Column(Integer, nullable=False)
     subject = Column(String, nullable=False)
     text = Column(String, nullable=False)
-
-    contest = relationship(Contest, backref=backref('announcements'))
 
     def __init__(self, timestamp, subject, text, contest=None):
         self.timestamp = timestamp
         self.subject = subject
         self.text = text
         self.contest = contest
-
-def sample_contest():
-    c = Contest("hello", "Hello world",
-                #[Task.sample_task() for i in range(3)],
-                #[User.sample_user() for i in range(10)],
-                [], [],
-                10, 3, 3, 30, 60)
-    #s = Submission.sample_submission()
-    #c.submissions.append(s)
-    # These to_couch() calls should never fail, because they act on
-    # freshly created document
-    #c.to_couch()
-    #s.task = c.tasks[0]
-    #s.user = c.users[0]
-    #s.to_couch()
-    #u = c.users[0]
-    #u.username = "username"
-    #u.to_couch()
-    return c
