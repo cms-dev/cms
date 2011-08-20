596bf97752b99aabd043964b11fa706de4fd217d
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2011-08-20 16:30:23 +0200

    SpoolExporter more or less working.

--- cms/contrib/SpoolExporter.py
+++ cms/contrib/SpoolExporter.py
@@ -22,88 +22,135 @@
 import sys
 import os
 import codecs
+import optparse
 
-import CouchObject
-import Utils
-import FileStorageLib
+from cms.async.AsyncLibrary import Service, logger
+from cms.async import ServiceCoord
+from cms.service.FileStorage import FileCacher
+
+from cms.db.SQLAlchemyAll import SessionGen, Contest
+from cms.db.Utils import ask_for_contest
+
+class SpoolExporter(Service):
+
+    def __init__(self, shard, contest_id, spool_dir):
+        self.contest_id = contest_id
+        self.spool_dir = spool_dir
+
+        logger.initialize(ServiceCoord("SpoolExporter", shard))
+        logger.debug("SpoolExporter.__init__")
+        Service.__init__(self, shard)
+        self.FS = self.connect_to(
+            ServiceCoord("FileStorage", 0))
+        if not self.FS.connected:
+            logger.error("Please run the FileStorage service.")
+            self.exit()
+        self.FC = FileCacher(self, self.FS)
+
+
+    def do_export(self):
+        logger.operation = "exporting contest %d" % (self.contest_id)
+        logger.info("Creating dir structure")
+        try:
+            os.mkdir(self.spool_dir)
+        except OSError:
+            logger.error("The specified directory already exists, I won't overwrite it")
+            sys.exit(1)
+        queue_file = codecs.open(os.path.join(self.spool_dir, "queue"), 'w', encoding='utf-8')
+        upload_dir = os.path.join(self.spool_dir, "upload")
+        os.mkdir(upload_dir)
+
+        with SessionGen() as session:
+
+            c = Contest.get_from_id(self.contest_id, session)
+
+            hidden_users = []
+            for user in c.users:
+                if not user.hidden:
+                    user_dir = os.path.join(upload_dir, user.username)
+                    os.mkdir(user_dir)
+                else:
+                    hidden_users.append(user.username)
+
+            for submission in c.get_submissions(session):
+                if submission.user.hidden:
+                    continue
+                logger.info("Exporting submission %s" % (submission.id))
+                username = submission.user.username
+                task = submission.task.name
+                timestamp = int(submission.timestamp)
+                checked, language = submission.verify_source(session)
+                if not checked:
+                    logger.warning("Could not verify language for submission %s, dropping it" % (submission.id))
+                    continue
+                user_dir = os.path.join(upload_dir, username)
+
+                file_digest = submission.files["%s.%s" % (task, language)].digest
+                upload_filename = os.path.join(user_dir, "%s.%d.%s" % (task, timestamp, language))
+                self.FC.get_file_to_path(file_digest, upload_filename, sync=True)
+                upload_filename = os.path.join(user_dir, "%s.%s" % (task, language))
+                self.FC.get_file_to_path(file_digest, upload_filename, sync=True)
+                print >> queue_file, "./upload/%s/%s.%d.%s" % (username, task, timestamp, language)
+
+                if submission.evaluations != []:
+                    res_file = codecs.open(os.path.join(self.spool_dir, "%d.%s.%s.%s.res" % (timestamp, username, task, language)), 'w', encoding='utf-8')
+                    res2_file = codecs.open(os.path.join(self.spool_dir, "%s.%s.%s.res" % (username, task, language)), 'w', encoding='utf-8')
+                    total = 0.0
+                    for num, evaluation in enumerate(submission.evaluations):
+                        outcome = float(evaluation.outcome)
+                        total += outcome
+                        line = "Executing on file n. %2d %s (%.4f)" % (num, evaluation.text, outcome)
+                        print >> res_file, line
+                        print >> res2_file, line
+                    line = "Score: %.6f" % (total)
+                    print >> res_file, line
+                    print >> res2_file, line
+                    res_file.close()
+                    res2_file.close()
+
+            print >> queue_file
+            queue_file.close()
+
+            logger.info("Exporting ranking")
+            ranking_file = codecs.open(os.path.join(self.spool_dir, "classifica.txt"), 'w', encoding='utf-8')
+            ranking_csv = codecs.open(os.path.join(self.spool_dir, "classifica.csv"), 'w', encoding='utf-8')
+            print >> ranking_file, "Classifica finale del contest `%s'" % (c.description)
+            #users = [(sum(c.ranking_view.scores[u]), u, c.ranking_view.scores[u]) for u in c.ranking_view.scores.keys() if u not in hidden_users]
+            users = {}
+            for u in c.users:
+                if u.username not in hidden_users:
+                    users[u.username] = [0, u.username, [None]*len(c.tasks)]
+            for (username, task_num), score in c.ranking_view.scores.iteritems():
+                if username not in hidden_users:
+                    users[username][0] += score.score
+                    users[username][2][task_num] = score.score
+            users = users.values()
+            users.sort(reverse=True)
+            print >> ranking_file, ("%20s %10s" + " %10s" * len(c.tasks)) % (("Utente", "Totale") + tuple([t.name for t in c.tasks]))
+            print >> ranking_csv, ("%s,%s" + ",%s" * len(c.tasks)) % (("utente", "totale") + tuple([t.name for t in c.tasks]))
+            for total, user, problems in users:
+                print >> ranking_file, ("%20s %10.3f" + " %10.3f" * len(c.tasks)) % ((user, total) + tuple(problems))
+                print >> ranking_csv, ("%s,%.6f" + ",%.6f" * len(c.tasks)) % ((user, total) + tuple(problems))
+            ranking_file.close()
+            ranking_csv.close()
 
 def main():
-    if len(sys.argv) < 2:
-        print "Use: %s dir [contest]"
-        sys.exit(1)
-    c = Utils.ask_for_contest(skip = 1)
-    spool_dir = sys.argv[1]
-    try:
-        os.mkdir(spool_dir)
-    except OSError:
-        print "The specified directory already exists, I won't overwrite it"
-        sys.exit(1)
-    queue_file = codecs.open(os.path.join(spool_dir, "queue"), 'w', encoding='utf-8')
-    upload_dir = os.path.join(spool_dir, "upload")
-    os.mkdir(upload_dir)
+    parser = optparse.OptionParser(usage="usage: %prog [options] contest_dir")
+    parser.add_option("-c", "--contest", help="contest ID to export",
+                      dest="contest_id", action="store", type="int", default=None)
+    parser.add_option("-s", "--shard", help="service shard number",
+                      dest="shard", action="store", type="int", default=None)
+    options, args = parser.parse_args()
+    if len(args) != 1:
+        parser.error("I need exactly one parameter, the directory where to export the contest")
+    if options.shard is None:
+        parser.error("The `-s' option is mandatory!")
 
-    fsl = FileStorageLib.FileStorageLib()
+    if options.contest_id is None:
+        options.contest_id = ask_for_contest()
 
-    hidden_users = []
-    for user in c.users:
-        if not user.hidden:
-            user_dir = os.path.join(upload_dir, user.username)
-            os.mkdir(user_dir)
-        else:
-            hidden_users.append(user.username)
-
-    for submission in c.submissions:
-        if submission.user.hidden:
-            continue
-        print "Exporting submission %s" % (submission.couch_id)
-        username = submission.user.username
-        task = submission.task.name
-        timestamp = int(submission.timestamp)
-        checked, language = submission.verify_source()
-        if not checked:
-            print "Could not verify language for submission %s, dropping it" % (submission.couch_id)
-            continue
-        user_dir = os.path.join(upload_dir, username)
-
-        file_digest = submission.files["%s.%s" % (task, language)]
-        upload_filename = os.path.join(user_dir, "%s.%d.%s" % (task, timestamp, language))
-        fsl.get(file_digest, upload_filename)
-        upload_filename = os.path.join(user_dir, "%s.%s" % (task, language))
-        fsl.get(file_digest, upload_filename)
-        print >> queue_file, "./upload/%s/%s.%d.%s" % (username, task, timestamp, language)
-
-        if submission.evaluation_outcome != None:
-            res_file = codecs.open(os.path.join(spool_dir, "%d.%s.%s.%s.res" % (timestamp, username, task, language)), 'w', encoding='utf-8')
-            res2_file = codecs.open(os.path.join(spool_dir, "%s.%s.%s.res" % (username, task, language)), 'w', encoding='utf-8')
-            total = 0.0
-            for num in xrange(len(submission.evaluation_outcome)):
-                total += submission.evaluation_outcome[num]
-                line = "Executing on file n. %2d %s (%.4f)" % (num, submission.evaluation_text[num],
-                                                               submission.evaluation_outcome[num])
-                print >> res_file, line
-                print >> res2_file, line
-            line = "Score: %.6f" % (total)
-            print >> res_file, line
-            print >> res2_file, line
-            res_file.close()
-            res2_file.close()
-
-    print >> queue_file
-    queue_file.close()
-
-    print "Exporting ranking"
-    ranking_file = codecs.open(os.path.join(spool_dir, "classifica.txt"), 'w', encoding='utf-8')
-    ranking_csv = codecs.open(os.path.join(spool_dir, "classifica.csv"), 'w', encoding='utf-8')
-    print >> ranking_file, "Classifica finale del contest `%s'" % (c.description)
-    users = [(sum(c.ranking_view.scores[u]), u, c.ranking_view.scores[u]) for u in c.ranking_view.scores.keys() if u not in hidden_users]
-    users.sort(reverse = True)
-    print >> ranking_file, ("%20s %10s" + " %10s" * len(c.tasks)) % (("Utente", "Totale") + tuple([t.name for t in c.tasks]))
-    print >> ranking_csv, ("%s,%s" + ",%s" * len(c.tasks)) % (("utente", "totale") + tuple([t.name for t in c.tasks]))
-    for total, user, problems in users:
-        print >> ranking_file, ("%20s %10.3f" + " %10.3f" * len(c.tasks)) % ((user, total) + tuple(problems))
-        print >> ranking_csv, ("%s,%.6f" + ",%.6f" * len(c.tasks)) % ((user, total) + tuple(problems))
-    ranking_file.close()
-    ranking_csv.close()
+    spool_exporter = SpoolExporter(shard=options.shard, contest_id=options.contest_id, spool_dir=args[0])
+    spool_exporter.do_export()
 
 if __name__ == "__main__":
     main()
