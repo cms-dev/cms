ba1f26a4fbfcfb8da3594f1a2298f936f08839f5
Stefano Maggiolo <s.maggiolo@gmail.com>
2011-08-22 15:28:05 +0200

    Temporary handling the removal of "tokens" from "User".

--- cms/cms/server/BusinessLayer.py
+++ cms/cms/server/BusinessLayer.py
@@ -112,6 +112,9 @@ def token_available(contest, user, task, timestamp):
     """
     Returns True if the given user can use a token for the given task.
     """
+    # TODO: since db changed, we have to fix this.
+    return False
+
     tokens_timestamp = [s.token_timestamp
                         for s in user.tokens]
     task_tokens_timestamp = [s.token_timestamp
