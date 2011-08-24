7a14e07a3e1438925c21ea342f1e66baf9f2b935
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2011-08-24 11:42:14 +0200

    Allowing waitpid when compiling (seems to be required by gcc-4.5).

--- cms/cms/service/TaskType.py
+++ cms/cms/service/TaskType.py
@@ -225,6 +225,7 @@ class BatchTaskType:
         self.sandbox.chdir = self.sandbox.path
         self.sandbox.preserve_env = True
         self.sandbox.filter_syscalls = 1
+        self.sandbox.allow_syscall = ["waitpid"]
         self.sandbox.allow_fork = True
         self.sandbox.file_check = 2
         # FIXME - File access limits are not enforced on children
