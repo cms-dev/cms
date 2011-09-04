062671a0d71214a401236946cc2bfe142e30f1b9
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2011-09-04 17:52:09 +0200

    Implement randomized actors and write some documentation.

--- cms/stresstesting/Irene.py
+++ cms/stresstesting/Irene.py
@@ -26,28 +26,43 @@ import urllib2
 import cookielib
 import threading
 import optparse
+import random
+import time
 
 from cms.db.SQLAlchemyAll import Contest, SessionGen
 
 
 class HTTPHelper:
+    """A class to emulate a browser's behaviour: for example, cookies
+    get automatically accepted, stored and sent with subsequent
+    requests.
+
+    """
 
     def __init__(self):
         self.cookies = cookielib.CookieJar()
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookies))
 
     def do_request(self, url, data=None):
+        """Request the specified URL.
+
+        url (string): the URL to request; the protocol is detected
+        from the URL.
+        data (dict): the data to sent with the URL; used when an
+                     HTTP(S) request is performed: if data is None, a
+                     plain GET request is performed. Otherwise a POST
+                     request is performed, with the attached data.
+        returns: the response; it is a file-like objects that the
+                 consumer can read; is also supports other methods,
+                 described in the documentation of urllib2.urlopen().
+
+        """
         if data is None:
             request = urllib2.Request(url)
         else:
-            request = urllib2.Request(url, self.encode_post_data(data))
-        #self.cookies.add_cookie_header(request)
+            request = urllib2.Request(url, urllib.urlencode(data))
         response = self.opener.open(request)
-        #self.cookies.extract_cookies(response, request)
         return response
-
-    def encode_post_data(self, data):
-        return urllib.urlencode(data)
 
 class TestRequest:
 
@@ -56,13 +71,13 @@ class TestRequest:
         self.base_url = base_url
 
     def execute(self):
-        print >> sys.stderr, "Cookies before execution:"
-        for cookie in self.http_helper.cookies:
-            print >> sys.stderr, "  " + repr(cookie)
+        #print >> sys.stderr, "Cookies before execution:"
+        #for cookie in self.http_helper.cookies:
+        #    print >> sys.stderr, "  " + repr(cookie)
         self.do_request()
-        print >> sys.stderr, "Cookies after execution:"
-        for cookie in self.http_helper.cookies:
-            print >> sys.stderr, "  " + repr(cookie)
+        #print >> sys.stderr, "Cookies after execution:"
+        #for cookie in self.http_helper.cookies:
+        #    print >> sys.stderr, "  " + repr(cookie)
         fail = self.test_fail()
         success = self.test_success()
         description = self.describe()
@@ -138,7 +153,18 @@ class LoginRequest(TestRequest):
             return True
         return False
 
+class ActorDying(Exception):
+    pass
+
 class Actor(threading.Thread):
+    """Class that simulates the behaviour of a user of the system. It
+    performs some requests at randomized times (checking CMS pages,
+    doing submissions, ...), checking for their success or failure.
+
+    The probability that the users doing actions depends on the value
+    specified in an object called "metrics".
+
+    """
 
     def __init__(self, username, password, metrics):
         threading.Thread.__init__(self)
@@ -147,14 +173,34 @@ class Actor(threading.Thread):
         self.password = password
         self.metric = metrics
 
+        self.name = "Actor thread for user %s" % (self.username)
+
         self.http_helper = HTTPHelper()
+        self.die = False
 
     def run(self):
-        print >> sys.stderr, "Starting actor for user %s" % (self.username)
-        request = LoginRequest(self.http_helper, self.username, self.password)
+        try:
+            print >> sys.stderr, "Starting actor for user %s" % (self.username)
+            self.do_step(LoginRequest(self.http_helper, self.username, self.password))
+            self.do_step(HomepageRequest(self.http_helper))
+        except ActorDying:
+            print >> sys.stderr, "Actor dying for user %s" % (self.username)
+
+    def do_step(self, request):
+        self.wait_next()
+        if self.die:
+            raise ActorDying()
         request.execute()
-        request = HomepageRequest(self.http_helper)
-        request.execute()
+
+    def wait_next(self):
+        """Wait some time. At the moment it waits c*X seconds, where c
+        is the time_coeff parameter in metrics and X is an
+        exponentially distributed random variable, with parameter
+        time_lambda in metrics.
+
+        """
+        time_to_wait = self.metric['time_coeff'] * random.expovariate(self.metric['time_lambda'])
+        time.sleep(time_to_wait)
 
 def harvest_user_data(contest_id):
     users = {}
@@ -164,15 +210,23 @@ def harvest_user_data(contest_id):
             users[u.username] = {'password': u.password}
     return users
 
-DEFAULT_METRICS = {}
+DEFAULT_METRICS = {'time_coeff':  1.0,
+                   'time_lambda': 0.5}
 
 def main():
     parser = optparse.OptionParser(usage="usage: %prog [options]")
     parser.add_option("-c", "--contest", help="contest ID to export",
                       dest="contest_id", action="store", type="int", default=None)
+    parser.add_option("-n", "--actor-num", help="the number of actors to spawn",
+                      dest="actor_num", action="store", type="int", default=None)
     options, args = parser.parse_args()
 
-    users = dict(harvest_user_data(options.contest_id).items()[:])
+    if options.actor_num is None:
+        users = harvest_user_data(options.contest_id)
+    else:
+        user_items = harvest_user_data(options.contest_id).items()
+        random.shuffle(user_items)
+        users = dict(user_items[:options.actor_num])
 
     actors = [Actor(username, data['password'], DEFAULT_METRICS) for username, data in users.iteritems()]
     for a in actors:
