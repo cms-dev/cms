bf27d2bc73e61602234cdfea77c5cf2563b504c1
Luca Wehrstedt <lerks@users.sourceforge.net>
2012-07-15 13:40:58 +0200

    Fix some issues with the unread messages indicator

--- cms/server/static/script_new.js
+++ cms/server/static/script_new.js
@@ -59,7 +59,7 @@ var Utils = new function () {
         if (counter > 0) {
             self.unread_count += counter;
             $("#unread_count").text(self.unread_count + " unread");
-            $("#unread_count").css("display", "inline");
+            $("#unread_count").removeClass("no_unread");
         }
     };
 
