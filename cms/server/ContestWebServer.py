cms-dev/cms

commit c7eb5bb86b2d1ea2e964f1794f597cf5863e0b68

Add better messages to explain token rules in CWS
Distinguish some cases (like "no tokens", "infinite tokens", "only
contest-tokens", "only task-tokens", "both contest- and task-tokens")
and show the appropriate messages (as simple as possible).

cms/server/ContestWebServer.py
@@ -139,6 +139,22 @@ class BaseHandler(CommonRequestHandler):
 
         return user
 
+    @staticmethod
+    def _get_token_status (obj):
+        """Return the status of the tokens for the given object.
+
+        obj (Contest or Task): an object that has the token_* attributes.
+        return (int): one of 0 (disabled), 1 (enabled/finite) and 2
+                      (enabled/infinite).
+
+        """
+        if obj.token_initial is None:
+            return 0
+        elif obj.token_gen_time == 0 and obj.token_gen_number > 0:
+            return 2
+        else:
+            return 1
+
     def render_params(self):
         """Return the default render params used by almost all handlers.
 
@@ -167,6 +183,23 @@ class BaseHandler(CommonRequestHandler):
                     ret["valid_phase_end"] = user_end_time
         ret["contest_list"] = self.sql_session.query(Contest).all()
         ret["cookie"] = str(self.cookies)
+
+        # some information about token configuration
+        ret["tokens_contest"] = self._get_token_status(self.contest)
+        if ret["tokens_contest"] == 2 and self.contest.token_min_interval == 0:
+            ret["tokens_contest"] = 3  # infinite and no min_interval
+
+        t_tokens = sum(self._get_token_status(t) for t in self.contest.tasks)
+        if t_tokens == 0:
+            ret["tokens_tasks"] = 0  # all disabled
+        elif t_tokens == 2 * len(self.contest.tasks):
+            ret["tokens_tasks"] = 2  # all infinite
+        else:
+            ret["tokens_tasks"] = 1  # all finite or mixed
+        if ret["tokens_tasks"] == 2 and \
+            not any(t.token_min_interval for t in self.contest.tasks):
+            ret["tokens_tasks"] = 3  # all infinite and no min_intervals
+
         return ret
 
     def finish(self, *args, **kwds):

cms/server/__init__.py
@@ -203,11 +203,15 @@ def format_amount_of_time(seconds):
     return ", ".join(ret)
 
 
-def format_token_rules (tokens, locale=None):
+def format_token_rules (tokens, t_type=None, locale=None):
     """Return a human-readable string describing the given token rules
 
     tokens (dict): all the token rules (as seen in Task or Contest),
                    without the "token_" prefix.
+    t_type (str): the type of tokens the string should refer to (can be
+                  "contest" to mean contest-tokens, "task" to mean
+                  task-tokens, any other value to mean normal tokens).
+    locale (tornado.locale.Locale): the locale to be used.
 
     return (string): localized string describing the rules.
 
@@ -215,73 +219,93 @@ def format_token_rules (tokens, locale=None):
     if locale is None:
         locale = tornado.locale.get()
 
+    if t_type == "contest":
+        tokens["type_none"] = locale.translate("no contest-tokens")
+        tokens["type_s"] = locale.translate("contest-token")
+        tokens["type_pl"] = locale.translate("contest-tokens")
+    elif t_type == "task":
+        tokens["type_none"] = locale.translate("no task-tokens")
+        tokens["type_s"] = locale.translate("task-token")
+        tokens["type_pl"] = locale.translate("task-tokens")
+    else:
+        tokens["type_none"] = locale.translate("no tokens")
+        tokens["type_s"] = locale.translate("token")
+        tokens["type_pl"] = locale.translate("tokens")
+
     result = ""
 
     if tokens['initial'] is None:
-        result += "Tokens disabled."
+        # note: we are sure that this text will only be displayed in task
+        # pages because if tokens are disabled for the whole contest they
+        # don't appear anywhere in CWS
+        result += locale.translate("You don't have %(type_pl)s available for this task.") % tokens
+    elif tokens['gen_time'] == 0 and tokens['gen_number'] > 0:
+        result += locale.translate("You have infinite %(type_pl)s.") % tokens
+
+        result += " "
+
+        if tokens['min_interval'] > 0:
+            if tokens['min_interval'] == 1:
+                result += locale.translate("You can use a %(type_s)s every second.") % tokens
+            else:
+                result += locale.translate("You can use a %(type_s)s every %(min_interval)d seconds.") % tokens
+        else:
+            result += locale.translate("You have no limitations on how you use them.") % tokens
     else:
         if tokens['initial'] == 0:
-            result += locale.translate("You start with no tokens.")
+            result += locale.translate("You start with %(type_none)s.") % tokens
         elif tokens['initial'] == 1:
-            result += locale.translate("You start with one token.")
+            result += locale.translate("You start with one %(type_s)s.") % tokens
         else:
-            result += locale.translate("You start with %(initial)d tokens.") % tokens
+            result += locale.translate("You start with %(initial)d %(type_pl)s.") % tokens
 
         result += " "
 
-        if tokens['gen_time'] is not None and tokens['gen_number'] is not None:
+        if tokens['gen_time'] > 0 and tokens['gen_number'] > 0:
+            if tokens['gen_time'] == 1:
+                result += locale.translate("Every minute ") % tokens
+            else:
+                result += locale.translate("Every %(gen_time)d minutes ") % tokens
             if tokens['max'] is not None:
-                if tokens['gen_time'] == 1 and tokens['gen_number'] == 1 and tokens['max'] == 1:
-                    result += locale.translate("Every minute you get another token, up to one token in total.")
-                elif tokens['gen_time'] == 1 and tokens['gen_number'] == 1:
-                    result += locale.translate("Every minute you get another token, up to %(max)d tokens in total.") % tokens
-                elif tokens['gen_time'] == 1 and tokens['max'] == 1:
-                    result += locale.translate("Every %(gen_time)d minutes you get another token, up to one token in total.") % tokens
-                elif tokens['gen_time'] == 1:
-                    result += locale.translate("Every %(gen_time)d minutes you get another token, up to %(max)d tokens in total.") % tokens
-                elif tokens['gen_number'] == 1 and tokens['max'] == 1:
-                    result += locale.translate("Every minute you get %(gen_number)d other tokens, up to one token in total.") % tokens
-                elif tokens['gen_number'] == 1:
-                    result += locale.translate("Every minute you get %(gen_number)d other tokens, up to %(max)d tokens in total.") % tokens
-                elif tokens['max'] == 1:
-                    result += locale.translate("Every %(gen_time)s minutes you get %(gen_number)d other tokens, up to one token in total.") % tokens
+                if tokens['gen_number'] == 1:
+                    result += locale.translate("you get another %(type_s)s, ") % tokens
                 else:
-                    result += locale.translate("Every %(gen_time)s minutes you get %(gen_number)d other tokens, up to %(max)d tokens in total.") % tokens
+                    result += locale.translate("you get %(gen_number)d other %(type_pl)s, ") % tokens
+                if tokens['max'] == 1:
+                    result += locale.translate("up to a maximum of one %(type_s)s.") % tokens
+                else:
+                    result += locale.translate("up to a maximum of %(max)d %(type_pl)s.") % tokens
             else:
-                if tokens['gen_time'] == 1 and tokens['gen_number'] == 1:
-                    result += locale.translate("Every minute you get another token.")
-                elif tokens['gen_time'] == 1:
-                    result += locale.translate("Every %(gen_time)d minutes you get another token.") % tokens
-                elif tokens['gen_number'] == 1:
-                    result += locale.translate("Every minute you get %(gen_number)d other tokens.") % tokens
+                if tokens['gen_number'] == 1:
+                    result += locale.translate("you get another %(type_s)s.") % tokens
                 else:
-                    result += locale.translate("Every %(gen_time)d minutes you get %(gen_number)d other tokens.") % tokens
+                    result += locale.translate("you get %(gen_number)d other %(type_pl)s.") % tokens
         else:
-            result += locale.translate("You don't get other tokens.") % tokens
+            result += locale.translate("You don't get other %(type_pl)s.") % tokens
 
         result += " "
 
-        if tokens['min_interval'] is not None and tokens['total'] is not None:
-            if tokens['min_interval'] == 1 and tokens['total'] == 1:
-                result += locale.translate("You can play a token every second and no more than one token in total.")
-            elif tokens['min_interval'] == 1:
-                result += locale.translate("You can play a token every second and no more than %(total)d tokens in total.") % tokens
-            elif tokens['total'] == 1:
-                result += locale.translate("You can play a token every %(min_interval)d seconds and no more than one token in total.") % tokens
-            else:
-                result += locale.translate("You can play a token every %(min_interval)d seconds and no more than %(total)d tokens in total.") % tokens
-        elif tokens['min_interval'] is not None:
+        if tokens['min_interval'] > 0 and tokens['total'] is not None:
             if tokens['min_interval'] == 1:
-                result += locale.translate("You can play a token every second.")
+                result += locale.translate("You can use a %(type_s)s every second ") % tokens
             else:
-                result += locale.translate("You can play a token every %(min_interval)d seconds.") % tokens
+                result += locale.translate("You can use a %(type_s)s every %(min_interval)d seconds ") % tokens
+            if tokens['total'] == 1:
+                result += locale.translate("and no more than one %(type_s)s in total.") % tokens
+            else:
+                result += locale.translate("and no more than %(total)d %(type_pl)s in total.") % tokens
+        elif tokens['min_interval'] > 0:
+            if tokens['min_interval'] == 1:
+                result += locale.translate("You can use a %(type_s)s every second.") % tokens
+            else:
+                result += locale.translate("You can use a %(type_s)s every %(min_interval)d seconds.") % tokens
         elif tokens['total'] is not None:
             if tokens['total'] == 1:
-                result += locale.translate("You can play no more than one token in total.")
+                result += locale.translate("You can use no more than one %(type_s)s in total.") % tokens
             else:
-                result += locale.translate("You can play no more than %(total)d tokens in total.") % tokens
+                result += locale.translate("You can use no more than %(total)d %(type_pl)s in total.") % tokens
         else:
-            result += locale.translate("You have no limitations on how you play them.")
+            result += locale.translate("You have no limitations on how you use them.") % tokens
 
     return result
 

cms/server/static/style_new.css
@@ -315,3 +315,9 @@ div.question_submit textarea {
     display: none;
 }
 
+/*** Token rules */
+
+td.token_rules p:last-child {
+    margin-bottom: 0;
+}
+

cms/server/templates/contest/overview.html
@@ -18,22 +18,93 @@
     <div class="span9">
 {% end %}
         <p>
-    {% if phase == -1 %}
+{% if phase == -1 %}
         {{ _("The contest will start at %(start_time)s and will end at %(stop_time)s.") % {"start_time": format_time_or_date(contest.start), "stop_time": format_time_or_date(contest.stop)} }}
-    {% elif phase == 0 %}
+{% elif phase == 0 %}
         {{ _("The contest started at %(start_time)s and will end at %(stop_time)s.") % {"start_time": format_time_or_date(contest.start), "stop_time": format_time_or_date(contest.stop)} }}
-    {% elif phase == -1 %}
+{% elif phase == -1 %}
         {{ _("The contest started at %(start_time)s and ended at %(stop_time)s.") % {"start_time": format_time_or_date(contest.start), "stop_time": format_time_or_date(contest.stop)} }}
-    {% end %}
+{% end %}
         </p>
 
-    {% if contest.per_user_time is not None %}
+{% if contest.per_user_time is not None %}
         <p>
         {{ _("Every user is allowed to compete (i.e. submit solutions) for a (contiguous/consecutive/continuous/uninterrupted) time frame of %(per_user_time)s.") % {"per_user_time": format_amount_of_time(contest.per_user_time)} }}
         </p>
-    {% end %}
+{% end %}
 
-        {% comment TODO something about tokens... %}
+{% if tokens_contest != 0 and tokens_tasks != 0 %}
+    {% if tokens_contest >= 2 and tokens_tasks >= 2 %}
+        <p>
+        {{ _("You have an infinite set of tokens.") }}
+        {{ _("You can use one of these tokens to see the detailed result of a submission.") }}
+        {{ _("Your score for each task will be the maximum among the tokened submissions and the last one.") }}
+        </p>
+
+        <p>
+        {% if tokens_contest == 2 %}
+            {% if contest.token_min_interval == 1 %}
+        {{ _("You can use at most one token every second among all tasks.") }}
+            {% else %}
+        {{ _("You can use at most one token every %(min_interval)d seconds among all tasks.") % {"min_interval": contest.token_min_interval} }}
+            {% end %}
+            {% if tokens_tasks == 2 %}
+        {{ _("Tasks may pose further limitations on how often you can use tokens.") }}
+        {{ _("See the description page of each task for more information.") }}
+            {% end %}
+        {% elif tokens_tasks == 2 %}
+        {{ _("Tasks may pose some limitations on how often you can use tokens.") }}
+        {{ _("See the description page of each task for more information.") }}
+        {% end %}
+        </p>
+    {% elif tokens_contest >= 2 %}
+        <p>
+        {{ _("You have a distinct set of tokens for each task.") }}
+        {{ _("You can use one of these tokens to see the detailed result of a submission.") }}
+        {{ _("Your score for each task will be the maximum among the tokened submissions and the last one.") }}
+        </p>
+
+        <p>
+        {{ _("You can find the rules for the tokens on the description page of each task.") }}
+        {% if tokens_contest == 2 %}
+            {% if contest.token_min_interval == 1 %}
+        {{ _("In addition to those rules, you can use at most one token every second among all tasks.") }}
+            {% else %}
+        {{ _("In addition to those rules, you can use at most one token every %(min_interval)d seconds among all tasks.") % {"min_interval": contest.token_min_interval} }}
+            {% end %}
+        {% end %}
+        </p>
+    {% elif tokens_tasks >= 2 %}
+        <p>
+        {{ _("You have a set of tokens shared among all tasks.") }}
+        {{ _("You can use one of these tokens to see the detailed result of a submission.") }}
+        {{ _("Your score for each task will be the maximum among the tokened submissions and the last one.") }}
+        </p>
+
+        <p>
+        {% set tokens = {"initial": contest.token_initial, "max": contest.token_max, "total": contest.token_total, "min_interval": contest.token_min_interval, "gen_time": contest.token_gen_time, "gen_number": contest.token_gen_number} %}
+        {{ format_token_rules(tokens) }}
+        {% if tokens_tasks == 2 %}
+            {{ _("In addition to these rules, tasks may pose some limitations on how often you can use tokens.") }}
+            {{ _("See the description page of each task for more information.") }}
+        {% end %}
+        </p>
+    {% else %}
+        <p>
+        {% raw _("You have two types of tokens: a set of <em>contest-tokens</em> shared among all tasks and a distinct set of <em>task-tokens</em> for each task.") %}
+        {{ _("You can see the detailed result of a submission by using two tokens on it, one of each type.") }}
+        {{ _("Your score for each task will be the maximum among the tokened submissions and the last one.") }}
+        </p>
+
+        <p>
+        {% set tokens = {"initial": contest.token_initial, "max": contest.token_max, "total": contest.token_total, "min_interval": contest.token_min_interval, "gen_time": contest.token_gen_time, "gen_number": contest.token_gen_number} %}
+        {{ format_token_rules(tokens, t_type="contest") }}
+        You can find the rules for the task-tokens on each task's description page.
+        </p>
+    {% end %}
+{% end %}
+
+
 
     </div>
 {% if contest.per_user_time is not None %}
@@ -66,7 +137,7 @@
             <th>{{ _("Testcases") }}</th>
             <th>{{ _("Type") }}</th>
             <th>{{ _("Files") }}</th>
-{% if contest.token_initial is not None %}
+{% if tokens_contest != 0 and tokens_tasks != 0 %}
             <th>{{ _("Tokens") }}</th>
 {% end %}
         </tr>

cms/server/templates/contest/task_description.html
@@ -85,19 +85,93 @@
     {% end %}
 </tr>
 {% end %}
-{% if task.contest.token_initial is not None %}
-        <tr>
-            <th>{{ _("Tokens") }}</th>
-            <td colspan="2">
-    {% if task.token_initial is None %}
-        No
-    {% else %}
-        Yes -
+
+{% if tokens_contest != 0 and tokens_tasks != 0 %}
+<tr>
+    <th>{{ _("Tokens") }}</th>
+    <td colspan="2" class="token_rules">
+    {% if tokens_contest >= 2 and tokens_tasks >= 2 %}
+        <p>
+        {{ _("You have an infinite set of tokens.") }}
+        </p>
+
+        {% if tokens_contest == 2 or tokens_tasks == 2 %}
+        <p>
+            {% comment Note that if task_min_interval <= contest_min_interval it "doesn't count" %}
+            {% if contest.token_min_interval != 0 and task.token_min_interval != 0 and task.token_min_interval > contest.token_min_interval %}
+                {% if task.token_min_interval == 1 %}
+                    {{ _("You can use at most one token every second for this task ") }}
+                {% else %}
+                    {{ _("You can use at most one token every %(min_interval)d seconds for this task ") % {"min_interval": task.token_min_interval} }}
+                {% end %}
+                {% if contest.token_min_interval == 1 %}
+                    {{ _("and at most one token every second among all tasks.") }}
+                {% else %}
+                    {{ _("and at most one token every %(min_interval)d seconds among all tasks.") % {"min_interval": contest.token_min_interval} }}
+                {% end %}
+            {% elif contest.token_min_interval != 0 %}
+                {% if contest.token_min_interval == 1 %}
+                    {{ _("You can use at most one token every second among all tasks.") }}
+                {% else %}
+                    {{ _("You can use at most one token every %(min_interval)d seconds among all tasks.") % {"min_interval": contest.token_min_interval} }}
+                {% end %}
+            {% elif task.token_min_interval != 0 %}
+                {% if task.token_min_interval == 1 %}
+                    {{ _("You can use at most one token every second for this task.") }}
+                {% else %}
+                    {{ _("You can use at most one token every %(min_interval)d seconds for this task.") % {"min_interval": task.token_min_interval} }}
+                {% end %}
+            {% else %}
+                    {{ _("On this task you don't have additional rules.") }}
+            {% end %}
+        </p>
+        {% end %}
+    {% elif tokens_contest >= 2 %}
+        <p>
         {% set tokens = {"initial": task.token_initial, "max": task.token_max, "total": task.token_total, "min_interval": task.token_min_interval, "gen_time": task.token_gen_time, "gen_number": task.token_gen_number} %}
         {{ format_token_rules(tokens) }}
+        </p>
+
+        <p>
+        {% if tokens_contest == 2 %}
+            {% if contest.token_min_interval == 1 %}
+        {{ _("In addition to these rules, you can use at most one token every second among all tasks.") }}
+            {% else %}
+        {{ _("In addition to these rules, you can use at most one token every %(min_interval)d seconds among all tasks.") % {"min_interval": contest.token_min_interval} }}
+            {% end %}
+        {% end %}
+        </p>
+    {% elif tokens_tasks >= 2 %}
+        <p>
+        You can find the rules for the tokens on the <a href="{{ url_root }}">contest overview page</a>.
+        </p>
+
+        <p>
+        {% if tokens_tasks == 2 %}
+            {% if task.token_min_interval != 0 %}
+                {% if task.token_min_interval == 1 %}
+        {{ _("In addition to those rules, you can use at most one token every second on this task.") }}
+                {% else %}
+        {{ _("In addition to those rules, you can use at most one token every %(min_interval)d seconds on this task.") % {"min_interval": task.token_min_interval} }}
+                {% end %}
+            {% else %}
+        {{ _("On this task you don't have additional rules.") }}
+            {% end %}
+        {% end %}
+        </p>
+    {% else %}
+        <p>
+        {{ _("Remeber that to see the detailed result of a submission you need to use both a contest-token and a task-token.") }}
+        </p>
+
+        <p>
+        {% set tokens = {"initial": task.token_initial, "max": task.token_max, "total": task.token_total, "min_interval": task.token_min_interval, "gen_time": task.token_gen_time, "gen_number": task.token_gen_number} %}
+        {{ format_token_rules(tokens, t_type="task") }}
+        You can find the rules for the contest-tokens on the <a href="{{ url_root }}">contest overview page</a>.
+        </p>
     {% end %}
-            </td>
-        </tr>
+    </td>
+</tr>
 {% end %}
     </tbody>
 </table>

commit 11ae398774c99afdb3a0a277313d573697b1f23c

Modify the way CWS stores a local copy of the submissions
CWS was using pickle.dump to store a local copy of the submissions, to
recover them in case of failure. It was dumping the ID of the Contest,
the ID of the User and the Task object, along with all the submitted
files. Now CWS stores just the ID of the Task (instead of the whole
Task).

Storing the whole Task object makes pickle fail because of some circular
references among objects, i.e. task == task.contest.tasks[i] for some i.
Pickle should handle them correctly (in my opinion), yet some bugs are
present[1].

This bug has been introduced by commit c7eb5bb. It wasn't present before
because, thanks to the lazy-loading of SQLALchemy relationships[2], the
contest.tasks attribute was never requested and, thus, never loaded.
This prevented pickle from trying to dump it and falling into infinite
recursion.

Fixes issue #16 (see that issue for more details on this bug).

[1] http://bugs.python.org/issue9269 (note that mike bayer, aka zzzeek,
    commenting on this issue is the author of SQLAlchemy)
[2] http://docs.sqlalchemy.org/en/rel_0_7/orm/loading.html

cms/server/ContestWebServer.py
@@ -811,7 +811,7 @@ class SubmitHandler(BaseHandler):
                                  "w", "utf-8") as file_:
                     pickle.dump((self.contest.id,
                                  self.current_user.id,
-                                 self.task,
+                                 self.task.id,
                                  self.files), file_)
                 self.local_copy_saved = True
             except Exception as error: