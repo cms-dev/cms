cms-dev/cms

commit f268441a413cb180ca11be1d7f05da6a561328b5

Add more limits to the number and frequency of submissions and usertests
Allow to set a per-contest and per-task limit on the total number of
submissions and usertest sent by each user and on their frequency.

The per-contest limit applies to *all* submissions or usertests of the
entire contest, while the per-task limit applies only to submissions or
usertest for that task.

Note that the default per-task limit of a submission every 60 seconds is
no more. It can be added back using AWS.

cms/server/ContestWebServer.py
@@ -49,13 +49,16 @@ import gettext
 
 import tornado.web
 
+from sqlalchemy import func
+
 from cms import config, default_argument_parser, logger
 from cms.async.WebAsyncLibrary import WebService
 from cms.async import ServiceCoord
 from cms.db import ask_for_contest
 from cms.db.FileCacher import FileCacher
-from cms.db.SQLAlchemyAll import Session, Contest, User, Question, \
-     Submission, Token, File, UserTest, UserTestFile, UserTestManager
+from cms.db.SQLAlchemyAll import Session, Contest, User, Task, \
+     Question, Submission, Token, File, UserTest, UserTestFile, \
+     UserTestManager
 from cms.grading.tasktypes import get_task_type
 from cms.grading.scoretypes import get_score_type
 from cms.server import file_handler_gen, extract_archive, \
@@ -706,21 +709,73 @@ class SubmitHandler(BaseHandler):
         except KeyError:
             raise tornado.web.HTTPError(404)
 
-        # Enforce minimum time between submissions for the same task.
-        last_submission = self.sql_session.query(Submission)\
-            .filter(Submission.task == task)\
-            .filter(Submission.user == self.current_user)\
-            .order_by(Submission.timestamp.desc()).first()
-        if last_submission is not None and \
-               self.timestamp - last_submission.timestamp < \
-               timedelta(seconds=config.min_submission_interval):
+        # Alias for easy access
+        contest = self.contest
+
+        # Enforce maximum number of submissions
+        try:
+            if contest.max_submission_number is not None:
+                submission_c = self.sql_session.query(func.count(Submission.id))\
+                    .join(Submission.task)\
+                    .filter(Task.contest == contest)\
+                    .filter(Submission.user == self.current_user).scalar()
+                if submission_c >= contest.max_submission_number:
+                    raise ValueError(
+                        self._("You have reached the maximum limit of "
+                               "at most %s submissions among all tasks.") %
+                        contest.max_submission_number)
+            if task.max_submission_number is not None:
+                submission_t = self.sql_session.query(func.count(Submission.id))\
+                    .filter(Submission.task == task)\
+                    .filter(Submission.user == self.current_user).scalar()
+                if submission_t >= task.max_submission_number:
+                    raise ValueError(
+                        self._("You have reached the maximum limit of "
+                               "at most %s submissions on this task.") %
+                        task.max_submission_number)
+        except ValueError as error:
+            self.application.service.add_notification(
+                self.current_user.username,
+                self.timestamp,
+                self._("Too many submissions!"),
+                str(error),
+                ContestWebServer.NOTIFICATION_ERROR)
+            self.redirect("/tasks/%s/submissions" % quote(task.name, safe=''))
+            return
+
+        # Enforce minimum time between submissions
+        try:
+            if contest.min_submission_interval is not None:
+                last_submission_c = self.sql_session.query(Submission)\
+                    .join(Submission.task)\
+                    .filter(Task.contest == contest)\
+                    .filter(Submission.user == self.current_user)\
+                    .order_by(Submission.timestamp.desc()).first()
+                if last_submission_c is not None and \
+                        self.timestamp - last_submission_c.timestamp < \
+                        contest.min_submission_interval:
+                    raise ValueError(
+                        self._("Among all tasks, you can submit again "
+                               "after %s seconds from last submission.") %
+                        contest.min_submission_interval)
+            if task.min_submission_interval is not None:
+                last_submission_t = self.sql_session.query(Submission)\
+                    .filter(Submission.task == task)\
+                    .filter(Submission.user == self.current_user)\
+                    .order_by(Submission.timestamp.desc()).first()
+                if last_submission_t is not None and \
+                        self.timestamp - last_submission_t.timestamp < \
+                        task.min_submission_interval:
+                    raise ValueError(
+                        self._("For this task, you can submit again "
+                               "after %s seconds from last submission.") %
+                        task.min_submission_interval)
+        except ValueError as error:
             self.application.service.add_notification(
                 self.current_user.username,
                 self.timestamp,
                 self._("Submissions too frequent!"),
-                self._("For each task, you can submit "
-                       "again after %s seconds from last submission.") %
-                config.min_submission_interval,
+                str(error),
                 ContestWebServer.NOTIFICATION_ERROR)
             self.redirect("/tasks/%s/submissions" % quote(task.name, safe=''))
             return
@@ -1131,21 +1186,73 @@ class UserTestHandler(BaseHandler):
         except KeyError:
             raise tornado.web.HTTPError(404)
 
-        # Enforce minimum time between submissions for the same task.
-        last_usertest = self.sql_session.query(UserTest)\
-            .filter(UserTest.task == task)\
-            .filter(UserTest.user == self.current_user)\
-            .order_by(UserTest.timestamp.desc()).first()
-        if last_usertest is not None and \
-               self.timestamp - last_usertest.timestamp < \
-               timedelta(seconds=config.min_submission_interval):
+        # Alias for easy access
+        contest = self.contest
+
+        # Enforce maximum number of usertests
+        try:
+            if contest.max_usertest_number is not None:
+                usertest_c = self.sql_session.query(func.count(UserTest.id))\
+                    .join(UserTest.task)\
+                    .filter(Task.contest == contest)\
+                    .filter(UserTest.user == self.current_user).scalar()
+                if usertest_c >= contest.max_usertest_number:
+                    raise ValueError(
+                        self._("You have reached the maximum limit of "
+                               "at most %s tests among all tasks.") %
+                        contest.max_usertest_number)
+            if task.max_usertest_number is not None:
+                usertest_t = self.sql_session.query(func.count(UserTest.id))\
+                    .filter(UserTest.task == task)\
+                    .filter(UserTest.user == self.current_user).scalar()
+                if usertest_t >= task.max_usertest_number:
+                    raise ValueError(
+                        self._("You have reached the maximum limit of "
+                               "at most %s tests on this task.") %
+                        task.max_usertest_number)
+        except ValueError as error:
             self.application.service.add_notification(
                 self.current_user.username,
                 self.timestamp,
-                self._("Test too frequent!"),
-                self._("For each task, you can test "
-                       "again after %s seconds from last test.") %
-                config.min_submission_interval,
+                self._("Too many tests!"),
+                str(error),
+                ContestWebServer.NOTIFICATION_ERROR)
+            self.redirect("/testing?%s" % quote(task.name, safe=''))
+            return
+
+        # Enforce minimum time between usertests
+        try:
+            if contest.min_usertest_interval is not None:
+                last_usertest_c = self.sql_session.query(UserTest)\
+                    .join(UserTest.task)\
+                    .filter(Task.contest == contest)\
+                    .filter(UserTest.user == self.current_user)\
+                    .order_by(UserTest.timestamp.desc()).first()
+                if last_usertest_c is not None and \
+                        self.timestamp - last_usertest_c.timestamp < \
+                        contest.min_usertest_interval:
+                    raise ValueError(
+                        self._("Among all tasks, you can test again "
+                               "after %s seconds from last test.") %
+                        contest.min_usertest_interval)
+            if task.min_usertest_interval is not None:
+                last_usertest_t = self.sql_session.query(UserTest)\
+                    .filter(UserTest.task == task)\
+                    .filter(UserTest.user == self.current_user)\
+                    .order_by(UserTest.timestamp.desc()).first()
+                if last_usertest_t is not None and \
+                        self.timestamp - last_usertest_t.timestamp < \
+                        task.min_usertest_interval:
+                    raise ValueError(
+                        self._("For this task, you can test again "
+                               "after %s seconds from last test.") %
+                        task.min_usertest_interval)
+        except ValueError as error:
+            self.application.service.add_notification(
+                self.current_user.username,
+                self.timestamp,
+                self._("Tests too frequent!"),
+                str(error),
                 ContestWebServer.NOTIFICATION_ERROR)
             self.redirect("/testing?%s" % quote(task.name, safe=''))
             return

cms/__init__.py
@@ -82,7 +82,6 @@ class Config:
         self.block_hidden_users = False
         self.is_proxy_used = False
         self.max_submission_length = 100000
-        self.min_submission_interval = 60
         self.stl_path = "/usr/share/doc/stl-manual/html/"
         self.allow_questions = True
         # Prefix of 'iso-codes'[1] installation. It can be found out

cms/db/Contest.py
@@ -99,6 +99,18 @@ class Contest(Base):
     # Max contest time for each user in seconds.
     per_user_time = Column(Interval, nullable=True)
 
+    # Maximum number of submissions or usertests allowed for each user
+    # during the whole contest or None to not enforce this limitation.
+    # TODO Add some CheckConstraints.
+    max_submission_number = Column(Integer, nullable=True)
+    max_usertest_number = Column(Integer, nullable=True)
+
+    # Minimum interval between two submissions or usertests, in seconds,
+    # or None to not enforce this limitation.
+    # TODO Add some CheckConstraints.
+    min_submission_interval = Column(Interval, nullable=True)
+    min_usertest_interval = Column(Interval, nullable=True)
+
     # Follows the description of the fields automatically added by
     # SQLAlchemy.
     # tasks (list of Task objects)
@@ -113,6 +125,8 @@ class Contest(Base):
                  token_min_interval=timedelta(),
                  token_gen_time=timedelta(), token_gen_number=0,
                  start=None, stop=None, timezone=None, per_user_time=None,
+                 max_submission_number=None, max_usertest_number=None,
+                 min_submission_interval=None, min_usertest_interval=None,
                  announcements=None):
         self.name = name
         self.description = description
@@ -128,6 +142,10 @@ class Contest(Base):
         self.stop = stop
         self.timezone = timezone
         self.per_user_time = per_user_time
+        self.max_submission_number = max_submission_number
+        self.max_usertest_number = max_usertest_number
+        self.min_submission_interval = min_submission_interval
+        self.min_usertest_interval = min_usertest_interval
         self.announcements = announcements if announcements is not None else []
 
     def export_to_dict(self, skip_submissions=False):
@@ -150,6 +168,10 @@ class Contest(Base):
                 'stop':               make_timestamp(self.stop) if self.stop is not None else None,
                 'timezone':           self.timezone,
                 'per_user_time':      self.per_user_time.total_seconds() if self.per_user_time is not None else None,
+                'max_submission_number': self.max_submission_number if self.max_submission_number is not None else None,
+                'max_usertest_number': self.max_usertest_number if self.max_usertest_number is not None else None,
+                'min_submission_interval': self.min_submission_interval.total_seconds() if self.min_submission_interval is not None else None,
+                'min_usertest_interval': self.min_usertest_interval.total_seconds() if self.min_usertest_interval is not None else None,
                 'announcements':      [announcement.export_to_dict()
                                        for announcement in self.announcements],
                 }

cms/db/ImportFromDict.py
@@ -57,6 +57,14 @@ def contest_import_from_dict(cls, data):
         data['token_gen_time'] = timedelta(minutes=data['token_gen_time'])
     if 'per_user_time' in data and data['per_user_time'] is not None:
         data['per_user_time'] = timedelta(seconds=data['per_user_time'])
+    if 'min_submission_interval' in data and \
+            data['min_submission_interval'] is not None:
+        data['min_submission_interval'] = \
+            timedelta(seconds=data['min_submission_interval'])
+    if 'min_usertest_interval' in data and \
+            data['min_usertest_interval'] is not None:
+        data['min_usertest_interval'] = \
+            timedelta(seconds=data['min_usertest_interval'])
     return cls(**data)
 
 

cms/db/Task.py
@@ -115,6 +115,19 @@ class Task(Base):
     token_gen_number = Column(
         Integer, CheckConstraint("token_gen_number >= 0"), nullable=False)
 
+    # Maximum number of submissions or usertests allowed for each user
+    # on this task during the whole contest or None to not enforce
+    # this limitation.
+    # TODO Add some CheckConstraints.
+    max_submission_number = Column(Integer, nullable=True)
+    max_usertest_number = Column(Integer, nullable=True)
+
+    # Minimum interval between two submissions or usertests for this
+    # task, in seconds, or None to not enforce this limitation.
+    # TODO Add some CheckConstraints.
+    min_submission_interval = Column(Interval, nullable=True)
+    min_usertest_interval = Column(Interval, nullable=True)
+
     # Follows the description of the fields automatically added by
     # SQLAlchemy.
     # submission_format (list of SubmissionFormatElement objects)
@@ -137,6 +150,8 @@ class Task(Base):
                  token_initial=None, token_max=None, token_total=None,
                  token_min_interval=timedelta(),
                  token_gen_time=timedelta(), token_gen_number=0,
+                 max_submission_number=None, max_usertest_number=None,
+                 min_submission_interval=None, min_usertest_interval=None,
                  contest=None, num=0):
         for filename, attachment in attachments.iteritems():
             attachment.filename = filename
@@ -166,6 +181,10 @@ class Task(Base):
         self.token_min_interval = token_min_interval
         self.token_gen_time = token_gen_time
         self.token_gen_number = token_gen_number
+        self.max_submission_number = max_submission_number
+        self.max_usertest_number = max_usertest_number
+        self.min_submission_interval = min_submission_interval
+        self.min_usertest_interval = min_usertest_interval
         self.contest = contest
 
     def export_to_dict(self):
@@ -203,7 +222,12 @@ class Task(Base):
                     self.token_min_interval.total_seconds(),
                 'token_gen_time':
                     self.token_gen_time.total_seconds() / 60,
-                'token_gen_number':     self.token_gen_number}
+                'token_gen_number':     self.token_gen_number,
+                'max_submission_number': self.max_submission_number if self.max_submission_number is not None else None,
+                'max_usertest_number': self.max_usertest_number if self.max_usertest_number is not None else None,
+                'min_submission_interval': self.min_submission_interval.total_seconds() if self.min_submission_interval is not None else None,
+                'min_usertest_interval': self.min_usertest_interval.total_seconds() if self.min_usertest_interval is not None else None,
+                }
 
     @classmethod
     def import_from_dict(cls, data):
@@ -226,9 +250,19 @@ class Task(Base):
                               for statement_data in data['statements']]
         data['statements'] = dict([(statement.language, statement)
                                    for statement in data['statements']])
-        data['token_min_interval'] = \
-            timedelta(seconds=data['token_min_interval'])
-        data['token_gen_time'] = timedelta(minutes=data['token_gen_time'])
+        if 'token_min_interval' in data:
+            data['token_min_interval'] = \
+                timedelta(seconds=data['token_min_interval'])
+        if 'token_gen_time' in data:
+            data['token_gen_time'] = timedelta(minutes=data['token_gen_time'])
+        if 'min_submission_interval' in data and \
+                data['min_submission_interval'] is not None:
+            data['min_submission_interval'] = \
+                timedelta(seconds=data['min_submission_interval'])
+        if 'min_usertest_interval' in data and \
+                data['min_usertest_interval'] is not None:
+            data['min_usertest_interval'] = \
+                timedelta(seconds=data['min_usertest_interval'])
         return cls(**data)
 
 

cms/db/UpdateDB.py
@@ -61,6 +61,7 @@ class ScriptsContainer(object):
             ("20120805", "support_output_only"),
             ("20120823", "add_user_tests"),
             ("20120824", "add_evaluation_text_to_user_test"),
+            ("20120911", "add_submission_and_usertest_limits"),
             ]
         self.list.sort()
 
@@ -728,6 +729,28 @@ CREATE INDEX ix_user_test_managers_user_test_id ON user_test_managers (user_test
         with SessionGen(commit=True) as session:
             session.execute("ALTER TABLE user_tests "
                             "ADD COLUMN evaluation_text VARCHAR;")
+
+    @staticmethod
+    def add_submission_and_usertest_limits():
+        """Add limits to the total number and the frequency of
+        submissions and usertests.
+
+        """
+        with SessionGen(commit=True) as session:
+            session.execute("""\
+ALTER TABLE contests
+ADD COLUMN max_submission_number INTEGER,
+ADD COLUMN max_usertest_number INTEGER,
+ADD COLUMN min_submission_interval INTERVAL,
+ADD COLUMN min_usertest_interval INTERVAL;""")
+            session.execute("""\
+ALTER TABLE tasks
+ADD COLUMN max_submission_number INTEGER,
+ADD COLUMN max_usertest_number INTEGER,
+ADD COLUMN min_submission_interval INTERVAL,
+ADD COLUMN min_usertest_interval INTERVAL;""")
+        print "Please remove 'min_submission_interval' from your configuration."
+        print "It is now possible to set it on a per-task and per-contest basis using AWS."
 
 
 def execute_single_script(scripts_container, script):

cms/server/AdminWebServer.py
@@ -394,6 +394,22 @@ class AddContestHandler(BaseHandler):
                 "token_gen_number",
                 0,
                 allow_empty=False)
+            max_submission_number = self.get_non_negative_int(
+                "max_submission_number",
+                None)
+            max_usertest_number = self.get_non_negative_int(
+                "max_usertest_number",
+                None)
+            min_submission_interval = self.get_non_negative_int(
+                "min_submission_interval",
+                None)
+            if min_submission_interval is not None:
+                min_submission_interval = timedelta(seconds=min_submission_interval)
+            min_usertest_interval = self.get_non_negative_int(
+                "min_usertest_interval",
+                None)
+            if min_usertest_interval is not None:
+                min_usertest_interval = timedelta(seconds=min_usertest_interval)
         except Exception as error:
             self.application.service.add_notification(
                 make_datetime(),
@@ -450,7 +466,9 @@ class AddContestHandler(BaseHandler):
         contest = Contest(name, description, [], [], token_initial,
                           token_max, token_total, token_min_interval,
                           token_gen_time, token_gen_number, start, stop,
-                          timezone, per_user_time)
+                          timezone, per_user_time,
+                          max_submission_number, max_usertest_number,
+                          min_submission_interval, min_usertest_interval)
 
         self.sql_session.add(contest)
         try_commit(self.sql_session, self)
@@ -763,6 +781,24 @@ class TaskViewHandler(BaseHandler):
                 "token_gen_number",
                 task.token_gen_number,
                 allow_empty=False)
+            task.max_submission_number = self.get_non_negative_int(
+                "max_submission_number",
+                task.max_submission_number)
+            task.max_usertest_number = self.get_non_negative_int(
+                "max_usertest_number",
+                task.max_usertest_number)
+            task.min_submission_interval = self.get_non_negative_int(
+                "min_submission_interval",
+                task.min_submission_interval.total_seconds() if \
+                    task.min_submission_interval is not None else None)
+            if task.min_submission_interval is not None:
+                task.min_submission_interval = timedelta(seconds=task.min_submission_interval)
+            task.min_usertest_interval = self.get_non_negative_int(
+                "min_usertest_interval",
+                task.min_usertest_interval.total_seconds() if \
+                    task.min_usertest_interval is not None else None)
+            if task.min_usertest_interval is not None:
+                task.min_usertest_interval = timedelta(seconds=task.min_usertest_interval)
         except ValueError as error:
             self.application.service.add_notification(
                 make_datetime(),
@@ -962,12 +998,30 @@ class AddTaskHandler(SimpleContestHandler("add_task.html")):
             "token_gen_number",
             0,
             allow_empty=False)
+        max_submission_number = self.get_non_negative_int(
+            "max_submission_number",
+            None)
+        max_usertest_number = self.get_non_negative_int(
+            "max_usertest_number",
+            None)
+        min_submission_interval = self.get_non_negative_int(
+            "min_submission_interval",
+            None)
+        if min_submission_interval is not None:
+            min_submission_interval = timedelta(seconds=min_submission_interval)
+        min_usertest_interval = self.get_non_negative_int(
+            "min_usertest_interval",
+            None)
+        if min_usertest_interval is not None:
+            min_usertest_interval = timedelta(seconds=min_usertest_interval)
         task = Task(name, title, statements, attachments,
                  time_limit, memory_limit, official_language,
                  task_type, task_type_parameters, submission_format, managers,
                  score_type, score_parameters, testcases,
                  token_initial, token_max, token_total,
                  token_min_interval, token_gen_time, token_gen_number,
+                 max_submission_number, max_usertest_number,
+                 min_submission_interval, min_usertest_interval,
                  contest=self.contest, num=len(self.contest.tasks))
         self.sql_session.add(task)
         if try_commit(self.sql_session, self):
@@ -1015,6 +1069,24 @@ class EditContestHandler(BaseHandler):
                 "token_gen_number",
                 self.contest.token_gen_number,
                 allow_empty=False)
+            max_submission_number = self.get_non_negative_int(
+                "max_submission_number",
+                self.contest.max_submission_number)
+            max_usertest_number = self.get_non_negative_int(
+                "max_usertest_number",
+                self.contest.max_usertest_number)
+            min_submission_interval = self.get_non_negative_int(
+                "min_submission_interval",
+                self.contest.min_submission_interval.total_seconds() if \
+                    self.contest.min_submission_interval is not None else None)
+            if min_submission_interval is not None:
+                min_submission_interval = timedelta(seconds=min_submission_interval)
+            min_usertest_interval = self.get_non_negative_int(
+                "min_usertest_interval",
+                self.contest.min_usertest_interval.total_seconds() if \
+                    self.contest.min_usertest_interval is not None else None)
+            if min_usertest_interval is not None:
+                min_usertest_interval = timedelta(seconds=min_usertest_interval)
         except Exception as error:
             self.application.service.add_notification(
                 make_datetime(),
@@ -1076,6 +1148,10 @@ class EditContestHandler(BaseHandler):
         self.contest.token_min_interval = token_min_interval
         self.contest.token_gen_time = token_gen_time
         self.contest.token_gen_number = token_gen_number
+        self.contest.max_submission_number = max_submission_number
+        self.contest.max_usertest_number = max_usertest_number
+        self.contest.min_submission_interval = min_submission_interval
+        self.contest.min_usertest_interval = min_usertest_interval
         self.contest.start = start
         self.contest.stop = stop
         self.contest.timezone = timezone

commit 94728067637210183ad3f0861dc1ee4d440e9471

Fix bug on ALLOW_PARTIAL_SUBMISSION tasks
Has been introduced in f268441a413cb180ca11be1d7f05da6a561328b5

cms/server/ContestWebServer.py
@@ -852,16 +852,16 @@ class SubmitHandler(BaseHandler):
         submission_lang = None
         file_digests = {}
         retrieved = 0
-        if task_type.ALLOW_PARTIAL_SUBMISSION and last_submission is not None:
+        if task_type.ALLOW_PARTIAL_SUBMISSION and last_submission_t is not None:
             for filename in required.difference(provided):
-                if filename in last_submission.files:
+                if filename in last_submission_t.files:
                     # If we retrieve a language-dependent file from
                     # last submission, we take not that language must
                     # be the same.
                     if "%l" in filename:
-                        submission_lang = last_submission.language
+                        submission_lang = last_submission_t.language
                     file_digests[filename] = \
-                        last_submission.files[filename].digest
+                        last_submission_t.files[filename].digest
                     retrieved += 1
 
         # We need to ensure that everytime we have a .%l in our
@@ -1329,16 +1329,16 @@ class UserTestHandler(BaseHandler):
         submission_lang = None
         file_digests = {}
         retrieved = 0
-        if task_type.ALLOW_PARTIAL_SUBMISSION and last_submission is not None:
+        if task_type.ALLOW_PARTIAL_SUBMISSION and last_usertest_t is not None:
             for filename in required.difference(provided):
-                if filename in last_usertest.files:
+                if filename in last_usertest_t.files:
                     # If we retrieve a language-dependent file from
                     # last submission, we take not that language must
                     # be the same.
                     if "%l" in filename:
-                        submission_lang = last_usertest.language
+                        submission_lang = last_usertest_t.language
                     file_digests[filename] = \
-                        last_usertest.files[filename].digest
+                        last_usertest_t.files[filename].digest
                     retrieved += 1
 
         # We need to ensure that everytime we have a .%l in our