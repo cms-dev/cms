7936363dbd67c70e14a92509b275d4cc904bbf7e
Luca Wehrstedt <luca.wehrstedt@gmail.com>
2013-02-15 18:43:24 +0100

    Clean up CWS's client-side time handling
    By removing the (unused) contest_start and contest_stop parameters and
    adding the (also yet unused) current_phase_begin parameter.

--- cms/server/static/script_new.js
+++ cms/server/static/script_new.js
@@ -5,12 +5,11 @@
 var Utils = new function () {
     var self = this;
 
-    self.init = function (timestamp, contest_start, contest_stop, current_phase_end, phase) {
+    self.init = function (timestamp, current_phase_begin, current_phase_end, phase) {
         self.last_notification = timestamp;
         self.server_timestamp = timestamp;
         self.client_timestamp = $.now() / 1000;
-        self.contest_start = contest_start;
-        self.contest_stop = contest_stop;
+        self.current_phase_begin = current_phase_begin;
         self.current_phase_end = current_phase_end;
         self.phase = phase;
         self.remaining_div = null;
