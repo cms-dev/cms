37adb34ff0d5c9d063ff0009f17dcc907d7166df
Bernard Blackham <b-gitcommits@largestprime.net>
2013-03-13 03:16:00 +0000

    Allow some leeway in wallclock timeout.
    If very short timelimits are involved (<= 0.1s), a wallclock limit of 2*t isn't
    always sufficient. For example, when communication tasks are involved, the user
    executable may be blocked, waiting on the manager to initialise and open its
    FIFO. Heavy disk I/O may also briefly stall the execution of a task without
    giving it any CPU time.
    
    Give it an extra second for cases like these.

--- cms/grading/__init__.py
+++ cms/grading/__init__.py
@@ -259,7 +259,7 @@ def evaluation_step_before_run(sandbox, command,
     """
     # Set sandbox parameters suitable for evaluation.
     sandbox.timeout = time_limit
-    sandbox.wallclock_timeout = 2 * time_limit
+    sandbox.wallclock_timeout = 2 * time_limit + 1
     sandbox.address_space = memory_limit * 1024
 
     if stdin_redirect is not None:
