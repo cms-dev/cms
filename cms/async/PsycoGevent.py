55d5a60c23e4d84d64b5d9bb2e94caeb2014bebf
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2013-06-04 19:52:00 +0200

    Fix some PEP8 warnings in PsycoGevent.py.

--- cms/async/PsycoGevent.py
+++ cms/async/PsycoGevent.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-# This file was taken from https://bitbucket.org/zzzeek/green_sqla/src/2732bb7ea9d06b9d4a61e8cd587a95148ce2599b/green_sqla/psyco_gevent.py?at=default
+# This file was taken from
+# https://bitbucket.org/zzzeek/green_sqla/src/2732bb7ea9d06b9d4a61e8cd587a95148ce2599b/green_sqla/psyco_gevent.py?at=default
 
 """A wait callback to allow psycopg2 cooperation with gevent.
 
@@ -11,17 +12,17 @@ Use `make_psycopg_green()` to enable gevent support in Psycopg.
 # Copyright (C) 2010 Daniele Varrazzo <daniele.varrazzo@gmail.com>
 # Copyright (C) 2013 Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
 # and licensed under the MIT license:
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -35,6 +36,7 @@ from psycopg2 import extensions
 
 from gevent.socket import wait_read, wait_write
 
+
 def make_psycopg_green():
     """Configure Psycopg to be used with gevent in non-blocking way."""
     if not hasattr(extensions, 'set_wait_callback'):
@@ -43,6 +45,7 @@ def make_psycopg_green():
             % psycopg2.__version__)
 
     extensions.set_wait_callback(gevent_wait_callback)
+
 
 def unmake_psycopg_green():
     """Undo make_psycopg_green()."""
@@ -53,6 +56,7 @@ def unmake_psycopg_green():
 
     print 'unmaking'
     extensions.set_wait_callback(None)
+
 
 def gevent_wait_callback(conn, timeout=None):
     """A wait callback useful to allow gevent to work with Psycopg."""
