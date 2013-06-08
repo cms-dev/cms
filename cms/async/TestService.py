ffcf705fc1972579611d21a0b7b40114ad655910
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2013-06-08 23:35:40 +0200

    Migrate TestService to gevent.

--- cms/async/TestService.py
+++ cms/async/TestService.py
@@ -27,7 +27,7 @@ service.
 
 import time
 
-from cms.async.AsyncLibrary import Service
+from cms.async.GeventLibrary import Service
 
 
 # Our logger. We cannot simply import from AsyncLibrary because at
@@ -43,7 +43,7 @@ class TestService(Service):
         Service.__init__(self, shard, custom_logger)
 
         global logger
-        from cms.async.AsyncLibrary import logger as _logger
+        from cms.async.GeventLibrary import logger as _logger
         logger = _logger
 
         self.start = 0
