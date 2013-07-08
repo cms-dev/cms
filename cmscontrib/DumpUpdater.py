585efba2f62ca76c9283481f5c42b228525faeef
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2013-07-09 00:27:26 +0200

    DumpUpdater: ability to update to a specified version.
    This is mostly meant for testing.

--- cmscontrib/DumpUpdater.py
+++ cmscontrib/DumpUpdater.py
@@ -3,6 +3,7 @@
 
 # Contest Management System
 # Copyright © 2013 Luca Wehrstedt <luca.wehrstedt@gmail.com>
+# Copyright © 2013 Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -38,10 +39,17 @@ def main():
     parser = argparse.ArgumentParser(
         description="Updater of CMS contest dumps.")
     parser.add_argument(
+        "-V", "--to-version", action="store", type=int, default=-1,
+        help="Update to given version number")
+    parser.add_argument(
         "path", help="location of the dump or of the 'contest.json' file")
 
     args = parser.parse_args()
     path = args.path
+
+    to_version = args.to_version
+    if to_version == -1:
+        to_version = model_version
 
     if not path.endswith("contest.json"):
         path = os.path.join(path, "contest.json")
@@ -59,20 +67,20 @@ def main():
     # export (before the new dump format was introduced).
     dump_version = data.get("_version", 0)
 
-    if dump_version == model_version:
+    if dump_version == to_version:
         logger.info(
             "The dump you're trying to update is already stored using "
             "the most recent format supported by this version of CMS.")
         return
 
-    if dump_version > model_version:
+    if dump_version > to_version:
         logger.critical(
             "The dump you're trying to update is stored using a format "
             "that's more recent than the one supported by this version "
             "of CMS. You probably need to update CMS to handle it.")
         return
 
-    for version in range(dump_version, model_version):
+    for version in range(dump_version, to_version):
         # Update from version to version+1
         updater = __import__(
             "cmscontrib.updaters.update_%d" % (version + 1),
@@ -80,7 +88,7 @@ def main():
         data = updater.run()
         data["_version"] = version + 1
 
-    assert data["_version"] == model_version
+    assert data["_version"] == to_version
 
     with io.open(path, 'wb') as fout:
         json.dump(data, fout, encoding="utf-8", indent=4, sort_keys=True)
