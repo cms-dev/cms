cms-dev/cms

commit 6e4c42f7dd3135e1464d2c6e9e5db15670d77335

TV: make SS re-send old submissions to RWS when active dataset changes
And, otherwise, send results only for the active dataset to RWS.

This commit is part of the series that introduces task versioning

cms/service/ScoringService.py
@@ -717,6 +717,18 @@ class ScoringService(Service):
             submission_result.ranking_score_details = \
                 scorer.pool[submission_id]["ranking_details"]
 
+            try:
+                ranking_score_details = json.loads(
+                        submission_result.ranking_score_details)
+            except (TypeError, ValueError):
+                # It may be blank.
+                ranking_score_details = None
+
+            # If we are not a live dataset then we can bail out here,
+            # and avoid updating RWS.
+            if dataset is not submission.task.active_dataset:
+                return
+
             # Data to send to remote rankings.
             submission_put_data = {
                 "user": encode_id(submission.user.username),
@@ -729,8 +741,9 @@ class ScoringService(Service):
                 "submission": encode_id(submission_id),
                 "time": int(make_timestamp(submission.timestamp)),
                 # We're sending the unrounded score to RWS
-                "score": submission_result.score,
-                "extra": submission_result.ranking_score_details}
+                "score": submission_result.score}
+            if ranking_score_details is not None:
+                subchange_put_data["extra"] = ranking_score_details
 
         # TODO: ScoreRelative here does not work with remote
         # rankings (it does in the ranking view) because we
@@ -856,6 +869,60 @@ class ScoringService(Service):
         if old_s + old_t == 0:
             self.add_timeout(self.score_old_submissions, None,
                              0.5, immediately=False)
+
+    @rpc_method
+    def dataset_updated(self, task_id):
+        """This function updates RWS with new data about a task. It should be
+        called after the live dataset of a task is changed.
+
+        task_id (int): id of the task whose dataset has changed.
+
+        """
+        with SessionGen(commit=False) as session:
+            task = Task.get_from_id(task_id, session)
+            dataset = task.active_dataset
+
+            logger.info("Dataset update for task %d (dataset now is %d)." % (
+                task.id, dataset.id))
+
+            subchanges = []
+            for submission in task.submissions:
+                submission_result = submission.get_result(dataset)
+
+                if submission_result is None:
+                    # Not yet compiled, evaluated or scored.
+                    score = None
+                    ranking_score_details = None
+                else:
+                    score = submission_result.score
+                    try:
+                        ranking_score_details = json.loads(
+                                submission_result.ranking_score_details)
+                    except (TypeError, ValueError):
+                        # It may be blank.
+                        ranking_score_details = None
+
+                # Data to send to remote rankings.
+                subchange_id = "%s%ss" % \
+                    (int(make_timestamp(submission.timestamp)),
+                     submission.id)
+                subchange_put_data = {
+                    "submission": encode_id(submission.id),
+                    "time": int(make_timestamp(submission.timestamp))}
+                if score is not None:
+                    # We're sending the unrounded score to RWS
+                    subchange_put_data["score"] = score
+                if ranking_score_details is not None:
+                    subchange_put_data["extra"] = ranking_score_details
+                subchanges.append((subchange_id, subchange_put_data))
+
+        # Adding operations to the queue.
+        with self.operation_queue_lock:
+            for ranking in self.rankings:
+                for subchange_id, data in subchanges:
+                    self.subchange_queue.setdefault(
+                        ranking,
+                        dict())[encode_id(subchange_id)] = data
 
 
 def main():

cms/server/AdminWebServer.py
@@ -1120,6 +1120,7 @@ class ActivateDatasetHandler(BaseHandler):
 
         if try_commit(self.sql_session, self):
             self.application.service.scoring_service.reinitialize()
+            self.application.service.scoring_service.dataset_updated(task_id=task.id)
 
             # This kicks off judging of any submissions which were previously
             # unloved, but are now part of an autojudged taskset.

commit af2338b9a22df8a19671c7fee78d9dc4b35c49ea

Fix SS not sending ranking details to RWSs
Bug introduced in 6e4c42f7dd3135e1464d2c6e9e5db15670d77335 but the
situation wasn't that "safe" even before: we were trying to store a list
of strings in the DB.

cms/grading/ScoreType.py
@@ -240,7 +240,7 @@ class ScoreTypeGroup(ScoreTypeAlone):
 
         """
         if not submission_result.evaluated():
-            return 0.0, "[]", 0.0, "[]", ["%lg" % 0.0 for _ in self.parameters]
+            return 0.0, "[]", 0.0, "[]", json.dumps(["%lg" % 0.0 for _ in self.parameters])
 
         # XXX Lexicographical order by codename
         indices = sorted(self.public_testcases.keys())
@@ -303,7 +303,7 @@ class ScoreTypeGroup(ScoreTypeAlone):
 
         return score, json.dumps(subtasks), \
                public_score, json.dumps(public_subtasks), \
-               ranking_details
+               json.dumps(ranking_details)
 
     def get_public_outcome(self, outcome, parameter):
         """Return a public outcome from an outcome.

cms/grading/scoretypes/Sum.py
@@ -98,7 +98,7 @@ class Sum(ScoreTypeAlone):
 
         """
         if not submission_result.evaluated():
-            return 0.0, "[]", 0.0, "[]", []
+            return 0.0, "[]", 0.0, "[]", json.dumps([])
 
         # XXX Lexicographical order by codename
         indices = sorted(self.public_testcases.keys())
@@ -128,7 +128,7 @@ class Sum(ScoreTypeAlone):
 
         return score, json.dumps(testcases), \
                public_score, json.dumps(public_testcases), \
-               []
+               json.dumps([])
 
     def get_public_outcome(self, outcome):
         """Return a public outcome from an outcome.

cms/service/ScoringService.py
@@ -433,13 +433,6 @@ class ScoringService(Service):
             submission_result.public_score_details = public_details
             submission_result.ranking_score_details = ranking_details
 
-            try:
-                ranking_score_details = json.loads(
-                        submission_result.ranking_score_details)
-            except (TypeError, ValueError):
-                # It may be blank.
-                ranking_score_details = None
-
             # If we are not a live dataset then we can bail out here,
             # and avoid updating RWS.
             if dataset is not submission.task.active_dataset:
@@ -457,9 +450,8 @@ class ScoringService(Service):
                 "submission": encode_id(str(submission_id)),
                 "time": int(make_timestamp(submission.timestamp)),
                 # We're sending the unrounded score to RWS
-                "score": submission_result.score}
-            if ranking_score_details is not None:
-                subchange_put_data["extra"] = ranking_score_details
+                "score": submission_result.score,
+                "extra": json.loads(submission_result.ranking_score_details)}
 
         # Adding operations to the queue.
         for ranking in self.rankings:
@@ -591,19 +583,6 @@ class ScoringService(Service):
             for submission in task.submissions:
                 submission_result = submission.get_result(dataset)
 
-                if submission_result is None:
-                    # Not yet compiled, evaluated or scored.
-                    score = None
-                    ranking_score_details = None
-                else:
-                    score = submission_result.score
-                    try:
-                        ranking_score_details = json.loads(
-                                submission_result.ranking_score_details)
-                    except (TypeError, ValueError):
-                        # It may be blank.
-                        ranking_score_details = None
-
                 # Data to send to remote rankings.
                 subchange_id = "%s%ss" % \
                     (int(make_timestamp(submission.timestamp)),
@@ -611,11 +590,11 @@ class ScoringService(Service):
                 subchange_data = {
                     "submission": encode_id(str(submission.id)),
                     "time": int(make_timestamp(submission.timestamp))}
-                if score is not None:
+                if submission_result is not None:
                     # We're sending the unrounded score to RWS
-                    subchange_put_data["score"] = score
-                if ranking_score_details is not None:
-                    subchange_put_data["extra"] = ranking_score_details
+                    subchange_data["score"] = submission_result.score
+                    subchange_data["extra"] = \
+                        json.loads(submission_result.ranking_score_details)
                 subchanges[subchange_id] = subchange_data
 
         # Adding operations to the queue.