5454409bd31f657d252a66dbca8cd52ceb1d11de
Damien Leroy <damien.leroy@be-oi.be>
2013-12-02 23:46:50 +0100

    test classes for java

---
+++ cmstestsuite/code/incorrect-stdio.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Task {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println("incorrect "+n);
+    }
+}
---
+++ cmstestsuite/code/oom-heap.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Task {
+
+    public static void main(String[] args) {
+    	int[] big = new int[64 * 1024 * 1024];
+        Scanner scanner = new Scanner(System.in);
+        big[10000] = scanner.nextInt();
+        System.out.println("correct "+big[10000]);
+    }
+}
