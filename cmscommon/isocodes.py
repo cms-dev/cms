cbe9ee3f4fdcced560fcecb7dcfe3ee58f6ac5fa
Luca Wehrstedt <luca.wehrstedt@gmail.com>
2013-12-31 18:52:11 +0100

    Fix some issues in cmscommon.mimetypes
    Thanks to Fabian Gundlach (@fagu) for the reports.

--- cmscommon/isocodes.py
+++ cmscommon/isocodes.py
@@ -18,6 +18,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import absolute_import
+
 import os.path
 from xml.sax import parse
 from xml.sax.handler import ContentHandler
