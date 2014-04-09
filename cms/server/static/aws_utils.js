93b289085f1bfd00b7df13189e388348794bccf2
Stefano Maggiolo <s.maggiolo@gmail.com>
2014-04-09 07:43:29 +0100

    Added license info and use strict to javascript.

--- cms/server/static/aws_utils.js
+++ cms/server/static/aws_utils.js
@@ -1,3 +1,22 @@
+/* Contest Management System
+ * Copyright © 2014 Stefano Maggiolo <s.maggiolo@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+"use strict";
+
 /**
  * Utility functions needed by AWS front-end.
  */
@@ -48,8 +67,8 @@ CMS.AWSUtils.prototype.hide_subpage = function() {
  *     successful.
  */
 CMS.AWSUtils.prototype.file_received = function(response, error) {
-    file_name = this.file_asked_name;
-    url = this.file_asked_url;
+    var file_name = this.file_asked_name;
+    var url = this.file_asked_url;
     var elements = [];
     if (error != null) {
         alert("File request failed.");
@@ -177,7 +196,7 @@ CMS.AWSUtils.prototype.update_unread_counts = function(delta_public, delta_priva
         }
     }
     if (unread_private) {
-        msg_private = parseInt(unread_private.text());
+        var msg_private = parseInt(unread_private.text());
         msgs_private += delta_private;
         unread_private.text(msgs_private);
         if (msgs_private > 0) {
