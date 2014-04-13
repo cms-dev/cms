1edb4bb6093aa483ee5df20817d64b9d9d6d7051
Luca Wehrstedt <luca.wehrstedt@gmail.com>
2014-04-13 19:12:31 +0200

    Fix empty __init__.py files
    Remove any copyright claim and add __future__ headers.

--- cmstaskenv/__init__.py
+++ cmstaskenv/__init__.py
@@ -2,9 +2,6 @@
 # -*- coding: utf-8 -*-
 
 # Contest Management System - http://cms-dev.github.io/
-# Copyright © 2010-2012 Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
-# Copyright © 2010-2012 Stefano Maggiolo <s.maggiolo@gmail.com>
-# Copyright © 2010-2012 Matteo Boscariol <boscarim@hotmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -18,3 +15,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
