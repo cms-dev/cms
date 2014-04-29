0db65e41c0d4b5d9982be44995c01b630dd72706
Artem Iglikov <artem.iglikov@gmail.com>
2014-04-30 00:11:17 +0600

    Add displaying submission limits in CWS

--- cms/server/ContestWebServer.py
+++ cms/server/ContestWebServer.py
@@ -7,6 +7,7 @@
 # Copyright © 2010-2012 Matteo Boscariol <boscarim@hotmail.com>
 # Copyright © 2012-2013 Luca Wehrstedt <luca.wehrstedt@gmail.com>
 # Copyright © 2013 Bernard Blackham <bernard@largestprime.net>
+# Copyright © 2014 Artem Iglikov <artem.iglikov@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -593,8 +594,30 @@ class TaskSubmissionsHandler(BaseHandler):
             .filter(Submission.user == self.current_user)\
             .filter(Submission.task == task).all()
 
+        submissions_left_contest = None
+        if self.contest.max_submission_number is not None:
+            submissions_c = self.sql_session\
+                .query(func.count(Submission.id))\
+                .join(Submission.task)\
+                .filter(Task.contest == self.contest)\
+                .filter(Submission.user == self.current_user).scalar()
+            submissions_left_contest = \
+                self.contest.max_submission_number - submissions_c
+
+        submissions_left_task = None
+        if task.max_submission_number is not None:
+            submissions_left_task = \
+                task.max_submission_number - len(submissions)
+
+        submissions_left = submissions_left_contest
+        if submissions_left_task is not None and \
+            (submissions_left_contest is None or
+             submissions_left_contest > submissions_left_task):
+            submissions_left = submissions_left_task
+
         self.render("task_submissions.html",
-                    task=task, submissions=submissions, **self.r_params)
+                    task=task, submissions=submissions,
+                    submissions_left=submissions_left, **self.r_params)
 
 
 class TaskStatementViewHandler(FileHandler):
@@ -1309,7 +1332,17 @@ class UserTestInterfaceHandler(BaseHandler):
     @actual_phase_required(0)
     def get(self):
         user_tests = dict()
+        user_tests_left = dict()
         default_task = None
+
+        user_tests_left_contest = None
+        if self.contest.max_user_test_number is not None:
+            user_test_c = self.sql_session.query(func.count(UserTest.id))\
+                .join(UserTest.task)\
+                .filter(Task.contest == self.contest)\
+                .filter(UserTest.user == self.current_user).scalar()
+            user_tests_left_contest = \
+                self.contest.max_user_test_number - user_test_c
 
         for task in self.contest.tasks:
             if self.request.query == task.name:
@@ -1317,12 +1350,22 @@ class UserTestInterfaceHandler(BaseHandler):
             user_tests[task.id] = self.sql_session.query(UserTest)\
                 .filter(UserTest.user == self.current_user)\
                 .filter(UserTest.task == task).all()
+            user_tests_left_task = None
+            if task.max_user_test_number is not None:
+                user_tests_left_task = \
+                    task.max_user_test_number - len(user_tests[task.id])
+            user_tests_left[task.id] = user_tests_left_contest
+            if user_tests_left_task is not None and \
+                (user_tests_left_contest is None or
+                 user_tests_left_contest > user_tests_left_task):
+                user_tests_left[task.id] = user_tests_left_task
 
         if default_task is None and len(self.contest.tasks) > 0:
             default_task = self.contest.tasks[0]
 
         self.render("test_interface.html", default_task=default_task,
-                    user_tests=user_tests, **self.r_params)
+                    user_tests=user_tests, user_tests_left=user_tests_left,
+                    **self.r_params)
 
 
 class UserTestHandler(BaseHandler):
