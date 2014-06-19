8d64210e20148a42579a71539173606f0d2e855a
Luca Wehrstedt <luca.wehrstedt@gmail.com>
2014-06-19 11:04:33 +0200

    Fixup 1edb4bb6093aa483ee5df20817d64b9d9d6d7051

--- cmscompat/__init__.py
+++ cmscompat/__init__.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 # Contest Management System - http://cms-dev.github.io/
-# Copyright © 2014 Luca Wehrstedt <luca.wehrstedt@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
