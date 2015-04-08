dec64ffc0057f35c5c0b9fd5a36f1892e5daf0af
Robert Newey <robert@nuiz.org>
2015-04-08 01:30:43 +0000

    added AIOC scoring:

---
+++ cms/grading/scoretypes/AIOCGroupMin.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Contest Management System - http://cms-dev.github.io/
+# Copyright © 2010-2012 Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
+# Copyright © 2010-2012 Stefano Maggiolo <s.maggiolo@gmail.com>
+# Copyright © 2010-2012 Matteo Boscariol <boscarim@hotmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from cms.grading.ScoreType import AIOCScoreTypeGroup
+
+
+# Dummy function to mark translatable string.
+def N_(message):
+    return message
+
+
+class AIOCGroupMin(AIOCScoreTypeGroup):
+    """The score of a submission is the sum of the product of the
+    minimum of the ranges with the multiplier of that range.
+
+    Parameters are [[m, t], ... ] (see ScoreTypeGroup).
+
+    """
+
+    def get_public_outcome(self, outcome, unused_parameter):
+        """See ScoreTypeGroup."""
+        if outcome <= 0.0:
+            return N_("Not correct")
+        elif outcome >= 1.0:
+            return N_("Correct")
+        else:
+            return N_("Partially correct")
+
+    def reduce(self, outcomes, unused_parameter):
+        """See ScoreTypeGroup."""
+        return min(outcomes)
