d39da92037fd7e5fb492126fafd177fd1f568ea7
Ben Stott <bgbnbigben@gmail.com>
2015-04-10 02:17:24 +0000

    A skeleton custom scorer. Doesn't actually do any score computation.

---
+++ cms/grading/scoretypes/AIOCCodebreakerScoreType.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Contest Management System - http://cms-dev.github.io/
+# Copyright © 2010-2012 Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
+# Copyright © 2010-2012 Stefano Maggiolo <s.maggiolo@gmail.com>
+# Copyright © 2010-2012 Matteo Boscariol <boscarim@hotmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from cms.grading.ScoreType import ScoreType
+
+
+# Dummy function to mark translatable string.
+def N_(message):
+    return message
+
+
+class AIOCCodebreakerScoreType(ScoreType):
+    """The score of a submission is 20 - sum(penalties) from previous
+    submissions. These are passed into __init__ as parameters
+
+    Parameters are a list of integers, containing the results of previous
+    evaluations. The evaluator returns -1 if the provided files do not break the
+    code, -2 if the provided input file is insane, -2 if the provided input file
+    doesn't produce the provided output file, and 1 if the input breaks the
+    code. These can be used to implement a custom score based on previous
+    evaluation results.
+
+    """
+
+    def max_scores(self):
+        """See ScoreType.max_scores"""
+        return (20., 20.)
+
+    def compute_score(self, unused_submission_result):
+        """See ScoreType.compute_score"""
+        raise NotImplementedError("Haven't yet implemented compute_score")
+
