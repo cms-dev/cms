cms-dev/cms

commit fa5e98cda752df039abc38cb9fe783e71d6eb0f5

Simplify SmartMappedCollection

cms/db/smartmappedcollection.py
@@ -106,22 +106,9 @@ class SmartMappedCollection(MappedCollection):
     # state, but SQLAlchemy will fix that immediately after.
     def _on_column_change(self, value, new_key, old_key, _sa_initiator):
         assert self._linked
-        if getattr(value, self._child_rel) is self._parent_obj:
-            # Get the old_key (the parameter may not be reliable) and
-            # do some consistency checks.
-            assert value in self.itervalues()
-            old_key = list(k for k, v in self.iteritems() if v is value)
-            assert len(old_key) == 1
-            old_key = old_key[0]
-            assert old_key == getattr(value, self._column)
-
-            # If necessary, move this object (and remove any old object
-            # with this key).
-            if new_key != old_key:
-                dict.__delitem__(self, old_key)
-                if new_key in self:
-                    MappedCollection.__delitem__(self, new_key)
-                dict.__setitem__(self, new_key, value)
+        if getattr(value, self._child_rel) is self._parent_obj \
+                and new_key != old_key:
+            self.__setitem__(new_key, value)
 
     # When this method gets called, the child object may think it's
     # already bound to the collection (i.e. its self._child_rel is set
@@ -131,29 +118,9 @@ class SmartMappedCollection(MappedCollection):
     def __setitem__(self, new_key, value, _sa_initiator=None):
         # TODO We could check if the object's type is correct.
         assert self._linked
-        if value in self.itervalues():
-            # Just some consistency checks, for extra safety!
-            assert getattr(value, self._child_rel) is self._parent_obj
-            old_key = list(k for k, v in self.iteritems() if v is value)
-            assert len(old_key) == 1
-            old_key = old_key[0]
-            assert old_key == getattr(value, self._column)
-
-            # If needed, we make SQLAlchemy call _on_column_changed to
-            # do the rest of the job (and repeat the above checks).
-            if new_key != getattr(value, self._column):
-                setattr(value, self._column, new_key)
-        else:
-            # We change the attribute before adding it to the collection
-            # to prevent the (unavoidable) call to _on_column_change
-            # from doing any damage.
-            if new_key != getattr(value, self._column):
-                setattr(value, self._column, new_key)
-
-            # Remove any old object with this key and add this instead.
-            if new_key in self:
-                MappedCollection.__delitem__(self, new_key)
-            MappedCollection.__setitem__(self, new_key, value)
+        setattr(value, self._child_rel, None)
+        setattr(value, self._column, new_key)
+        MappedCollection.__setitem__(self, new_key, value)
 
     def keyfunc(self, value):
         return getattr(value, self._column)

commit 81e7a200f1d480f4c00a8d0bedbdd9d902eb7a89

Fix SmartMappedCollection
Fixes a bug introduced by fa5e98cda752df039abc38cb9fe783e71d6eb0f5 which
caused collections loaded from the DB to be empty (even if the tests
passed). The cause was setting the parent of the child to None inside
__setitem__. The decision to do it came from a wrong understanding of
the dynamics of SQLAlchemy and of the situations that method could be
called in. The new code (that doesn't assume anything about the state
the child is in) should be more correct.

I couldn't find a way to trigger the bug in a testing environment to
make sure to catch it in case it should pop up again.

cms/db/smartmappedcollection.py
@@ -124,6 +124,14 @@ class SmartMappedCollection(MappedCollection):
             # dispose_collection
             self.on_dispose()
 
+    # We guarantee the values of the dictionary to be unique (because
+    # the key is also stored in the value itself) and this gives us
+    # an invertible mapping. This is the inverse function.
+    def get_key_of_value(self, value):
+        for k, v in self.iteritems():
+            if v == value:
+                return k
+
     # The following two methods have to maintain consistency, i.e.:
     # - self.values() is equal to all and only those objects of the
     #   self.child_cls class that have the self.child_rel_name
@@ -132,27 +140,39 @@ class SmartMappedCollection(MappedCollection):
     # - for each (key, value) in self.items(), key is equal to the
     #   self.child_key_name attribute of value.
 
+    # The general rule here is: don't ever assume anything about the
+    # state of the child, neither about its relationship attribute
+    # (i.e. who it considers its parent) nor on its key attribute.
+
     # This method is called when the key of a child object changes. In
     # such an event the binding in the dictionary has to change as well
-    # (i.e. del d[old_key] and d[new_key] = value). Actually, we simply
-    # delegate this task to the method just after this one.
-    def on_key_update(self, value, new_key, old_key, _):
+    # (i.e. del d[old_key] and d[new_key] = value).
+    def on_key_update(self, value, new_key, unused_old_key,
+                      unused_sa_initiator=None):
         assert self.linked
-        if getattr(value, self.child_rel_name) is self.parent_obj \
-                and new_key != old_key:
-            self.__setitem__(new_key, value)
+        old_key = self.get_key_of_value(value)
+        if value in self.itervalues() and new_key != old_key:
+            if new_key in self.iterkeys():
+                # We use the method of MappedCollection because we want
+                # it to trigger all the necessary Alchemy machinery to
+                # detach the child from the parent.
+                MappedCollection.__delitem__(self, new_key)
+            # We use the methods of dict directly because we want this
+            # to be transparent to Alchemy: the child was and remains
+            # bound, only its key changes, and Alchemy doesn't care
+            # about this.
+            dict.__delitem__(self, old_key)
+            dict.__setitem__(self, new_key, value)
 
     # This method is called when a binding to a new child is performed.
-    # As the child could come from anywhere we first detach it from its
-    # former parent (if any; note that this could also be ourselves).
-    # Then, as we want to bind it to new_key, we update its key to be
-    # that value. Finally we add it to the dictionary: this operation,
-    # implicitly, removes the old binding of the new key (if any).
+    # The only additional feature with respect to the superclass method
+    # is that it enforces the key stored in the child to be equal to
+    # the one that is being used to bind the child to the collection.
     @collection.internally_instrumented
     def __setitem__(self, new_key, value, _sa_initiator=None):
         # TODO We could check if the object's type is correct.
         assert self.linked
-        setattr(value, self.child_rel_name, None)
+        # This is the only difference with the standard behavior.
         setattr(value, self.child_key_name, new_key)
         MappedCollection.__setitem__(self, new_key, value, _sa_initiator)
 