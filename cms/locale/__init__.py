1d2efbb2c97e1199728bf9bbe1b80c82279c05ad
William Di Luigi <williamdiluigi@gmail.com>
2015-09-28 20:07:00 +0200

    Update location and installation of localization files
    This makes it easier to install them since the cms.mo files are just
    package data. It also makes it easier to retrieve them, since we can
    just use pkg_resources.resource_filename() instead of looking for a
    specific path.

---
+++ cms/locale/__init__.py
@@ -0,0 +1,8 @@
+from locale import \
+    filter_language_codes, get_system_translations, get_translations, \
+    wrap_translations_for_tornado
+
+__all__ = [
+    "filter_language_codes", "get_system_translations", "get_translations", \
+    "wrap_translations_for_tornado"
+]
