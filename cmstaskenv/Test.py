cms-dev/cms

commit 2d3f38c2de6ff3bcb622e032c20c57633016c0c6

Suggestions from pylint

cmstaskenv/Test.py
@@ -178,10 +178,8 @@ def test_testcases(base_dir, solution, language, assume=None):
         move_cursor(directions.UP, erase=True)
 
     # Subtasks scoring
-    try:
-        subtasks = json.loads(dataset.score_type_parameters)
-        subtasks[0]
-    except:
+    subtasks = json.loads(dataset.score_type_parameters)
+    if len(subtasks) == 0:
         subtasks = [[100, len(info)]]
 
     if dataset.score_type == 'GroupMin':

cms/db/user.py
@@ -222,7 +222,8 @@ class Participation(Base):
                         passive_deletes=True))
     __table_args__ = (UniqueConstraint('contest_id', 'user_id'),)
 
-    # Team (id and object) that the user is representing with this participation.
+    # Team (id and object) that the user is representing with this
+    # participation.
     team_id = Column(
         Integer,
         ForeignKey(Team.id,

cms/io/rpc.py
@@ -194,7 +194,7 @@ class RemoteServiceBase(object):
                          self._repr_remote(), error)
         finally:
             self.finalize("Disconnection requested.")
-            return True
+        return True
 
     def _read(self):
         """Receive a message from the socket.

cms/server/util.py
@@ -213,15 +213,15 @@ def format_size(n):
         return '0 B'
 
     # Use the last unit that's smaller than n
-    unit = map(lambda x: n >= x, DIMS).index(False) - 1
-    n = float(n) / DIMS[unit]
+    unit_index = next(i for i, x in enumerate(DIMS) if n >= x) - 1
+    n = float(n) / DIMS[unit_index]
 
     if n < 10:
-        return "%g %s" % (round(n, 2), UNITS[unit])
+        return "%g %s" % (round(n, 2), UNITS[unit_index])
     elif n < 100:
-        return "%g %s" % (round(n, 1), UNITS[unit])
+        return "%g %s" % (round(n, 1), UNITS[unit_index])
     else:
-        return "%g %s" % (round(n, 0), UNITS[unit])
+        return "%g %s" % (round(n, 0), UNITS[unit_index])
 
 
 def format_date(dt, timezone, locale=None):

cmscommon/crypto.py
@@ -46,8 +46,7 @@ __all__ = [
 # If that's the case, fallback to weak standard library PRG
 try:
     from Crypto import Random
-    _rndfile = Random.new()
-    get_random_bits = lambda x: _rndfile.read(x)
+    get_random_bits = Random.new().read
     is_random_secure = True
 except ImportError:
     import random

cmscommon/isocodes.py
@@ -42,7 +42,7 @@ __all__ = [
 from cms import config
 
 
-class _make_dict (ContentHandler):
+class _make_dict(ContentHandler):
     def __init__(self, path, key, value, result):
         self.path = path
         self.key = key

cmscommon/mimetypes.py
@@ -70,7 +70,7 @@ _types = list(l.strip() for l in
 _comments = dict()
 
 
-class _get_comment (ContentHandler):
+class _get_comment(ContentHandler):
     def __init__(self):
         self.inside = False
         self.result = None

cmsranking/Scoring.py
@@ -186,7 +186,7 @@ class Score(object):
     def delete_subchange(self, key):
         # Delete the subchange from the (sorted) list and reset the
         # history.
-        self._changes = filter(lambda a: a.key != key, self._changes)
+        self._changes = [c for c in self._changes if c.key != key]
         self.reset_history()
         logger.info("Reset history after deleting subchange '%s'", key)
 
@@ -212,8 +212,7 @@ class Score(object):
         if key in self._submissions:
             del self._submissions[key]
             # Delete all its subchanges.
-            self._changes = filter(lambda a: a.submission != key,
-                                   self._changes)
+            self._changes = [c for c in self._changes if c.submission != key]
             self.reset_history()
 
     def update_score_mode(self, score_mode):

commit dea96108d7654d10104ea7c1559e23afacf06747

Fix regression introduced by 2d3f38c2de6ff3bcb622e032c20c57633016c0c6

cmstaskenv/Test.py
@@ -179,7 +179,7 @@ def test_testcases(base_dir, solution, language, assume=None):
 
     # Subtasks scoring
     subtasks = json.loads(dataset.score_type_parameters)
-    if len(subtasks) == 0:
+    if not isinstance(subtasks, list):
         subtasks = [[100, len(info)]]
 
     if dataset.score_type == 'GroupMin':