8e5f9ae91712f85365207c1d6bc7f4cbb8494017
Stefano Maggiolo <s.maggiolo@gmail.com>
2016-01-04 00:07:27 +0000

    Use direct queries for sweeping missing operations in SS

---
+++ cms/service/scoringoperations.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Contest Management System - http://cms-dev.github.io/
+# Copyright © 2010-2013 Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
+# Copyright © 2010-2016 Stefano Maggiolo <s.maggiolo@gmail.com>
+# Copyright © 2010-2012 Matteo Boscariol <boscarim@hotmail.com>
+# Copyright © 2013 Luca Wehrstedt <luca.wehrstedt@gmail.com>
+# Copyright © 2013 Bernard Blackham <bernard@largestprime.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""The ScoringService operation class, and related functions to
+compute sets of operations to do.
+
+"""
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import logging
+
+from cms.io import QueueItem
+from cms.db import Dataset, Submission, SubmissionResult, \
+    Task
+
+
+logger = logging.getLogger(__name__)
+
+
+FILTER_DATASETS_TO_JUDGE = (
+    (Dataset.id == Task.active_dataset_id) |
+    (Dataset.autojudge.is_(True))
+)
+FILTER_SUBMISSION_RESULTS_TO_SCORE = (
+    (~SubmissionResult.filter_scored()) & (
+        (SubmissionResult.filter_compilation_failed()) |
+        (SubmissionResult.filter_evaluated()))
+)
+
+
+def get_operations(session):
+    """Return all the operations to do for all submissions.
+
+    session (Session): the database session to use.
+
+    return ([ScoringOperation, float]): a list of operations and
+        timestamps.
+
+    """
+    # Retrieve all the compilation operations for submissions
+    # already having a result for a dataset to judge.
+    results = session.query(Submission)\
+        .join(Submission.task)\
+        .join(Submission.results)\
+        .join(SubmissionResult.dataset)\
+        .filter(
+            (FILTER_DATASETS_TO_JUDGE) &
+            (FILTER_SUBMISSION_RESULTS_TO_SCORE))\
+        .with_entities(Submission.id, Dataset.id, Submission.timestamp)\
+        .all()
+
+    return [(ScoringOperation(result[0], result[1]), result[2])
+            for result in results]
+
+
+class ScoringOperation(QueueItem):
+    """The operation for the scoring service executor.
+
+    It represent the operation of scoring a submission result,
+    therefore it only contains the data identifying it: the ids for
+    the submission and the dataset.
+
+    """
+
+    def __init__(self, submission_id, dataset_id):
+        self.submission_id = submission_id
+        self.dataset_id = dataset_id
+
+    def __eq__(self, other):
+        if self.__class__ != other.__class__:
+            return False
+        return self.submission_id == other.submission_id \
+            and self.dataset_id == other.dataset_id
+
+    def __hash__(self):
+        return hash((self.submission_id, self.dataset_id))
+
+    def __str__(self):
+        return "scoring submission %d on dataset %d" % (
+            self.submission_id, self.dataset_id)
+
+    def __repr__(self):
+        return "(%s, %s)" % (
+            self.submission_id,
+            self.dataset_id)
+
+    def to_dict(self):
+        return {"submission_id": self.submission_id,
+                "dataset_id": self.dataset_id}
---
+++ cmstestsuite/unit_tests/service/scoringoperationstest.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Contest Management System - http://cms-dev.github.io/
+# Copyright © 2015-2016 Stefano Maggiolo <s.maggiolo@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Tests for the scoringoperations module (containing ScoringOperation
+and the function to compute them).
+
+"""
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import unittest
+
+from cmstestsuite.unit_tests.testdbgenerator import TestCaseWithDatabase
+
+from cms.service.scoringoperations import ScoringOperation, get_operations
+
+
+class TestScoringOperations(TestCaseWithDatabase):
+
+    def setUp(self):
+        super(TestScoringOperations, self).setUp()
+
+        # First set up the interesting contest, with a few copies
+        # of everything.
+        self.contest = self.add_contest()
+        self.participation = self.add_participation(contest=self.contest)
+        self.tasks = [
+            self.add_task(self.contest),
+            self.add_task(self.contest)
+        ]
+        self.datasets = sum([[
+            self.add_dataset(task, autojudge=False),
+            self.add_dataset(task, autojudge=True),
+            self.add_dataset(task, autojudge=False),
+        ] for task in self.tasks], [])
+        # Similarly to esoperationstest, the active dataset is not
+        # autojudged.
+        for task in self.tasks:
+            task.active_dataset = task.datasets[0]
+        self.testcases = sum([[
+            self.add_testcase(dataset),
+            self.add_testcase(dataset),
+            self.add_testcase(dataset),
+        ] for dataset in self.datasets], [])
+
+        self.session.flush()
+
+    def tearDown(self):
+        self.session.close()
+        super(TestScoringOperations, self).tearDown()
+
+    # Testing get_operations.
+
+    def test_get_operations_no_operations(self):
+        """Test for submissions without operations to do."""
+        # Submission to compile, without a result.
+        submission = self.add_submission(self.tasks[0], self.participation)
+
+        # Again to compile, but with a result.
+        submission, results = self.add_submission_with_results(
+            self.tasks[0], self.participation)
+
+        # With many failure during compilation.
+        submission, results = self.add_submission_with_results(
+            self.tasks[0], self.participation)
+        for result in results:
+            result.compilation_tries = 25
+
+        # Submission to evaluate.
+        submission, results = self.add_submission_with_results(
+            self.tasks[0], self.participation, True)
+
+        # With many failures during evaluation.
+        submission, results = self.add_submission_with_results(
+            self.tasks[0], self.participation, True)
+        for result in results:
+            result.evaluation_tries = 25
+
+        # Submission partially evaluated.
+        submission, results = self.add_submission_with_results(
+            self.tasks[0], self.participation, True)
+        evaluated_codenames = set()
+        for result in results:
+            evaluated_codename = result.dataset.testcases.keys()[0]
+            self.add_evaluation(
+                result, result.dataset.testcases[evaluated_codename])
+            evaluated_codenames.add(evaluated_codename)
+
+        self.session.flush()
+        self.assertEqual(set(get_operations(self.session)), set())
+
+    def test_get_operations_compilation_failed(self):
+        """Test for a submission which failed to compile."""
+        submission, results = self.add_submission_with_results(
+            self.tasks[0], self.participation, False)
+        self.session.flush()
+
+        expected_operations = set(
+            self._scoring_operation(submission, dataset)
+            for dataset in submission.task.datasets if self.to_judge(dataset))
+
+        self.assertEqual(
+            set(get_operations(self.session)),
+            expected_operations)
+
+    def test_get_operations_evaluated(self):
+        """Test for a submission which failed to compile."""
+        # A submission completely evaluated.
+        submission, results = self.add_submission_with_results(
+            self.tasks[0], self.participation, True)
+        for result in results:
+            for codename, testcase in result.dataset.testcases.items():
+                self.add_evaluation(result, testcase)
+                result.set_evaluation_outcome()
+        self.session.flush()
+
+        expected_operations = set(
+            self._scoring_operation(submission, dataset)
+            for dataset in submission.task.datasets if self.to_judge(dataset))
+
+        self.assertEqual(
+            set(get_operations(self.session)),
+            expected_operations)
+
+    def _scoring_operation(self, submission, dataset):
+        return (ScoringOperation(submission.id, dataset.id),
+                submission.timestamp)
+
+    @staticmethod
+    def to_judge(dataset):
+        return (
+            dataset.autojudge
+            or dataset.task.active_dataset_id == dataset.id)
+
+
+if __name__ == "__main__":
+    unittest.main()
