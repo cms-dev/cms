871634f046465c818363ccee80fe22d2f0bc991f
Stefano Maggiolo <s.maggiolo@gmail.com>
2016-02-02 13:39:32 +0000

    Correctly update dumps from user to participation

--- cmscontrib/updaters/update_15.py
+++ cmscontrib/updaters/update_15.py
@@ -3,6 +3,7 @@
 
 # Contest Management System - http://cms-dev.github.io/
 # Copyright © 2014 Luca Versari <veluca93@gmail.com>
+# Copyright © 2016 Stefano Maggiolo <s.maggiolo@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -58,10 +59,18 @@ class Updater(object):
             v = self.objs[k]
             if v["_class"] == "User":
                 self.split_user(k, v)
+            elif v["_class"] in ["Message", "PrintJob", "Question",
+                                 "Submission", "UserTest"]:
+                v["participation"] = v["user"]
+                del v["user"]
         return self.objs
 
     def split_user(self, user_id, user_data):
-        participation_id = self.get_id()
+        # Since all the other objects will now refer to the
+        # participation, we use the same id for the participation, and
+        # create a new one for the user.
+        new_user_id = self.get_id()
+        new_participation_id = user_id
         participation_data = split_dict(
             user_data,
             "contest", "delay_time",
@@ -69,15 +78,16 @@ class Updater(object):
             "messages", "usertests",
             "questions", "starting_time",
             "submissions", "hidden")
-        self.objs[participation_id] = participation_data
+        self.objs[new_participation_id] = participation_data
 
         assert user_data["_class"] == "User"
         participation_data["_class"] = "Participation"
 
         participation_data["password"] = None
-        user_data["participations"] = [participation_id]
+        user_data["participations"] = [new_participation_id]
 
-        participation_data["user"] = user_id
+        participation_data["user"] = new_user_id
+        self.objs[new_user_id] = user_data
 
         # FIXME: information about primary_statements /
         # preferrend_languages are silently dropped
