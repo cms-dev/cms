9cb1bd1aff036ddc257ee98f6f337343627afda9
Stefano Maggiolo <s.maggiolo@gmail.com>
2016-03-08 09:09:00 +0000

    Fix broken build on readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,25 @@
 
 import sys, os
 
+
+
+# To avoid builds failing because of missing C libraries, see:
+# http://read-the-docs.readthedocs.org/en/latest/faq.html
+#   #i-get-import-errors-on-libraries-that-depend-on-c-modules
+try:
+    from unittest.mock import MagicMock
+except ImportError:
+    from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['cups']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
