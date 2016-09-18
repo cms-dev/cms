5033f48bbc0f7f20cd7ee62b40aa6c93fd9f1597
William Di Luigi <williamdiluigi@gmail.com>
2016-09-18 19:50:03 +0200

    Prefix fix for StartHandler as well

--- cms/server/contest/handlers/main.py
+++ cms/server/contest/handlers/main.py
@@ -145,7 +145,7 @@ class StartHandler(ContestHandler):
         participation.starting_time = self.timestamp
         self.sql_session.commit()
 
-        self.redirect(self.r_params["contest_root"])
+        self.redirect(self.r_params["real_contest_root"])
 
 
 class LogoutHandler(ContestHandler):
