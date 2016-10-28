33373bc70db095eb332b96a44f014a0df6a899d2
Peyman Jabbarzade Ganje <peyman.jabarzade@gmail.com>
2016-10-29 00:12:29 +0330

    put import testcases at cmscommon

--- cmscommon/imports.py
+++ cmscommon/imports.py
@@ -17,74 +17,23 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""This script add multiple testcases.
-
-"""
-
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import argparse
 import logging
-import sys
-import re
 import zipfile
 
-from cms import utf8_decoder
-from cms.db import SessionGen, Dataset, Testcase, Task, Contest
+from cms.db import Testcase
 from cms.db.filecacher import FileCacher
 
 logger = logging.getLogger(__name__)
 
 
-def main():
-    """Parse arguments and launch process.
-
-    """
-    parser = argparse.ArgumentParser(description="Add an admin to CMS.")
-    parser.add_argument("task_name", action="store", type=utf8_decoder,
-                        help="name of task which tests will be attached to")
-    parser.add_argument("file", action="store", type=utf8_decoder,
-                        help="a zip file which contains tests")
-    parser.add_argument("inputtemplate", action="store", type=utf8_decoder,
-                        help="format of input")
-    parser.add_argument("outputtemplate", action="store", type=utf8_decoder,
-                        help="format of output")
-    parser.add_argument("-p", "--public", action="store_true",
-                        help="if tests should be public")
-    parser.add_argument("-o", "--overwrite", action="store_true",
-                        help="if tests can overwrite existing tests")
-    parser.add_argument("-c", "--contest_name", action="store",
-                        help="name of contest which tests will be attached to")
-    args = parser.parse_args()
-
-    with SessionGen() as session:
-        dataset = session.query(Dataset)\
-            .join(Dataset.task)\
-            .filter(Task.name == args.task_name).first()
-        if not dataset:
-            print("No task called %s found." % args.task_name)
-            return
-        task = dataset.task
-        if args.contest_name is not None:
-            contest = session.query(Contest)\
-                .filter(Contest.name == args.contest_name).first()
-            if task.contest != contest:
-                print("%s is not in %s" % (args.task_name, args.contest_name))
-                return False
-        archive = args.file
-
-        # Get input/output file names templates
-        input_template = args.inputtemplate
-        output_template = args.outputtemplate
-        input_re = re.compile(re.escape(input_template).replace("\\*",
-                              "(.*)") + "$")
-        output_re = re.compile(re.escape(output_template).replace("\\*",
-                               "(.*)") + "$")
-
-        task_name = task.name
-
+def import_testcases_from_zipfile(session, dataset, archive, input_re,
+                                  output_re, overwrite, public):
+    task_name = dataset.task.name
+    try:
         with zipfile.ZipFile(archive, "r") as archive_zfp:
             tests = dict()
             # Match input/output file names to testcases' codenames.
@@ -115,36 +64,57 @@ def main():
                 if codename in dataset.testcases:
                     # If we are allowed, remove existing testcase.
                     # If not - skip this testcase.
-                    if args.overwrite:
+                    if overwrite:
                         testcase = dataset.testcases[codename]
                         session.delete(testcase)
-                        session.commit()
+                        try:
+                            session.commit()
+                        except:
+                            skipped_tc.append(codename)
+                            continue
                         overwritten_tc.append(codename)
                     else:
                         skipped_tc.append(codename)
                         continue
 
                 # Add current testcase.
-                input_ = archive_zfp.read(testdata[0])
-                output = archive_zfp.read(testdata[1])
-                file_cacher = FileCacher()
-                input_digest = file_cacher.put_file_content(
+                try:
+                    input_ = archive_zfp.read(testdata[0])
+                    output = archive_zfp.read(testdata[1])
+                except Exception:
+                    raise Exception("Reading testcase %s failed" % codename)
+                    return
+                try:
+                    file_cacher = FileCacher()
+                    input_digest = file_cacher.put_file_content(
                         input_,
                         "Testcase input for task %s" % task_name)
-                output_digest = file_cacher.put_file_content(
+                    output_digest = file_cacher.put_file_content(
                         output,
                         "Testcase output for task %s" % task_name)
+                except Exception:
+                    raise Exception("Testcase storage failed")
+                    return
 
-                testcase = Testcase(codename, args.public, input_digest,
+                testcase = Testcase(codename, public, input_digest,
                                     output_digest, dataset=dataset)
                 session.add(testcase)
-                session.commit()
-
+                try:
+                    session.commit()
+                except Exception:
+                    raise Exception("Couldn't add test %s" % codename)
                 if codename not in overwritten_tc:
                     added_tc.append(codename)
+    except zipfile.BadZipfile:
+        raise Exception(
+            "The selected file is not a zip file."
+            " Please select a valid zip file.")
+        return
 
-    return True
-
-
-if __name__ == "__main__":
-    sys.exit(0 if main() is True else 1)
+    return (
+        "Successfully added %d and overwritten %d testcase(s)" %
+        (len(added_tc), len(overwritten_tc)),
+        "Added: %s; overwritten: %s; skipped: %s" %
+        (", ".join(added_tc) if added_tc else "none",
+         ", ".join(overwritten_tc) if overwritten_tc else "none",
+         ", ".join(skipped_tc) if skipped_tc else "none"))
