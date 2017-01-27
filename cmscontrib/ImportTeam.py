cms-dev/cms

commit b13b5843395fe5fec6e4903894b7d1368a7c0509

Make main() return an exit code instead of a boolean for all cmscontrib scripts
When installed, the object returned by main() is transformed into a
return code, so False becomes 0 and True becomes 1 (which is not the
right convention).

cmscontrib/ImportTeam.py
@@ -3,6 +3,7 @@
 
 # Contest Management System - http://cms-dev.github.io/
 # Copyright © 2015 William Di Luigi <williamdiluigi@gmail.com>
+# Copyright © 2016 Stefano Maggiolo <s.maggiolo@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -37,6 +38,7 @@ gevent.monkey.patch_all()
 import argparse
 import logging
 import os
+import sys
 
 from cms import utf8_decoder
 from cms.db import SessionGen
@@ -61,7 +63,7 @@ class TeamImporter(object):
         # Get the team
         team = self.loader.get_team()
         if team is None:
-            return
+            return False
 
         # Store
         try:
@@ -72,9 +74,10 @@ class TeamImporter(object):
                 team_id = team.id
         except IntegrityError:
             logger.critical("The team already exists.")
-            return
+            return False
 
         logger.info("Import finished (new team id: %s).", team_id)
+        return True
 
     def do_import_all(self, base_path, get_loader):
         """Get the participation list from the ContestLoader and then
@@ -82,6 +85,7 @@ class TeamImporter(object):
 
         """
         added = set()
+        success = True
 
         _, _, participations = self.loader.get_contest()
         for p in participations:
@@ -90,10 +94,12 @@ class TeamImporter(object):
 
                 if team_path not in added:
                     added.add(team_path)
-                    TeamImporter(
+                    importer = TeamImporter(
                         path=team_path,
                         loader_class=get_loader(team_path)
-                    ).do_import()
+                    )
+                    success = success and importer.do_import()
+        return success
 
 
 def main():
@@ -137,9 +143,11 @@ def main():
     )
 
     if args.all:
-        importer.do_import_all(args.target, get_loader)
+        success = importer.do_import_all(args.target, get_loader)
     else:
-        importer.do_import()
+        success = importer.do_import()
+    return 0 if success is True else 1
+
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())

cmscontrib/ImportUser.py
@@ -42,6 +42,7 @@ gevent.monkey.patch_all()
 import argparse
 import logging
 import os
+import sys
 
 from cms import utf8_decoder
 from cms.db import Contest, Participation, SessionGen, User
@@ -70,7 +71,7 @@ class UserImporter(object):
         # Get the user
         user = self.loader.get_user()
         if user is None:
-            return
+            return False
 
         # Store
         logger.info("Creating user %s on the database.", user.username)
@@ -85,7 +86,7 @@ class UserImporter(object):
                         "The specified contest (id %s) does not exist. "
                         "Aborting.",
                         self.contest_id)
-                    return
+                    return False
 
             # Check whether the user already exists
             old_user = session.query(User) \
@@ -93,7 +94,7 @@ class UserImporter(object):
                               .first()
             if old_user is not None:
                 logger.critical("The user already exists.")
-                return
+                return False
 
             session.add(user)
 
@@ -107,19 +108,25 @@ class UserImporter(object):
             user_id = user.id
 
         logger.info("Import finished (new user id: %s).", user_id)
+        return True
 
     def do_import_all(self, base_path, get_loader):
         """Get the participation list from the ContestLoader and then
-        try to import the corresponding users."""
+        try to import the corresponding users.
+
+        """
+        success = True
 
         _, _, participations = self.loader.get_contest()
         for p in participations:
             user_path = os.path.join(base_path, p["username"])
-            UserImporter(
+            importer = UserImporter(
                 path=user_path,
                 contest_id=self.contest_id,
                 loader_class=get_loader(user_path)
-            ).do_import()
+            )
+            success = success and importer.do_import()
+        return success
 
 
 def main():
@@ -168,9 +175,11 @@ def main():
     )
 
     if args.all:
-        importer.do_import_all(args.target, get_loader)
+        success = importer.do_import_all(args.target, get_loader)
     else:
-        importer.do_import()
+        success = importer.do_import()
+    return 0 if success is True else 1
+
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())

cmscontrib/AddAdmin.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Contest Management System - http://cms-dev.github.io/
-# Copyright © 2015 Stefano Maggiolo <s.maggiolo@gmail.com>
+# Copyright © 2015-2016 Stefano Maggiolo <s.maggiolo@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -77,8 +77,9 @@ def main():
 
     args = parser.parse_args()
 
-    return add_admin(args.username[0], args.password)
+    success = add_admin(args.username[0], args.password)
+    return 0 if success is True else 1
 
 
 if __name__ == "__main__":
-    sys.exit(0 if main() is True else 1)
+    sys.exit(main())

cmscontrib/AddParticipation.py
@@ -121,11 +121,12 @@ def main():
     if args.contest_id is None:
         args.contest_id = ask_for_contest()
 
-    return add_participation(args.username, args.contest_id,
-                             args.ip, args.delay_time, args.extra_time,
-                             args.password, args.team,
-                             args.hidden, args.unrestricted)
+    success = add_participation(args.username, args.contest_id,
+                                args.ip, args.delay_time, args.extra_time,
+                                args.password, args.team,
+                                args.hidden, args.unrestricted)
+    return 0 if success is True else 1
 
 
 if __name__ == "__main__":
-    sys.exit(0 if main() is True else 1)
+    sys.exit(main())

cmscontrib/AddStatement.py
@@ -3,6 +3,7 @@
 
 # Contest Management System - http://cms-dev.github.io/
 # Copyright © 2016 Myungwoo Chun <mc.tamaki@gmail.com>
+# Copyright © 2016 Stefano Maggiolo <s.maggiolo@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -103,9 +104,10 @@ def main():
 
     args = parser.parse_args()
 
-    return add_statement(args.task_name, args.language_code,
-                         args.statement_file, args.overwrite)
+    success = add_statement(args.task_name, args.language_code,
+                            args.statement_file, args.overwrite)
+    return 0 if success is True else 1
 
 
 if __name__ == "__main__":
-    sys.exit(0 if main() is True else 1)
+    sys.exit(main())

cmscontrib/AddSubmission.py
@@ -157,14 +157,12 @@ def main():
             return 1
         files[name] = filename
 
-    if add_submission(contest_id=args.contest_id,
-                      username=args.username,
-                      task_name=args.task_name,
-                      timestamp=args.timestamp,
-                      files=files):
-        return 0
-    else:
-        return 1
+    success = add_submission(contest_id=args.contest_id,
+                             username=args.username,
+                             task_name=args.task_name,
+                             timestamp=args.timestamp,
+                             files=files)
+    return 0 if success is True else 1
 
 
 if __name__ == "__main__":

cmscontrib/AddTeam.py
@@ -70,8 +70,9 @@ def main():
 
     args = parser.parse_args()
 
-    return add_team(args.code, args.name)
+    success = add_team(args.code, args.name)
+    return 0 if success is True else 1
 
 
 if __name__ == "__main__":
-    sys.exit(0 if main() is True else 1)
+    sys.exit(main())

cmscontrib/AddTestcases.py
@@ -41,6 +41,55 @@ from cmscommon.importers import import_testcases_from_zipfile
 logger = logging.getLogger(__name__)
 
 
+def add_testcases(archive, input_template, output_template,
+                  task_name, dataset_description=None, contest_name=None,
+                  public=False, overwrite=False):
+    with SessionGen() as session:
+        task = session.query(Task)\
+            .filter(Task.name == task_name).first()
+        if not task:
+            logger.error("No task called %s found." % task_name)
+            return False
+        dataset = task.active_dataset
+        if dataset_description is not None:
+            dataset = session.query(Dataset)\
+                .filter(Dataset.task_id == task.id)\
+                .filter(Dataset.description == dataset_description)\
+                .first()
+            if not dataset:
+                logger.error("No dataset called %s found."
+                             % dataset_description)
+                return False
+        if contest_name is not None:
+            contest = session.query(Contest)\
+                .filter(Contest.name == contest_name).first()
+            if task.contest != contest:
+                logger.error("%s is not in %s" %
+                             (task_name, contest_name))
+                return False
+
+        file_cacher = FileCacher()
+
+        # Get input/output file names templates
+        input_re = re.compile(
+            re.escape(input_template).replace("\\*", "(.*)") + "$")
+        output_re = re.compile(
+            re.escape(output_template).replace("\\*", "(.*)") + "$")
+
+        try:
+            successful_subject, successful_message = \
+                import_testcases_from_zipfile(
+                    session, file_cacher, dataset,
+                    archive, input_re, output_re, overwrite, public)
+        except Exception as error:
+            logger.error(str(error))
+            return False
+
+        logger.info(successful_subject)
+        logger.info(successful_message)
+    return True
+
+
 def main():
     """Parse arguments and launch process."""
     parser = argparse.ArgumentParser(description="Add an admin to CMS.")
@@ -62,54 +111,12 @@ def main():
                         help="dataset testcases will be attached to")
     args = parser.parse_args()
 
-    with SessionGen() as session:
-        task = session.query(Task)\
-            .filter(Task.name == args.task_name).first()
-        if not task:
-            logger.error("No task called %s found." % args.task_name)
-            return False
-        dataset = task.active_dataset
-        if args.dataset_description is not None:
-            dataset = session.query(Dataset)\
-                .filter(Dataset.task_id == task.id)\
-                .filter(Dataset.description == args.dataset_description)\
-                .first()
-            if not dataset:
-                logger.error("No dataset called %s found."
-                             % args.dataset_description)
-                return False
-        if args.contest_name is not None:
-            contest = session.query(Contest)\
-                .filter(Contest.name == args.contest_name).first()
-            if task.contest != contest:
-                logger.error("%s is not in %s" %
-                             (args.task_name, args.contest_name))
-                return False
-        archive = args.file
-
-        file_cacher = FileCacher()
-
-        # Get input/output file names templates
-        input_template = args.inputtemplate
-        output_template = args.outputtemplate
-        input_re = re.compile(
-            re.escape(input_template).replace("\\*", "(.*)") + "$")
-        output_re = re.compile(
-            re.escape(output_template).replace("\\*", "(.*)") + "$")
-
-        try:
-            successful_subject, successful_message = \
-                import_testcases_from_zipfile(
-                    session, file_cacher, dataset,
-                    archive, input_re, output_re, args.overwrite, args.public)
-        except Exception as error:
-            logger.error(str(error))
-            return False
-
-        logger.info(successful_subject)
-        logger.info(successful_message)
-    return True
+    success = add_testcases(
+        args.file, args.inputtemplate, args.outputtemplate,
+        args.task_name, args.dataset_description, args.contest_name,
+        args.public, args.overwrite)
+    return 0 if success is True else 1
 
 
 if __name__ == "__main__":
-    sys.exit(0 if main() is True else 1)
+    sys.exit(main())

commit 3f5bd505cf0574e30b65d2b6c12377847bd1fc6b

Allow importing all even if some imports fail
Used to work this way. The regression was introduced in b13b5843395fe5fec6e4903894b7d1368a7c0509

cmscontrib/ImportTeam.py
@@ -85,7 +85,6 @@ class TeamImporter(object):
 
         """
         added = set()
-        success = True
 
         _, _, participations = self.loader.get_contest()
         for p in participations:
@@ -98,8 +97,9 @@ class TeamImporter(object):
                         path=team_path,
                         loader_class=get_loader(team_path)
                     )
-                    success = success and importer.do_import()
-        return success
+                    importer.do_import()
+
+        return True
 
 
 def main():

cmscontrib/ImportUser.py
@@ -115,8 +115,6 @@ class UserImporter(object):
         try to import the corresponding users.
 
         """
-        success = True
-
         _, _, participations = self.loader.get_contest()
         for p in participations:
             user_path = os.path.join(base_path, p["username"])
@@ -125,8 +123,9 @@ class UserImporter(object):
                 contest_id=self.contest_id,
                 loader_class=get_loader(user_path)
             )
-            success = success and importer.do_import()
-        return success
+            importer.do_import()
+
+        return True
 
 
 def main():