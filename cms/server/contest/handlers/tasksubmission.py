cms-dev/cms

commit e16ad239a9c681a263a3c8b4ae0faaeceefb9e38

Plugin system for programming languages

cms/server/contest/handlers/tasksubmission.py
@@ -3,7 +3,7 @@
 
 # Contest Management System - http://cms-dev.github.io/
 # Copyright © 2010-2014 Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
-# Copyright © 2010-2016 Stefano Maggiolo <s.maggiolo@gmail.com>
+# Copyright © 2010-2017 Stefano Maggiolo <s.maggiolo@gmail.com>
 # Copyright © 2010-2012 Matteo Boscariol <boscarim@hotmail.com>
 # Copyright © 2012-2016 Luca Wehrstedt <luca.wehrstedt@gmail.com>
 # Copyright © 2013 Bernard Blackham <bernard@largestprime.net>
@@ -46,8 +46,9 @@ import tornado.web
 from sqlalchemy import func
 from sqlalchemy.orm import joinedload
 
-from cms import config, filename_to_language
+from cms import config
 from cms.db import File, Submission, SubmissionResult, Task, Token
+from cms.grading.languagemanager import get_language
 from cms.grading.scoretypes import get_score_type
 from cms.grading.tasktypes import get_task_type
 from cms.server import actual_phase_required
@@ -70,6 +71,7 @@ class SubmitHandler(BaseHandler):
 
     def _send_error(self, subject, text, task):
         """Shorthand for sending a notification and redirecting."""
+        logger.warning("Sent error: `%s' - `%s'", subject, text)
         self.application.service.add_notification(
             self.current_user.user.username,
             self.timestamp,
@@ -232,46 +234,38 @@ class SubmitHandler(BaseHandler):
         for uploaded, data in self.request.files.iteritems():
             files[uploaded] = (data[0]["filename"], data[0]["body"])
 
-        # If we allow partial submissions, implicitly we recover the
-        # non-submitted files from the previous submission. And put them
-        # in file_digests (i.e. like they have already been sent to FS).
-        submission_lang = None
+        # Read the submission language provided in the request; we
+        # integrate it with the language fetched from the previous
+        # submission (if we use it) and later make sure it is
+        # recognized and allowed.
+        submission_lang = self.get_argument("language", None)
+        need_lang = any(our_filename.find(".%l") != -1
+                        for our_filename in files)
+
+        # If we allow partial submissions, we implicitly recover the
+        # non-submitted files from the previous submission (if it has
+        # the same programming language of the current one), and put
+        # them in file_digests (since they are already in FS).
         file_digests = {}
         if task_type.ALLOW_PARTIAL_SUBMISSION and \
-                last_submission_t is not None:
+                last_submission_t is not None and \
+                (submission_lang is None or
+                 submission_lang == last_submission_t.language):
+            submission_lang = last_submission_t.language
             for filename in required.difference(provided):
                 if filename in last_submission_t.files:
-                    # If we retrieve a language-dependent file from
-                    # last submission, we take not that language must
-                    # be the same.
-                    if "%l" in filename:
-                        submission_lang = last_submission_t.language
                     file_digests[filename] = \
                         last_submission_t.files[filename].digest
 
-        # We need to ensure that everytime we have a .%l in our
-        # filenames, the user has the extension of an allowed
-        # language, and that all these are the same (i.e., no
-        # mixed-language submissions).
-
-        error = None
-        for our_filename in files:
-            user_filename = files[our_filename][0]
-            if our_filename.find(".%l") != -1:
-                lang = filename_to_language(user_filename)
-                if lang is None:
-                    error = self._("Cannot recognize submission's language.")
-                    break
-                elif submission_lang is not None and \
-                        submission_lang != lang:
-                    error = self._("All sources must be in the same language.")
-                    break
-                elif lang not in contest.languages:
-                    error = self._(
-                        "Language %s not allowed in this contest.") % lang
-                    break
-                else:
-                    submission_lang = lang
+        # Throw an error if task needs a language, but we don't have
+        # it or it is not allowed / recognized.
+        if need_lang:
+            error = None
+            if submission_lang is None:
+                error = self._("Cannot recognize the submission language.")
+            elif submission_lang not in contest.languages:
+                error = self._("Language %s not allowed in this contest.") \
+                    % submission_lang
         if error is not None:
             self._send_error(self._("Invalid submission!"), error, task)
             return
@@ -552,29 +546,26 @@ class SubmissionFileHandler(FileHandler):
         # submissions, yet, if the submission_format changes during the
         # competition, this may not hold anymore for old submissions.
 
-        # filename follows our convention (e.g. 'foo.%l'), real_filename
-        # follows the one we present to the user (e.g. 'foo.c').
-        real_filename = filename
+        # filename is the name used by the browser, hence is something
+        # like 'foo.c' (and the extension is CMS's preferred extension
+        # for the language). To retrieve the right file, we need to
+        # decode it to 'foo.%l'.
+        stored_filename = filename
         if submission.language is not None:
-            if filename in submission.files:
-                real_filename = filename.replace("%l", submission.language)
-            else:
-                # We don't recognize this filename. Let's try to 'undo'
-                # the '%l' -> 'c|cpp|pas' replacement before giving up.
-                filename = re.sub(r'\.%s$' % submission.language, '.%l',
-                                  filename)
+            extension = get_language(submission.language).source_extension
+            stored_filename = re.sub(r'%s$' % extension, '.%l', filename)
 
-        if filename not in submission.files:
+        if stored_filename not in submission.files:
             raise tornado.web.HTTPError(404)
 
-        digest = submission.files[filename].digest
+        digest = submission.files[stored_filename].digest
         self.sql_session.close()
 
-        mimetype = get_type_for_file_name(real_filename)
+        mimetype = get_type_for_file_name(filename)
         if mimetype is None:
             mimetype = 'application/octet-stream'
 
-        self.fetch(digest, mimetype, real_filename)
+        self.fetch(digest, mimetype, filename)
 
 
 class UseTokenHandler(BaseHandler):

cms/__init__.py
@@ -35,11 +35,6 @@ import cms.log
 
 __all__ = [
     "__version__",
-    "LANG_C", "LANG_CPP", "LANG_PASCAL", "LANG_PYTHON", "LANG_PHP", "LANG_HS",
-    "LANGUAGE_NAMES", "LANGUAGES", "DEFAULT_LANGUAGES",
-    "SOURCE_EXT_TO_LANGUAGE_MAP", "filename_to_language",
-    "LANGUAGE_TO_SOURCE_EXT_MAP", "LANGUAGE_TO_HEADER_EXT_MAP",
-    "LANGUAGE_TO_OBJ_EXT_MAP",
     "SCORE_MODE_MAX", "SCORE_MODE_MAX_TOKENED_LAST",
     # log
     # Nothing intended for external use, no need to advertise anything.
@@ -58,90 +53,6 @@ __version__ = '1.3.dev0'
 
 
 # Instantiate or import these objects.
-
-# Shorthand codes for all supported languages.
-LANG_C = "c"
-LANG_CPP = "cpp"
-LANG_PASCAL = "pas"
-LANG_PYTHON = "py"
-LANG_PHP = "php"
-LANG_JAVA = "java"
-LANG_HS = "hs"
-
-LANGUAGE_NAMES = {
-    LANG_C: "C",
-    LANG_CPP: "C++",
-    LANG_PASCAL: "Pascal",
-    LANG_PYTHON: "Python",
-    LANG_PHP: "PHP",
-    LANG_JAVA: "Java",
-    LANG_HS: "Haskell",
-}
-
-LANGUAGES = [
-    LANG_C,
-    LANG_CPP,
-    LANG_PASCAL,
-    LANG_PYTHON,
-    LANG_PHP,
-    LANG_JAVA,
-    LANG_HS
-]
-
-DEFAULT_LANGUAGES = [LANG_C, LANG_CPP, LANG_PASCAL]
-
-# A reference for extension-based automatic language detection.
-# (It's more difficult with headers because ".h" is ambiguous.)
-SOURCE_EXT_TO_LANGUAGE_MAP = {
-    ".c": LANG_C,
-    ".cpp": LANG_CPP,
-    ".cxx": LANG_CPP,
-    ".cc": LANG_CPP,
-    ".C": LANG_CPP,
-    ".c++": LANG_CPP,
-    ".pas": LANG_PASCAL,
-    ".py": LANG_PYTHON,
-    ".php": LANG_PHP,
-    ".java": LANG_JAVA,
-    ".hs": LANG_HS
-}
-
-# Our preferred source file and header file extension for each language.
-LANGUAGE_TO_SOURCE_EXT_MAP = {
-    LANG_C: ".c",
-    LANG_CPP: ".cpp",
-    LANG_PASCAL: ".pas",
-    LANG_PYTHON: ".py",
-    LANG_PHP: ".php",
-    LANG_JAVA: ".java",
-    LANG_HS: ".hs",
-}
-LANGUAGE_TO_HEADER_EXT_MAP = {
-    LANG_C: ".h",
-    LANG_CPP: ".h",
-    LANG_PASCAL: "lib.pas",
-}
-LANGUAGE_TO_OBJ_EXT_MAP = {
-    LANG_C: ".o",
-    LANG_CPP: ".o",
-    LANG_PASCAL: ".o",
-    LANG_HS: ".o",
-}
-
-
-def filename_to_language(filename):
-    """Determine the programming language of filename from its extension.
-
-    filename (string): the file to test.
-
-    return (string|None): the extension of filename, or None if it is
-        not a recognized language.
-
-    """
-    for source_ext, language in SOURCE_EXT_TO_LANGUAGE_MAP.iteritems():
-        if filename.endswith(source_ext):
-            return language
-    return None
 
 
 # Task score modes.

cms/db/contest.py
@@ -39,7 +39,6 @@ from sqlalchemy.orm import relationship, backref
 
 from . import Base, RepeatedUnicode
 
-from cms import DEFAULT_LANGUAGES
 from cmscommon.datetime import make_datetime
 
 
@@ -76,12 +75,11 @@ class Contest(Base):
         nullable=False,
         default=[])
 
-    # The list of languages shorthand allowed in the contest,
-    # e.g. cpp. The codes must be the same as those in cms.LANGUAGES.
+    # The list of names of languages allowed in the contest.
     languages = Column(
         RepeatedUnicode(),
         nullable=False,
-        default=DEFAULT_LANGUAGES)
+        default=["C11 / gcc", "C++11 / g++", "Pascal / fpc"])
 
     # Whether contestants allowed to download their submissions.
     submissions_download_allowed = Column(

cms/grading/TaskType.py
@@ -163,14 +163,15 @@ class TaskType(object):
         submission_format ([string]): the list of files provided by the
             user that have to be compiled (the compilation command may
             contain references to other files like graders, stubs, etc...);
-            they may contain the string "%l" as a language-wildcard.
+            they may contain the string ".%l" as a language-wildcard.
+
         return ({string: [[string]]}|None): for each language (indexed
-            by its shorthand code i.e. one of the cms.LANG_* constants)
-            provide a list of commands, each as a list of tokens. That
-            is because some languages may require multiple operations
-            to compile or because some task types may require multiple
-            independent compilations (e.g. encoder and decoder); return
-            None if no compilation is required (e.g. output only).
+            by its name) provide a list of commands, each as a list of
+            tokens. That is because some languages may require
+            multiple operations to compile or because some task types
+            may require multiple independent compilations
+            (e.g. encoder and decoder); return None if no compilation
+            is required (e.g. output only).
 
         """
         raise NotImplementedError("Please subclass this class.")

cms/grading/__init__.py
@@ -3,7 +3,7 @@
 
 # Contest Management System - http://cms-dev.github.io/
 # Copyright © 2010-2015 Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
-# Copyright © 2010-2016 Stefano Maggiolo <s.maggiolo@gmail.com>
+# Copyright © 2010-2017 Stefano Maggiolo <s.maggiolo@gmail.com>
 # Copyright © 2010-2012 Matteo Boscariol <boscarim@hotmail.com>
 # Copyright © 2013 Bernard Blackham <bernard@largestprime.net>
 # Copyright © 2013-2014 Luca Wehrstedt <luca.wehrstedt@gmail.com>
@@ -37,11 +37,27 @@ from collections import namedtuple
 
 from sqlalchemy.orm import joinedload
 
-from cms import config, \
-    LANG_C, LANG_CPP, LANG_PASCAL, LANG_PYTHON, LANG_PHP, LANG_JAVA, \
-    LANG_HS, SCORE_MODE_MAX
+from cms import SCORE_MODE_MAX, config
 from cms.db import Submission
 from cms.grading.Sandbox import Sandbox
+
+from .language import Language, CompiledLanguage
+
+
+__all__ = [
+    # __init__.py
+    "JobException",
+    "COMPILATION_MESSAGES", "EVALUATION_MESSAGES",
+    "format_status_text",
+    "compilation_step", "evaluation_step",
+    "evaluation_step_before_run", "evaluation_step_after_run",
+    "human_evaluation_message", "is_evaluation_passed",
+    "filter_ansi_escape", "extract_outcome_and_text",
+    "white_diff_step",
+    "compute_changes_for_dataset", "task_score",
+    # language.py
+    "Language", "CompiledLanguage",
+]
 
 
 logger = logging.getLogger(__name__)
@@ -194,124 +210,6 @@ class JobException(Exception):
 
     def __repr__(self):
         return "JobException(\"%s\")" % (repr(self.msg))
-
-
-def get_compilation_commands(language, source_filenames, executable_filename,
-                             for_evaluation=True):
-    """Return the compilation commands.
-
-    The compilation commands are for the specified language, source
-    filenames and executable filename. Each command is a list of
-    strings, suitable to be passed to the methods in subprocess
-    package.
-
-    language (string): one of the recognized languages.
-    source_filenames ([string]): a list of the string that are the
-        filenames of the source files to compile; the order is
-        relevant: the first file must be the one that contains the
-        program entry point (with some langages, e.g. Pascal, only the
-        main file must be passed to the compiler).
-    executable_filename (string): the output file.
-    for_evaluation (bool): if True, define EVAL during the compilation;
-        defaults to True.
-
-    return ([[string]]): a list of commands, each a list of strings to
-        be passed to subprocess.
-
-    """
-    commands = []
-    if language == LANG_C:
-        command = ["/usr/bin/gcc"]
-        if for_evaluation:
-            command += ["-DEVAL"]
-        command += ["-static", "-O2", "-std=c11",
-                    "-o", executable_filename]
-        command += source_filenames
-        command += ["-lm"]
-        commands.append(command)
-    elif language == LANG_CPP:
-        command = ["/usr/bin/g++"]
-        if for_evaluation:
-            command += ["-DEVAL"]
-        command += ["-static", "-O2", "-std=c++11",
-                    "-o", executable_filename]
-        command += source_filenames
-        commands.append(command)
-    elif language == LANG_PASCAL:
-        command = ["/usr/bin/fpc"]
-        if for_evaluation:
-            command += ["-dEVAL"]
-        command += ["-XS", "-O2", "-o%s" % executable_filename]
-        command += [source_filenames[0]]
-        commands.append(command)
-    elif language == LANG_PYTHON:
-        # The executable name is fixed, and there is no way to specify
-        # the name of the pyc, so we need to bundle together two
-        # commands (compilation and rename).
-        # In order to use Python 3 change them to:
-        # /usr/bin/python3 -m py_compile %s
-        # mv __pycache__/%s.*.pyc %s
-        py_command = ["/usr/bin/python2", "-m", "py_compile",
-                      source_filenames[0]]
-        mv_command = ["/bin/mv", "%s.pyc" % os.path.splitext(os.path.basename(
-                      source_filenames[0]))[0], executable_filename]
-        commands.append(py_command)
-        commands.append(mv_command)
-    elif language == LANG_PHP:
-        command = ["/bin/cp", source_filenames[0], executable_filename]
-        commands.append(command)
-    elif language == LANG_JAVA:
-        class_name = os.path.splitext(source_filenames[0])[0]
-        command = ["/usr/bin/gcj", "--main=%s" % class_name, "-O3", "-o",
-                   executable_filename] + source_filenames
-        commands.append(command)
-    elif language == LANG_HS:
-        # Haskell module names are capitalized, so we change the source file
-        # names (except for the first one) to match the module's name.
-        # The first source file is, instead, the grader or the standalone
-        # source file; it won't be imported in any other source file, so
-        # there is no need to capitalize it.
-        def capitalize(string):
-            dirname, basename = os.path.split(string)
-            return os.path.join(dirname, basename[0].upper() + basename[1:])
-        for source in source_filenames[1:]:
-            commands.append(["/bin/ln", "-s", os.path.basename(source),
-                             capitalize(source)])
-        commands.append(["/usr/bin/ghc", "-static", "-O2", "-Wall", "-o",
-                         executable_filename, source_filenames[0]])
-    else:
-        raise ValueError("Unknown language %s." % language)
-    return commands
-
-
-def get_evaluation_commands(language, executable_filename):
-    """Return the evaluation commands.
-
-    The evaluation commands are for the given language and executable
-    filename. Each command is a list of strings, suitable to be passed
-    to the methods in subprocess package.
-
-    language (string): one of the recognized languages.
-    executable_filename (string): the name of the executable.
-
-    return ([[string]]): a list of string to be passed to subprocess.
-
-    """
-    commands = []
-    if language in (LANG_C, LANG_CPP, LANG_PASCAL, LANG_JAVA, LANG_HS):
-        command = [os.path.join(".", executable_filename)]
-        commands.append(command)
-    elif language == LANG_PYTHON:
-        # In order to use Python 3 change it to:
-        # /usr/bin/python3 %s
-        command = ["/usr/bin/python2", executable_filename]
-        commands.append(command)
-    elif language == LANG_PHP:
-        command = ["/usr/bin/php", executable_filename]
-        commands.append(command)
-    else:
-        raise ValueError("Unknown language %s." % language)
-    return commands
 
 
 def format_status_text(status, translator=None):

cms/grading/language.py
@@ -1,0 +1,134 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Contest Management System - http://cms-dev.github.io/
+# Copyright © 2016-2017 Stefano Maggiolo <s.maggiolo@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Interfaces for supported programming languages."""
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import logging
+import os
+
+
+logger = logging.getLogger(__name__)
+
+
+class Language(object):
+    """A supported programming language"""
+
+    @property
+    def name(self):
+        """Returns the name of the language.
+
+        Should be uniquely describing the language and the
+        version/compiler used, for example "C++11 / g++" better than "C++",
+        or "Java 1.5 / JDK", "Java 1.4 / gcj" better than "Java".
+
+        return (str): the name
+
+        """
+        raise NotImplementedError("Please subclass this class.")
+
+    @property
+    def source_extensions(self):
+        """Extensions used for sources for this language (including the dot).
+
+        The first one is the canonical one, used by CMS. Any of these
+        can be used by contestants.
+
+        """
+        return []
+
+    @property
+    def source_extension(self):
+        """Default source extension for the language."""
+        return self.source_extensions[0] \
+            if len(self.source_extensions) > 0 else None
+
+    @property
+    def header_extensions(self):
+        """Extensions used for headers for this language (including the dot).
+
+        """
+        return []
+
+    @property
+    def header_extension(self):
+        """Default header extension for the language."""
+        return self.header_extensions[0] \
+            if len(self.header_extensions) > 0 else None
+
+    @property
+    def object_extensions(self):
+        """Extensions used for object files for this language (including the
+        dot).
+
+        """
+        return []
+
+    @property
+    def object_extension(self):
+        """Default object extension for the language."""
+        return self.object_extensions[0] \
+            if len(self.object_extensions) > 0 else None
+
+    def get_compilation_commands(self,
+                                 source_filenames, executable_filename,
+                                 for_evaluation=True):
+        """Return the compilation commands.
+
+        The compilation commands are for the specified language,
+        source filenames and executable filename. Each command is a
+        list of strings, suitable to be passed to the methods in
+        subprocess package.
+
+        source_filenames ([string]): a list of the string that are the
+            filenames of the source files to compile; the order is
+            relevant: the first file must be the one that contains the
+            program entry point (with some langages, e.g. Pascal, only
+            the main file must be passed to the compiler).
+        executable_filename (string): the output file.
+        for_evaluation (bool): if True, define EVAL during the
+            compilation; defaults to True.
+
+        return ([[string]]): a list of commands, each a list of
+            strings to be passed to subprocess.
+
+        """
+        raise NotImplementedError("Please subclass this class.")
+
+    def get_evaluation_commands(self, executable_filename):
+        """Return the evaluation commands.
+
+        executable_filename: the name of the "executable" (does not
+            need to be executable per se).
+        return ([[string]]): a list of commands, each a list of
+            strings to be passed to subprocess.
+
+        """
+        raise NotImplementedError("Please subclass this class.")
+
+
+class CompiledLanguage(Language):
+    """A language where the compilation step produces an executable."""
+
+    def get_evaluation_commands(self, executable_filename):
+        """See Language.get_evaluation_commands."""
+        return [[os.path.join(".", executable_filename)]]

cms/grading/languagemanager.py
@@ -1,0 +1,96 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Contest Management System - http://cms-dev.github.io/
+# Copyright © 2016-2017 Stefano Maggiolo <s.maggiolo@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Provide utilities to work with programming language classes."""
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from cms import plugin_list
+
+
+__all__ = [
+    "LANGUAGES",
+    "HEADER_EXTS", "SOURCE_EXTS", "OBJECT_EXTS",
+    "get_language", "filename_to_language"
+]
+
+
+LANGUAGES = []
+_BY_NAME = dict()
+HEADER_EXTS = set()
+OBJECT_EXTS = set()
+SOURCE_EXTS = set()
+
+
+def get_language(name):
+    """Return the language object corresponding to the given name.
+
+    name (unicode): name of the requested language.
+    return (Language): language object.
+
+    raise (KeyError): if the name does not correspond to a language.
+
+    """
+    if name not in _BY_NAME:
+        raise KeyError("Language `%s' not supported." % name)
+    return _BY_NAME[name]
+
+
+def filename_to_language(filename):
+    """Return one of the languages inferred from the given filename.
+
+    filename (string): the file to test.
+
+    return (Language|None): one (arbitrary, but deterministic)
+        language matching the given filename, or None if none
+        match.
+
+    """
+    ext_index = filename.rfind(".")
+    if ext_index == -1:
+        return None
+    ext = filename[ext_index:]
+    names = sorted([language.name
+                    for language in LANGUAGES
+                    if ext in language.source_extensions])
+    return None if len(names) == 0 else get_language(names[0])
+
+
+def _load_languages():
+    """Load the available languages and fills all other data structures."""
+    global LANGUAGES, _BY_NAME, HEADER_EXTS, OBJECT_EXTS, SOURCE_EXTS
+    if LANGUAGES != []:
+        return
+
+    LANGUAGES = [
+        language()
+        for language in plugin_list("cms.grading.languages", "languages")
+    ]
+    _BY_NAME = dict(
+        (language.name, language) for language in LANGUAGES)
+    for lang in LANGUAGES:
+        HEADER_EXTS |= set(lang.header_extensions)
+        OBJECT_EXTS |= set(lang.object_extensions)
+        SOURCE_EXTS |= set(lang.source_extensions)
+
+
+# Initialize!
+_load_languages()

cms/grading/languages/__init__.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Contest Management System - http://cms-dev.github.io/
-# Copyright © 2013-2017 Stefano Maggiolo <s.maggiolo@gmail.com>
-# Copyright © 2014 Luca Wehrstedt <luca.wehrstedt@gmail.com>
+# Copyright © 2016 Stefano Maggiolo <s.maggiolo@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -17,3 +16,33 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import logging
+
+from cms import plugin_lookup
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_language_class(name):
+    """Load the Language subclass given as parameter."""
+    return plugin_lookup(name, "cms.grading.languages", "languages")
+
+
+def get_language(name=None):
+    """Construct the Language specified by parameters.
+
+    name (unicode): the name of the Language class.
+
+    return (Language): an instance of the correct Language class.
+
+    raise (ValueError): when there is no such language.
+
+    """
+    class_ = get_language_class(name)
+    return class_()

commit f1a43963f60765dbb00c4d52a7c7ddd981d03176

Fix outputonly regression caused by e16ad23.

cms/server/contest/handlers/tasksubmission.py
@@ -266,9 +266,9 @@ class SubmitHandler(BaseHandler):
             elif submission_lang not in contest.languages:
                 error = self._("Language %s not allowed in this contest.") \
                     % submission_lang
-        if error is not None:
-            self._send_error(self._("Invalid submission!"), error, task)
-            return
+            if error is not None:
+                self._send_error(self._("Invalid submission!"), error, task)
+                return
 
         # Check if submitted files are small enough.
         if any([len(f[1]) > config.max_submission_length