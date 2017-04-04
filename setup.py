0d5f41d26a0c4aea33ded49b137e20d53e7821c6
Luca Wehrstedt <luca.wehrstedt@gmail.com>
2017-04-04 17:57:24 -0400

    Remove obsolete scripts and clean up entry points in setup.py
    Some were leftovers from 4da507a6c6a0b45b4dc37f5e976da86df38d5a0e,
    others were leftovers from long before.

--- setup.py
+++ setup.py
@@ -121,9 +121,6 @@ setup(
     entry_points={
         "console_scripts": [
             "cmsRunTests=cmstestsuite.RunTests:main",
-            "cmsReplayContest=cmstestsuite.ReplayContest:main",
-            "cmsAdaptContest=cmstestsuite.AdaptContest:main",
-            "cmsTestFileCacher=cmstestsuite.TestFileCacher:main",
             "cmsAddAdmin=cmscontrib.AddAdmin:main",
             "cmsAddParticipation=cmscontrib.AddParticipation:main",
             "cmsAddStatement=cmscontrib.AddStatement:main",
@@ -138,8 +135,8 @@ setup(
             "cmsDumpUpdater=cmscontrib.DumpUpdater:main",
             "cmsExportSubmissions=cmscontrib.ExportSubmissions:main",
             "cmsImportContest=cmscontrib.ImportContest:main",
-            "cmsImportTask=cmscontrib.ImportTask:main",
             "cmsImportDataset=cmscontrib.ImportDataset:main",
+            "cmsImportTask=cmscontrib.ImportTask:main",
             "cmsImportTeam=cmscontrib.ImportTeam:main",
             "cmsImportUser=cmscontrib.ImportUser:main",
             "cmsRWSHelper=cmscontrib.RWSHelper:main",
