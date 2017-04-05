50792eb2f03a38131df2996b7aabe575ef9597df
Luca Wehrstedt <luca.wehrstedt@gmail.com>
2017-04-04 20:50:33 -0400

    Add updater for the previous six commits

--- cms/db/__init__.py
+++ cms/db/__init__.py
@@ -86,7 +86,7 @@ __all__ = [
 
 # Instantiate or import these objects.
 
-version = 26
+version = 27
 
 engine = create_engine(config.database, echo=config.database_debug,
                        pool_timeout=60, pool_recycle=120)
