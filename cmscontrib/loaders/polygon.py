b8e2948e9d34d64b3debee86fbc7a96e040e8d48
Luca Wehrstedt <luca.wehrstedt@gmail.com>
2018-01-23 15:47:23 +0100

    Handle dynamic imports using imp(ortlib)
    The use of __import__ is discouraged and the reload builtin function
    has disappeared in py3. The imp module provides a way to do the same
    thing that is supported in both py2 and py3. It is not the ideal
    solution, as it has already been deprecated and superseded by importlib
    in py3, but it probably will do until we drop py2 support.

--- cmscontrib/loaders/polygon.py
+++ cmscontrib/loaders/polygon.py
@@ -28,10 +28,10 @@ from future.builtins.disabled import *
 from future.builtins import *
 from six import iteritems
 
+import imp
 import io
 import logging
 import os
-import sys
 
 from datetime import datetime
 from datetime import timedelta
@@ -157,15 +157,14 @@ class PolygonTaskLoader(TaskLoader):
         # args['token_gen_interval'] = make_timedelta(1800)
         # args['token_gen_max'] = 2
 
-        task_cms_conf_path = os.path.join(self.path, 'files')
+        task_cms_conf_path = os.path.join(self.path, 'files', 'cms_conf.py')
         task_cms_conf = None
-        if os.path.exists(os.path.join(task_cms_conf_path, 'cms_conf.py')):
-            sys.path.append(task_cms_conf_path)
+        if os.path.exists(task_cms_conf_path):
             logger.info("Found additional CMS options for task %s.", name)
-            task_cms_conf = __import__('cms_conf')
-            # TODO: probably should find more clever way to get rid of caching
-            task_cms_conf = reload(task_cms_conf)
-            sys.path.pop()
+            with open(task_cms_conf_path, 'r') as f:
+                task_cms_conf = imp.load_module('cms_conf', f,
+                                                task_cms_conf_path,
+                                                ('.py', 'r', imp.PY_SOURCE))
         if task_cms_conf is not None and hasattr(task_cms_conf, "general"):
             args.update(task_cms_conf.general)
 
