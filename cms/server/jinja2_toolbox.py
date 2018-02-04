50af9257510be7010a80c9c219968ed10f44e7fc
Luca Wehrstedt <luca.wehrstedt@gmail.com>
2018-02-04 22:27:02 +0100

    Port CWS to Jinja2: replace comprehensions with "pipelines"

--- cms/server/jinja2_toolbox.py
+++ cms/server/jinja2_toolbox.py
@@ -32,13 +32,112 @@ from future.builtins.disabled import *
 from future.builtins import *
 from six import iterkeys, itervalues, iteritems
 
-from jinja2 import Environment, StrictUndefined
+from jinja2 import Environment, StrictUndefined, contextfilter, contextfunction
 
+from cmscommon.datetime import utc
 from cmscommon.mimetypes import get_type_for_file_name, get_name_for_type, \
     get_icon_for_type
+from cms.grading.languagemanager import get_language
 from cms.grading.scoretypes import get_score_type
 from cms.grading.tasktypes import get_task_type
 from cms.server.contest.formatting import get_score_class
+
+
+@contextfilter
+def all_(ctx, l, test=None, *args):
+    """Check if all elements of the given list pass the given test.
+
+    ctx (Context): a Jinja2 context, needed to retrieve the test
+        function by its name and to execute it.
+    l (list): a list of objects.
+    test (str|None): the name of the test to execute on each object of
+        l (leave unspecified to just check their truth value).
+    *args: parameters to pass to the test function.
+
+    return (bool): all(test(i, *args) for i in l).
+
+    """
+    if test is None:
+        test = bool
+    else:
+        test = ctx.environment.tests[test]
+    for i in l:
+        if not ctx.call(test, i, *args):
+            return False
+    return True
+
+
+@contextfilter
+def any_(ctx, l, test=None, *args):
+    """Check if any element of the given list passes the given test.
+
+    ctx (Context): a Jinja2 context, needed to retrieve the test
+        function by its name and to execute it.
+    l (list): a list of objects.
+    test (str|None): the name of the test to execute on each object of
+        l (leave unspecified to just check their truth value).
+    *args: parameters to pass to the test function.
+
+    return (bool): any(test(i, *args) for i in l).
+
+    """
+    if test is None:
+        test = bool
+    else:
+        test = ctx.environment.tests[test]
+    for i in l:
+        if ctx.call(test, i, *args):
+            return True
+    return False
+
+
+# FIXME once we drop py2 do dictselect(ctx, d, test=None, *args, by="key")
+@contextfilter
+def dictselect(ctx, d, test=None, *args, **kwargs):
+    """Filter the given dict: keep only items that pass the given test.
+
+    ctx (Context): a Jinja2 context, needed to retrieve the test
+        function by its name and execute it.
+    d (dict): a dict.
+    test (str|None): the name of the test to execute on either the key
+        or the value of each item of d (leave unspecified to just check
+        the truth value).
+    *args: parameters to pass to the test function.
+    by (str): either "key" (default) or "value", specifies on which
+        component to perform the test.
+
+    return (dict): {k, v for k, v in d.items() if test(k/v, *args)}.
+
+    """
+    if test is None:
+        test = bool
+    else:
+        test = ctx.environment.tests[test]
+    by = kwargs.pop("by", "key")
+    if len(kwargs) > 0:
+        raise ValueError("Invalid keyword argument: %s"
+                         % next(iterkeys(kwargs)))
+    if by not in {"key", "value"}:
+        raise ValueError("Invalid value of \"by\" keyword argument: %s" % by)
+    return dict((k, v) for k, v in iteritems(d)
+                if ctx.call(test, {"key": k, "value": v}[by], *args))
+
+
+@contextfunction
+def today(ctx, dt):
+    """Returns whether the given datetime is today.
+
+    ctx (Context): a Jinja2 context, needed to retrieve the current
+        datetime and the timezone to use when comparing.
+    dt (datetime): a datetime.
+
+    return (bool): whether dt occurred today in the timezone.
+
+    """
+    now = ctx["now"]
+    timezone = ctx["timezone"]
+    return dt.replace(tzinfo=utc).astimezone(timezone).date() \
+        == now.replace(tzinfo=utc).astimezone(timezone).date()
 
 
 def instrument_generic_toolbox(env):
@@ -55,6 +154,17 @@ def instrument_generic_toolbox(env):
     env.globals["get_mimetype_for_file_name"] = get_type_for_file_name
     env.globals["get_name_for_mimetype"] = get_name_for_type
     env.globals["get_icon_for_mimetype"] = get_icon_for_type
+
+    env.filters["all"] = all_
+    env.filters["any"] = any_
+    env.filters["dictselect"] = dictselect
+
+    env.filters["to_language"] = get_language
+
+    env.tests["contains"] = lambda s, p: p in s
+    env.tests["endswith"] = lambda s, p: s.endswith(p)
+
+    env.tests["today"] = today
 
 
 GLOBAL_ENVIRONMENT = Environment(
