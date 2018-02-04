7327bb108b1f5c9a5f14eb879db531ca7f1c29a5
Luca Wehrstedt <luca.wehrstedt@gmail.com>
2018-02-04 23:01:36 +0100

    Port CWS to Jinja2: remove imports

--- cms/server/jinja2_toolbox.py
+++ cms/server/jinja2_toolbox.py
@@ -30,8 +30,31 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from future.builtins.disabled import *
 from future.builtins import *
+from six import iterkeys, itervalues, iteritems
 
 from jinja2 import Environment, StrictUndefined
+
+from cmscommon.mimetypes import get_type_for_file_name, get_name_for_type, \
+    get_icon_for_type
+from cms.grading.scoretypes import get_score_type
+from cms.grading.tasktypes import get_task_type
+from cms.server.contest.formatting import get_score_class
+
+
+def instrument_generic_toolbox(env):
+    env.globals["iterkeys"] = iterkeys
+    env.globals["itervalues"] = itervalues
+    env.globals["iteritems"] = iteritems
+    env.globals["next"] = next
+
+    env.globals["get_task_type"] = get_task_type
+    env.globals["get_score_type"] = get_score_type
+
+    env.globals["get_score_class"] = get_score_class
+
+    env.globals["get_mimetype_for_file_name"] = get_type_for_file_name
+    env.globals["get_name_for_mimetype"] = get_name_for_type
+    env.globals["get_icon_for_mimetype"] = get_icon_for_type
 
 
 GLOBAL_ENVIRONMENT = Environment(
@@ -47,3 +70,6 @@ GLOBAL_ENVIRONMENT = Environment(
     # Don't check the disk every time to see whether the templates'
     # files have changed.
     auto_reload=False)
+
+
+instrument_generic_toolbox(GLOBAL_ENVIRONMENT)
