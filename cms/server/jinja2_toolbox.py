9692472852df6d0b2a981c2b0e38c739df8b05ad
Luca Wehrstedt <luca.wehrstedt@gmail.com>
2018-02-06 00:00:57 +0100

    Have CWS load templates using Jinja2 rather than Tornado

---
+++ cms/server/jinja2_toolbox.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Contest Management System - http://cms-dev.github.io/
+# Copyright © 2018 Luca Wehrstedt <luca.wehrstedt@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Provide a generic Jinja2 environment.
+
+Create a Jinja2 environment and instrument it with utilities (globals,
+filters, tests, etc. that are useful for generic global usage.
+
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from future.builtins.disabled import *
+from future.builtins import *
+
+from jinja2 import Environment, StrictUndefined
+
+
+GLOBAL_ENVIRONMENT = Environment(
+    trim_blocks=True, lstrip_blocks=True, undefined=StrictUndefined,
+    autoescape=True, cache_size=-1, auto_reload=False)
