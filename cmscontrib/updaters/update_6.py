cms-dev/cms

commit d2b9063916025ba46eb5f4b69e5b2737095949ef

Replace d.iterkeys/values/items() method calls with iter...(d) function calls
In py2 .keys() returns a list and .iterkeys() returns an iterator. In
py3 there is only .keys() which returns a view, which is iterable. The
six module offers a iterkeys function which translates to iterkeys on
py2 and to the iterable of the keys view in py3. The same holds for
values and items.

cmscontrib/updaters/update_6.py
@@ -33,6 +33,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from future.builtins.disabled import *
 from future.builtins import *
+from six import iteritems
 
 import json
 
@@ -113,7 +114,7 @@ class Updater(object):
         self.objs = data
 
     def run(self):
-        for k, v in self.objs.iteritems():
+        for k, v iteritems(in self.objs):
             if k.startswith("_"):
                 continue
 

commit 97ed363ced1fe663347e00739e6c6a3aa36db255

Fix syntax error in update_6.py
Introduced in commit d2b9063

cmscontrib/updaters/update_6.py
@@ -114,7 +114,7 @@ class Updater(object):
         self.objs = data
 
     def run(self):
-        for k, v iteritems(in self.objs):
+        for k, v in iteritems(self.objs):
             if k.startswith("_"):
                 continue
 