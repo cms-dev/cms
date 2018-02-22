9e9cbcdce6c26dbe9b6d69636f9a999f9cd59b62
Luca Wehrstedt <luca.wehrstedt@gmail.com>
2018-02-22 23:08:41 +0100

    Port AWS to Jinja2: remove imports

--- cms/server/contest/handlers/tasksubmission.py
+++ cms/server/contest/handlers/tasksubmission.py
@@ -411,7 +411,6 @@ class TaskSubmissionsHandler(ContestHandler):
                     submissions_left=submissions_left,
                     submissions_download_allowed=
                         self.contest.submissions_download_allowed,
-                    SubmissionResult=SubmissionResult,
                     **self.r_params)
 
 
