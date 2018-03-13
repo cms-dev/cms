ef79c649c488ab9ace32219f4764b690d9bfe12c
Stefano Maggiolo <s.maggiolo@gmail.com>
2018-03-13 10:53:20 +0000

    Fixup: docstring

--- cmstestsuite/unit_tests/cmscommon/digest_test.py
+++ cmstestsuite/unit_tests/cmscommon/digest_test.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Tests for the DumpExporter script"""
+"""Tests for the the digest module"""
 
 from __future__ import absolute_import
 from __future__ import division
