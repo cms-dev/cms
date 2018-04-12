6ddb5d253b4dc6fe0ce2f4fff35e22ecfb8252ab
Luca Wehrstedt <luca.wehrstedt@gmail.com>
2018-04-12 09:20:32 +0200

    Fixup docstring

--- cms/server/contest/submission.py
+++ cms/server/contest/submission.py
@@ -26,7 +26,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Submission-related handlers for CWS for a specific task.
+"""Submission-related helpers for CWS.
 
 """
 
