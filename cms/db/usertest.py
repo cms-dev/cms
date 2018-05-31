0b5596c37b32c99f482e34d8a860a52a0d0d3002
Fabian Gundlach <320pointsguy@googlemail.com>
2018-05-31 14:58:34 +0200

    Use BigInteger for storing execution_memory in the database
    When a submission or user test used more than 2GB of memory, the number
    of used bytes doesn't fit in an Integer.

--- cms/db/usertest.py
+++ cms/db/usertest.py
@@ -32,7 +32,8 @@ from future.builtins import *  # noqa
 
 from sqlalchemy.schema import Column, ForeignKey, ForeignKeyConstraint, \
     UniqueConstraint
-from sqlalchemy.types import Integer, Float, String, Unicode, DateTime
+from sqlalchemy.types import Integer, Float, String, Unicode, DateTime, \
+    BigInteger
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm.collections import attribute_mapped_collection
 from sqlalchemy.dialects.postgresql import ARRAY
@@ -357,7 +358,7 @@ class UserTestResult(Base):
         Float,
         nullable=True)
     execution_memory = Column(
-        Integer,
+        BigInteger,
         nullable=True)
 
     # Worker shard and sandbox where the evaluation was performed.
