d283e77b5cb2c751b866cd302cf40ce11fba050c
William Di Luigi <williamdiluigi@gmail.com>
2018-08-22 18:46:52 +0200

    Remove unique=True from Task.name

--- cms/db/task.py
+++ cms/db/task.py
@@ -99,8 +99,7 @@ class Task(Base):
     # Short name and long human readable title of the task.
     name = Column(
         Codename,
-        nullable=False,
-        unique=True)
+        nullable=False)
     title = Column(
         Unicode,
         nullable=False)
