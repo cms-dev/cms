9975278bbd528bde4d20cca23518f619d41e8c9f
Stefano Maggiolo <s.maggiolo@gmail.com>
2018-09-09 17:46:03 +0100

    Fixup tests

--- cmstestsuite/tasks/batch_fileio_managed/code/grader.py
+++ cmstestsuite/tasks/batch_fileio_managed/code/grader.py
@@ -3,4 +3,4 @@ import batchfileiomanaged
 n = int(open("input.txt").readline().strip())
 f = open("output.txt", "w")
 f.write("correct %d\n" % batchfileiomanaged.userfunc(n))
-# f intentionally left opened.
+# f intentionally left open.
