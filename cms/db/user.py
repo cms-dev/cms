21c05549e827b9cda5d9adae08d9901e1c24080a
Stefano Maggiolo <s.maggiolo@gmail.com>
2018-09-14 21:27:48 +0100

    Fixup q

--- cms/db/user.py
+++ cms/db/user.py
@@ -394,7 +394,7 @@ class Question(Base):
 
     # Latest admin to interact with the question (null if no interactions
     # yet, or if the admin has been later deleted). Admins only loosely "own" a
-    # question, so we do not back populate any field in Admin, not delete the
+    # question, so we do not back populate any field in Admin, nor delete the
     # question if the admin gets deleted.
     admin_id = Column(
         Integer,
