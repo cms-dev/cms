4621c647a65ebea1865646e6b49c77cbb43942e8
Luca Wehrstedt <luca.wehrstedt@gmail.com>
2018-10-03 23:14:03 +0200

    Update shebang to reference Python 3 only

--- cmstestsuite/unit_tests/server/contest/phase_management_test.py
+++ cmstestsuite/unit_tests/server/contest/phase_management_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Contest Management System - http://cms-dev.github.io/
--- docs/gh_links.py
+++ docs/gh_links.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Sphinx extension to add roles for some GitHub features
