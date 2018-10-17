59d0054fcf5e05343b152c3a2d01507bd5afe543
William Di Luigi <williamdiluigi@gmail.com>
2018-10-17 17:22:29 +0200

    Add basic registration form

---
+++ cmscontrib/updaters/update_42.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# Contest Management System - http://cms-dev.github.io/
+# Copyright © 2018 William Di Luigi <williamdiluigi@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""A class to update a dump created by CMS.
+
+Used by DumpImporter and DumpUpdater.
+
+Add default value for Contest.allow_registration
+
+"""
+
+
+class Updater:
+
+    def __init__(self, data):
+        assert data["_version"] == 41
+        self.objs = data
+
+    def run(self):
+        for k, v in self.objs.items():
+            if k.startswith("_"):
+                continue
+            if v["_class"] == "Contest":
+                v["allow_registration"] = False
+
+        return self.objs
