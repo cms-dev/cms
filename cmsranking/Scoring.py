183075cb87d13b4052545ac7b23b927305653912
William Di Luigi <williamdiluigi@gmail.com>
2018-10-19 19:42:59 +0200

    Fix bug with RWS and the new score mode
    Also slightly rewrite a max expression

--- cmsranking/Scoring.py
+++ cmsranking/Scoring.py
@@ -3,6 +3,7 @@
 # Contest Management System - http://cms-dev.github.io/
 # Copyright © 2011-2013 Luca Wehrstedt <luca.wehrstedt@gmail.com>
 # Copyright © 2018 Stefano Maggiolo <s.maggiolo@gmail.com>
+# Copyright © 2018 William Di Luigi <williamdiluigi@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -114,11 +115,11 @@ class Score:
             self._last = self._submissions[s_id]
 
         if self._score_mode == SCORE_MODE_MAX:
-            score = max([0.0] +
-                        [submission.score
-                         for submission in self._submissions.values()])
+            score = max((submission.score
+                         for submission in self._submissions.values()),
+                        default=0.0)
         elif self._score_mode == SCORE_MODE_MAX_SUBTASK:
-            scores_by_submission = (s.extra or []
+            scores_by_submission = (map(float, s.extra or [])
                                     for s in self._submissions.values())
             scores_by_subtask = zip_longest(*scores_by_submission,
                                             fillvalue=0.0)
