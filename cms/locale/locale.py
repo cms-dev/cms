bf37719c0e5f4c9e626c0a4e94f8fab561c38a0d
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2018-10-26 18:27:52 +0200

    Fixed bug if the memory ends between 1000 and 1024 XiB
    If the memory used (or other values used as parameter of format_size)
    ends in the range [1000, 1024) KiB/MiB/GiB... AWS and CWS will fail
    rendering that size and the submission details wont be shown! This
    because the number of digits of the number was supposed being at most
    3, not true anymore since 1024 is used as the base.
    
    Allowing an extra significant digit for those cases is the proposed solution.

--- cms/locale/locale.py
+++ cms/locale/locale.py
@@ -223,7 +223,8 @@ class Translation:
             if n < self.PREFIX_FACTOR:
                 f = copy.copy(self.locale.decimal_formats[None])
                 # We need int because floor returns a float in py2.
-                d = int(2 - math.floor(math.log10(n)))
+                # if 1000 <= n < 1024 d can be negative, cap to 0 decimals
+                d = max(0, int(2 - math.floor(math.log10(n))))
                 f.frac_prec = (d, d)
                 return (self.gettext(unit)
                         % babel.numbers.format_decimal(n, format=f,
