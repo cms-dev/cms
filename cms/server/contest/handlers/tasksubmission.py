da345405fe35b2d5754c17a823a5699aacf80d00
Stefano Maggiolo <s.maggiolo@gmail.com>
2018-11-27 09:46:02 +0000

    Fix loading in task score not going away after a compilation failed
    Fixes #1077

--- cms/server/contest/handlers/tasksubmission.py
+++ cms/server/contest/handlers/tasksubmission.py
@@ -171,6 +171,14 @@ class TaskSubmissionsHandler(ContestHandler):
 
 class SubmissionStatusHandler(ContestHandler):
 
+    STATUS_TEXT = {
+        SubmissionResult.COMPILING: N_("Compiling..."),
+        SubmissionResult.COMPILATION_FAILED: N_("Compilation failed"),
+        SubmissionResult.EVALUATING: N_("Evaluating..."),
+        SubmissionResult.SCORING: N_("Scoring..."),
+        SubmissionResult.SCORED: N_("Evaluated"),
+    }
+
     refresh_cookie = False
 
     def add_task_score(self, participation, task, data):
@@ -231,39 +239,36 @@ class SubmissionStatusHandler(ContestHandler):
         else:
             data["status"] = sr.get_status()
 
-        if data["status"] == SubmissionResult.COMPILING:
-            data["status_text"] = self._("Compiling...")
-        elif data["status"] == SubmissionResult.COMPILATION_FAILED:
-            data["status_text"] = self._("Compilation failed")
-        elif data["status"] == SubmissionResult.EVALUATING:
-            data["status_text"] = self._("Evaluating...")
-        elif data["status"] == SubmissionResult.SCORING:
-            data["status_text"] = self._("Scoring...")
-        elif data["status"] == SubmissionResult.SCORED:
-            data["status_text"] = self._("Evaluated")
+        data["status_text"] = self._(self.STATUS_TEXT[data["status"]])
+
+        # For terminal statuses we add the scores information to the payload.
+        if data["status"] == SubmissionResult.COMPILATION_FAILED \
+                or data["status"] == SubmissionResult.SCORED:
             self.add_task_score(submission.participation, task, data)
 
             score_type = task.active_dataset.score_type_object
             if score_type.max_public_score > 0:
                 data["max_public_score"] = \
                     round(score_type.max_public_score, task.score_precision)
-                data["public_score"] = \
-                    round(sr.public_score, task.score_precision)
-                data["public_score_message"] = score_type.format_score(
-                    sr.public_score, score_type.max_public_score,
-                    sr.public_score_details, task.score_precision,
-                    translation=self.translation)
+                if data["status"] == SubmissionResult.SCORED:
+                    data["public_score"] = \
+                        round(sr.public_score, task.score_precision)
+                    data["public_score_message"] = score_type.format_score(
+                        sr.public_score, score_type.max_public_score,
+                        sr.public_score_details, task.score_precision,
+                        translation=self.translation)
             if score_type.max_public_score < score_type.max_score \
                     and (submission.token is not None
                          or self.r_params["actual_phase"] == 3):
                 data["max_score"] = \
                     round(score_type.max_score, task.score_precision)
-                data["score"] = \
-                    round(sr.score, task.score_precision)
-                data["score_message"] = score_type.format_score(
-                    sr.score, score_type.max_score,
-                    sr.score_details, task.score_precision,
-                    translation=self.translation)
+                if data["status"] == SubmissionResult.SCORED:
+                    data["score"] = \
+                        round(sr.score, task.score_precision)
+                    data["score_message"] = score_type.format_score(
+                        sr.score, score_type.max_score,
+                        sr.score_details, task.score_precision,
+                        translation=self.translation)
 
         self.write(data)
 
