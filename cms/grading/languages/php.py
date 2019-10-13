c579de3d3fe7776d388a8e4b0c6e1146bba86472
Andrey Vihrov <andrey.vihrov@gmail.com>
2019-10-13 15:15:45 +0300

    Update PHP language to use executable extension

--- cms/grading/languages/php.py
+++ cms/grading/languages/php.py
@@ -40,11 +40,20 @@ class Php(Language):
         """See Language.source_extensions."""
         return [".php"]
 
+    @property
+    def executable_extension(self):
+        """See Language.executable_extension."""
+        return ".php"
+
     def get_compilation_commands(self,
                                  source_filenames, executable_filename,
                                  for_evaluation=True):
         """See Language.get_compilation_commands."""
-        return [["/bin/cp", source_filenames[0], executable_filename]]
+        if source_filenames[0] != executable_filename:
+            return [["/bin/cp", source_filenames[0], executable_filename]]
+        else:
+            # We need at least one command to collect execution stats.
+            return [["/bin/true"]]
 
     def get_evaluation_commands(
             self, executable_filename, main=None, args=None):
