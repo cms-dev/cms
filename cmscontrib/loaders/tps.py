e1f12303e48c88d3d6c729dca04b175859cf7623
Jonathan Irvin Gunawan <jonathanirvings@users.noreply.github.com>
2021-03-16 23:01:59 +0800

    Use SCORE_MODE_MAX_SUBTASK as default score_mode for tps loader

--- cmscontrib/loaders/tps.py
+++ cmscontrib/loaders/tps.py
@@ -26,6 +26,7 @@ import subprocess
 from datetime import timedelta
 
 from cms.db import Task, Dataset, Manager, Testcase, Attachment, Statement
+from cmscommon.constants import SCORE_MODE_MAX_SUBTASK
 from .base_loader import TaskLoader
 
 
@@ -123,6 +124,7 @@ class TpsTaskLoader(TaskLoader):
 
         args["name"] = name
         args["title"] = data['name']
+        args["score_mode"] = SCORE_MODE_MAX_SUBTASK
 
         # Statements
         if get_statement:
