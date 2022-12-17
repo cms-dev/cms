fc280d8c0befa65e7e5bbd53adee5312449ea5c9
William Di Luigi <williamdiluigi@gmail.com>
2022-12-17 01:26:43 +0100

    Remove codecov upload logic (now it's in gh action)

--- cmstestsuite/testrunner.py
+++ cmstestsuite/testrunner.py
@@ -3,6 +3,7 @@
 # Contest Management System - http://cms-dev.github.io/
 # Copyright © 2015-2018 Stefano Maggiolo <s.maggiolo@gmail.com>
 # Copyright © 2016 Amir Keivan Mohtashami <akmohtashami97@gmail.com>
+# Copyright © 2022 William Di Luigi <williamdiluigi@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
