3f2a08fdc31d44e0f094332719249beda71c16bd
Filippo Casarin <55024474+Virv12@users.noreply.github.com>
2024-12-05 18:43:11 +0100

    Customizable documentation (#1300)
    Allows documentation for multiple languges, when docs_path is not set
    the legacy stl_path variable is used mantaining retrocompatibility.
    
    Co-authored-by: Edoardo Morassutto <edoardo.morassutto@gmail.com>

--- cms/server/contest/server.py
+++ cms/server/contest/server.py
@@ -100,7 +100,7 @@ class ContestWebServer(WebService):
             listen_address=listen_address)
 
         self.wsgi_app = SharedDataMiddleware(
-            self.wsgi_app, {"/stl": config.stl_path},
+            self.wsgi_app, {"/docs": config.docs_path or config.stl_path},
             cache=True, cache_timeout=SECONDS_IN_A_YEAR,
             fallback_mimetype="application/octet-stream")
 
