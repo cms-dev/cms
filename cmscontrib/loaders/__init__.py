6e2a2fa2fddeb2ab3c5cc0c3784ef6c51781c6da
Ubuntu <ubuntu@ip-172-31-12-16.ap-southeast-2.compute.internal>
2024-12-10 16:52:57 +1100

    fix ctf loader

--- cmscontrib/loaders/__init__.py
+++ cmscontrib/loaders/__init__.py
@@ -18,13 +18,15 @@
 from .italy_yaml import YamlLoader
 from .polygon import PolygonTaskLoader, PolygonUserLoader, PolygonContestLoader
 from .tps import TpsTaskLoader
+from .ctf import CtfTaskLoader
 
 
 LOADERS = dict(
     (loader_class.short_name, loader_class) for loader_class in [
         YamlLoader,
         PolygonTaskLoader, PolygonUserLoader, PolygonContestLoader,
-        TpsTaskLoader
+        TpsTaskLoader,
+        CtfTaskLoader
     ]
 )
 
