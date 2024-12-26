5a6ca3469d9dc2d8893af44e5848675375cddcf7
William Di Luigi <williamdiluigi@gmail.com>
2024-12-26 20:29:22 +0100

    Drop logic for non-installed use case

--- cmscontrib/loaders/polygon.py
+++ cmscontrib/loaders/polygon.py
@@ -185,9 +185,6 @@ class PolygonTaskLoader(TaskLoader):
                     os.path.dirname(checker_src), "checker")
                 testlib_path = "/usr/local/include/cms"
                 testlib_include = os.path.join(testlib_path, "testlib.h")
-                if not config.installed:
-                    testlib_path = os.path.join(os.path.dirname(__file__),
-                                                "polygon")
                 code = subprocess.call(["g++", "-x", "c++", "-O2", "-static",
                                         "-DCMS", "-I", testlib_path,
                                         "-include", testlib_include,
