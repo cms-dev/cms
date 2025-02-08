e25e313d1a858cc91bb6a8aa11ea28781ce44bfc
HeziCyan <hezicyan@gmail.com>
2025-02-09 04:30:24 +0800

    Always return max_score in submission data when terminal

--- cms/server/contest/handlers/tasksubmission.py
+++ cms/server/contest/handlers/tasksubmission.py
@@ -267,12 +267,12 @@ class SubmissionStatusHandler(ContestHandler):
                         sr.public_score, score_type.max_public_score,
                         sr.public_score_details, task.score_precision,
                         translation=self.translation)
-            if score_type.max_public_score < score_type.max_score \
-                    and (submission.token is not None
-                         or self.r_params["actual_phase"] == 3):
+            if score_type.max_public_score < score_type.max_score:
                 data["max_score"] = \
                     round(score_type.max_score, task.score_precision)
-                if data["status"] == SubmissionResult.SCORED:
+                if data["status"] == SubmissionResult.SCORED \
+                        and (submission.token is not None
+                            or self.r_params["actual_phase"] == 3):
                     data["score"] = \
                         round(sr.score, task.score_precision)
                     data["score_message"] = score_type.format_score(
