13c491e5e6b37d92adbd2af0039b808a7299a8dc
Pasit Sangprachathanarak <ouipingpasit@gmail.com>
2025-08-05 21:12:03 +0700

    Add credit for original owner?

--- cms/server/contest/handlers/main.py
+++ cms/server/contest/handlers/main.py
@@ -11,6 +11,7 @@
 # Copyright © 2015-2018 William Di Luigi <williamdiluigi@gmail.com>
 # Copyright © 2021 Grace Hawkins <amoomajid99@gmail.com>
 # Copyright © 2025 Pasit Sangprachathanarak <ouipingpasit@gmail.com>
+# Copyright © 2025 kk@cscmu-cnx
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
