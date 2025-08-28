39e65ae048df0538731b06e3a13058da1f2cf25a
prandla <prandla@r9.pm>
2025-08-28 03:21:49 +0300

    Make the helper scripts in docker/ a bit more robust
    When the git repo is in a detached HEAD state, GIT_BRANCH_NAME became
    "HEAD", which docker does not like as a project name. So I lowercased it
    and added a cms- in front for good measure.

--- docker/cms-test.sh
+++ docker/cms-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
 
-GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD | tr A-Z a-z)
 
-docker compose -p $GIT_BRANCH_NAME -f docker/docker-compose.test.yml run --build --rm testcms
+docker compose -p cms-$GIT_BRANCH_NAME -f docker/docker-compose.test.yml run --build --rm testcms
