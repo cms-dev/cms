a025cd1e1c6874c1378fa634926011e517c52839
singto1597 <singto1597@gmail.com>
2025-11-01 17:32:42 +0700

    Delete Folder

--- scriptCMS/startCmsWithTilix.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-tilix -e "/home/singto1597/Documents/program-project/cmsPiriyalai/scriptCMS/runSystem.sh" 
-
-tilix -e "/home/singto1597/Documents/program-project/cmsPiriyalai/scriptCMS/runCmsSystemAll.sh"
