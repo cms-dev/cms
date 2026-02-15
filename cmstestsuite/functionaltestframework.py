32b1a1fceb50c37a6875659e3e9b49398c0a5a3f
Jonathan Baumann <jonathan.baumann@mpi-sp.org>
2026-02-15 15:02:29 +0100

    fixup! Fix (most) failing tests

--- cmstestsuite/functionaltestframework.py
+++ cmstestsuite/functionaltestframework.py
@@ -213,7 +213,7 @@ class FunctionalTestFramework:
             r'method="POST" name="edit_contest" style="display:inline;">',
             page)
         match_group = re.search(
-            r'<a href="../contest/[0-9]+/group/([0-9]+)/edit">',
+            r'<a href=\'../contest/[0-9]+/group/([0-9]+)/edit\'>',
             page)
         if match_contest is not None and match_group is not None:
             contest_id = int(match_contest.groups()[0])
